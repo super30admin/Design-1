c4590517f037132e513a612bab4115702c5b75d2
shazmaan <shazmaan@buffalo.edu>
2019-07-01 23:21:05 +0400

    Completed Problem2.

--- Problem2.cpp
+++ Problem2.cpp
@@ -5,14 +5,15 @@
 
 using namespace std;
 
-#define MAX 3
+#define MAX 10
 
 class SpecialStack {
-    int top;
-    int counter = 0;
+    int top; int MinTop;
+    int counter = 0; int MinIndex=0;
     int minCounter = 0;
 public:
     int a[MAX];// Maximum size of Stack
+    int mina[MAX];
     int size = 0;
     SpecialStack() { //Constructor here
     }
@@ -29,6 +30,7 @@ bool SpecialStack::push(int x){
     if(counter<MAX){
         a[counter]=x;
         if((size==0)||x<minCounter){
+            MinTop=x; mina[MinIndex]=x; MinIndex++;
             minCounter=x;
         }
 //        cout<<"counter : "<<counter<<endl;
@@ -54,6 +56,11 @@ int SpecialStack::pop(){
         ret = a[counter];
         top=a[size-2];
         size--;
+        if(ret==MinTop){
+            minCounter = mina[MinIndex-2];
+            MinTop = minCounter;
+            MinIndex--;
+        }
 //        cout<<"TOP : "<<top<<endl;
 
     }
@@ -99,16 +106,26 @@ int main() {
 
     class SpecialStack s;
 
+    s.push(15);
+    s.push(30);
+
     s.push(10);
 
     s.push(20);
-
-    s.push(30);
+//    cout<<s.size<<endl;
+//    cout << s.pop() << " Popped from stack\n";
+//    s.push(30);
     cout<<s.size<<endl;
+    cout<<s.getMin()<<endl;
     cout << s.pop() << " Popped from stack\n";
-    s.push(30);
-    cout<<s.size<<endl;
-    cout<<s.getMin();
+    cout<<s.getMin()<<endl;
+    cout << s.pop() << " Popped from stack\n";
+    cout<<s.getMin()<<endl;
+    s.push(10);
+    cout<<s.getMin()<<endl;
+
+    cout << s.pop() << " Popped from stack\n";
+    cout<<s.getMin()<<endl;
 //    cout<<s.peek();
 //    s.push(30);
 
