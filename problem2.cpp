10f57909c88a61ed891fddb8b15cd3f673d2506a
GAURAV JOSHI <gsj38@msstate.edu>
2019-07-01 20:05:20 -0500

    Updated pop() method to fix error when minimum element is negative

--- problem2.cpp
+++ problem2.cpp
@@ -15,33 +15,47 @@ public:
     int minimumElement;
     stack<int> myStack;
 
+    //Prints out the minimum Element unless stack is empty
     void getMin()
     {
         if (myStack.empty())
-            throw exception("Stack is empty");
-        cout<<minimumElement<<endl;
+            cout<<"Stack is empty";
+        else    
+            cout<<minimumElement<<endl;
     }
 
+    /*
+        Two scenarios to be considered.
+        1. If top is greater than/equal to min value then print it. 
+        1. If the top is the min value then 
+    
+     */
     void pop()
     {
         if(myStack.empty())
-            throw exception("Stack is empty");
+            cout<<"Stack is empty";
         
-        int t = myStack.top();
+        int top = myStack.top();
         myStack.pop();
+        
+        if(top < minimumElement)    
+            minimumElement = 2*minimumElement - top;
+        else
+            cout<<top;
+
     }
 
     void peek()
     {
         if(myStack.empty())
-            throw exception("Stack is empty");
+            cout<<"Stack is empty";
         
-        int t = myStack.top();
+        int top = myStack.top();
 
-        if(t < minimumElement)
+        if(top < minimumElement)
             cout<<minimumElement;
         else
-            cout<<t;
+            cout<<top;
     }
 
     void push(int x)
