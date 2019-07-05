f96e36999f7a8f85ba798718fbb412bb2761561b
bharatreddypothireddy <35764299+bharatreddypothireddy@users.noreply.github.com>
2019-07-05 00:34:07 -0500

    Completed Day 1 - HashMap Implementation
    Implemented a hashmap using LIstNodes method taught in the session

--- NewHashMap.java
+++ NewHashMap.java
@@ -1,23 +1,66 @@
 //keys should be positive
 public class NewHashMap {
-    boolean[] keyArr = new boolean[1000000];
-    int[] valueArr = new int[100000];
+    ListNode[] nodes ;
 
-    public void put(int key,int value){
-        keyArr[key] = true;
-        valueArr[key] = value;
-    }
-
-    //returns -1 if no key found
-    public int get(int key){
-        if(keyArr[key]){
-            return valueArr[key];
+    public class ListNode{
+        int key;
+        int val;
+        ListNode next;
+        public ListNode(int data,int val){
+            this.key = data;
+            this.val = val;
+            this.next = null;
         }
-        return -1;
+    }
+    public NewHashMap(){
+        nodes = new ListNode[100000];
     }
 
+    public void put(int key,int val){
+        int index = index(key);
+        if(nodes[index] == null)
+            nodes[index] = new ListNode(-1,-1);
+        ListNode prev = find(nodes[index],key);
+        if(prev.next==null)
+            prev.next = new ListNode(key,val);
+        else
+            prev.next.val = val;
+
+
+    }
     public void remove(int key){
-        keyArr[key] = false;
+        if(nodes[index(key)]==null)
+            return;
+        ListNode prev = find(nodes[index(key)],key);
+        if(prev.next == null)
+            return;
+        else
+            prev.next = prev.next.next;
+    }
+    public int get(int key){
+        int index = index(key);
+        if(nodes[index] == null)
+            return -1;
+        else{
+            ListNode prev = find(nodes[index],key);
+            if(prev.next==null)
+                return -1;
+            return prev.next.val;
+        }
+    }
+
+    public int index(int key){
+        //int x = Integer.hashCode(key);
+        return Integer.hashCode(key)%nodes.length;
+
+    }
+    public ListNode find(ListNode bucket,int key){
+        ListNode dummy = bucket,prev = null;
+        while (dummy!=null && dummy.key!=key){
+            prev = dummy;
+            dummy = dummy.next;
+        }
+        return prev;
     }
 
     public static void main(String[] args) {
