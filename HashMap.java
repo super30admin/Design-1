73f61b27741aa04d0c0974f98dbb88a8a9492a60
Sarang <saranggrover4488@gmail.com>
2019-07-07 22:33:31 -0400

    hashmap

--- HashMap.java
+++ HashMap.java
@@ -1,31 +1,73 @@
 public class HashMap {
-	boolean keyArray[] = new boolean[1000001];
-	int valueArray[] = new int[1000001];
+	ListNode[] nodes = new ListNode[10000];
 	
+	public class ListNode{
+		int key,value;
+		ListNode next;
+		
+		public ListNode(int key,int value) {
+			this.key=key;this.value=value;
+		}
+	}
 	
-	public void put(int key,int value) {
-		keyArray[key]= true;
-		valueArray[key]=value;
+	public void add (int key, int value) {
+		int i = index(key);
+		if(nodes[i]==null) {
+			nodes[i]= new ListNode(-1,-1);
+		}
+		
+		ListNode prev = find(nodes[i],key);
+		if(prev.next==null) {
+			prev.next= new ListNode(key,value);
+		}else {
+			prev.next.value=value;
+		}
+		
+		
+	}
+	
+	public int get (int key) {
+		int i = index(key);
+		if(nodes[i]==null) {
+			return -1;
+		}
+		ListNode prev = find(nodes[i],key);
+		return prev.next==null ? -1: prev.next.value;
 	}
 
-	public int get(int key) {
-		if(keyArray[key]==true) {
-			return valueArray[key];
-		}
-		return -1;
-			
+	public void remove(int key) {
+		int i = index(key);
+		if(nodes[i]==null) return;
+	    
+		ListNode prev = find(nodes[i],key);
+		if(prev.next==null) return;
+		
+		prev.next = prev.next.next;
 	}
 	
-	public void remove(int key) {
-		keyArray[key]=false;
+	int index(int key) {
+		return Integer.hashCode(key)%nodes.length;
+		
 	}
+	
+	ListNode find(ListNode bucket, int key) {
+		ListNode node = bucket;
+		ListNode prev = null;
+		while(node!=null && node.key!=key) {
+			prev = node;
+			node = node.next;
+		}
+		return prev;
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      HashMap obj = new HashMap();
-      obj.put(1, 20);
-      System.out.println(obj.get(1));
-      obj.remove(1);
-      System.out.println(obj.get(1));
+		 HashMap obj = new HashMap();
+	      obj.add(1, 20);
+	      System.out.println(obj.get(1));
+	      obj.remove(1);
+	      System.out.println(obj.get(1));
 	}
-
 }
+
