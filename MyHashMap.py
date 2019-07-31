379f1b23075c06b581af3af6773364b129a2f7fa
Mudita Bysani <muditabysani@muditas-mbp.lan>
2019-07-30 20:13:31 -0700

    Optimized HashMap and SpecialStack

--- MyHashMap.py
+++ MyHashMap.py
@@ -1,38 +1,66 @@
 class MyHashMap:
-    # Initialize your data structure here
-    def __init__(self):
-        self.keys = []
-        self.values = []
+	# Initialize your data structure here
+	# Here we can even fix the length of the horizontal linked list to 8 and then do the same thing
+	# then we have to implement rehashing and everything
+	class Node(object):
+		def __init__(self, key, val):
+			self.key = key
+			self.val = val
+			self.next = None
 
-    # value will always be non-negative
-    def put(self, key, value):
-        if key in self.keys:
-            self.values[self.keys.index(key)] = value
-        else:
-            self.keys.append(key)
-            self.values.append(value)
+	def __init__(self):
+		self.Hashlist = [None] * 10000
+	
+	# value will always be non-negative
+	def put(self, key, value):
+		i = self.find_index(key)
+		if self.Hashlist[i] == None:
+			self.Hashlist[i] = self.Node(-1, -1)
+		prev_node = self.find_node(self.Hashlist[i], key)
+		if prev_node.next == None:
+			prev_node.next = self.Node(key, value)
+		else:
+			prev_node.next.val = value
 
-    # Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-    def get(self, key):
-        if key in self.keys:
-            return self.values[self.keys.index(key)]
-        else:
-            return -1
+	# Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+	def get(self, key):
+		i = self.find_index(key)
+		if self.Hashlist[i] == None:
+			return -1
+		prev_node = self.find_node(self.Hashlist[i], key)
+		# why should we check for this condition again because node.next would never be none if self.Hashlist[i] is not none
+		# And the length of our horizontal list is either zero or minimum two nodes right
+		return -1 if prev_node.next == None else prev_node.next.val
 
-    # Removes the mapping of the specified value key if this map contains a mapping for the key
-    def remove(self, key):
-        if key in self.keys:
-            key_index = self.keys.index(key)
-            del self.keys[key_index]
-            del self.values[key_index]
+	# Removes the mapping of the specified value key if this map contains a mapping for the key
+	def remove(self, key):
+		i = self.find_index(key)
+		if self.Hashlist[i] == None:
+			return
+		prev_node = self.find_node(self.Hashlist[i], key)
+		if prev_node.next == None:
+			return None
+		prev_node.next = prev_node.next.next
+
+	def find_index(self, key):
+		return hash(key) % len(self.Hashlist)
+
+	def find_node(self, head, key):
+		cur = head
+		prev = None
+		while cur != None and cur.key != key:
+			prev = cur
+			cur = cur.next
+		return prev
 
 if __name__ == "__main__":
-    hashMap = MyHashMap()
-    hashMap.put(1, 1)
-    hashMap.put(2, 2)
-    print(hashMap.get(1))
-    print(hashMap.get(3))
-    hashMap.put(2, 1)
-    print(hashMap.get(2))
-    hashMap.remove(2)
-    print(hashMap.get(2))
+	hashMap = MyHashMap()
+	hashMap.put(1, 1)
+	hashMap.put(2, 2)
+	print(hashMap.get(1))
+	print(hashMap.get(3))
+	print(hashMap.get(2))
+	hashMap.put(2, 1)
+	print(hashMap.get(2))
+	hashMap.remove(2)
+	print(hashMap.get(2))
--- SpecialStack.py
+++ SpecialStack.py
@@ -2,9 +2,12 @@ import sys
 
 class SpecialStack:
 	SIZE = 100
+	# Two stack approach
 	def __init__(self):
 		self.stack = []
+		self.minstack = []
 		self.minval = sys.maxsize
+		self.minstack.append(self.minval)
 
 	def isEmpty(self):
 		if len(self.stack) == 0:
@@ -18,35 +21,45 @@ class SpecialStack:
 		else:
 			return False
 
-	def push(self, val):
+	def st_push(self, val):
 		if self.isFull():
 			print("The stack is full. So, can't add more values")
 		else:
 			if val < self.minval:
 				self.minval = val
+			self.minstack.append(self.minval)
 			self.stack.append(val)
 
-	def spop(self):
+	def st_pop(self):
 		if self.isEmpty():
 			print("Stack is empty")
-			return -1
 		else:
-			val = self.stack.pop()
-			self.minval = self.stack[0]
-			# Not an O(1) operation
-			for i in self.stack[1:]:
-				if i < self.minval:
-					self.minval = i
-			return val
+			self.stack.pop()
+			self.minstack.pop()
+			self.minval = self.minstack[-1]
+
+	def st_top(self):
+		return self.stack[-1]
 
 	def getMin(self):
 		return self.minval
 
 if __name__ == "__main__":
 	ss = SpecialStack()
-	ss.push(10)
-	ss.push(4)
-	ss.push(0)
-	print(ss.spop())
-	print(ss.stack)
+	ss.st_push(5)
+	ss.st_push(9)
+	print(ss.getMin())
+	ss.st_push(4)
+	print(ss.getMin())
+	ss.st_pop()
+	ss.st_push(12)
+	print(ss.getMin())
+	ss.st_pop()
+	ss.st_push(3)
+	print(ss.getMin())
+	ss.st_pop()
+	print(ss.getMin())
+	ss.st_pop()
+	print(ss.getMin())
+	ss.st_pop()
 	print(ss.getMin())
