d97c224777c25773f4dc5cd513f06077154e6545
Ajith Balaji <ajithbalajiun@gmail.com>
2019-08-20 17:46:42 -0700

    re wrote the codes in the same way as taught in the class

--- specialstack.java
+++ specialstack.java
@@ -1,47 +1,69 @@
 import java.util.Stack; 
   
 
-class SpecialStack extends Stack<Integer> 
-{ 
-    Stack<Integer> min = new Stack<>(); 
-      
-    
-    void push(int x) 
-    { 
-        if(isEmpty() == true) 
-        { 
-            super.push(x); 
-            min.push(x); 
-        } 
-        else
-        { 
-            super.push(x); 
-            int y = min.pop(); 
-            min.push(y); 
-            if(x < y) 
-                min.push(x); 
-            else
-                min.push(y); 
-        } 
-    } 
-  
-    /* SpecialStack's member method to insert an element to it. This method 
-    makes sure that the min stack is also updated with appropriate minimum 
-    values */
-    public Integer pop() 
-    { 
-        int x = super.pop(); 
-        min.pop(); 
-        return x; 
-    } 
-  
-    /* SpecialStack's member method to get minimum element from it. */
-    int getMin() 
-    { 
-        int x = min.pop(); 
-        min.push(x); 
-        return x;
+class MinStack{
+    Stack<Integer> min;
+    MinStack(){
+         min = new Stack<Integer>();
     }
-}
-//Special stack has been implemented using the stack data struction in java.
-//Time complexity:O(1).
+   int mini;
+   public void push(int x){
+        if(min.isEmpty()){
+            min.push(x);
+            mini=x;
+        }
+        else{
+            if(x>mini){
+                min.push(x);
+            }
+            else{
+                min.push(mini);
+                mini=x;
+                min.push(x);
+            }
+        }
+    }
+    public void pop(){
+        if(min.isEmpty()){
+            return ;
+        }
+        else{
+            if(min.peek()!=mini){
+                min.pop();
+            }
+            else{
+                min.pop();
+                if(!min.isEmpty()){
+                    mini=min.peek();
+                    min.pop();
+                }
+                else{
+                    mini=0;
+                }
+                
+            }
+        }
+       
+    }
+    public int top(){
+        if (min.isEmpty()){
+            return 0;
+        }
+        else{
+             return min.peek();
+        }
+       
+    }
+    public int getMin(){
+        if(min.isEmpty()){
+            return 0;
+        }
+        else{
+             return mini;
+        }
+       
+    }
+    }
+
+    // Min stack implemented using one single stack as taught in the class. Run time exceptions in this code has been handled by checking whether the stack runs empty or not during every peek(), pop() and getMin() operations.
+    // Time Complexity: O(1), Average Space Complexity: N.
