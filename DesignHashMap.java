7fac33bf15a7e0b2015f8eaae6db21229ad3ba56
Neha Mishra <neha79091@gmail.com>
2019-11-06 15:41:24 +0530

    CompletedDesign_1_with_minStackUsingTwoStacksAsWell

--- DesignHashMap.java
+++ DesignHashMap.java
@@ -1,7 +1,7 @@
 // Time Complexity :1
 // Space Complexity :O(n)
 // Did this code successfully run on Leetcode :Yes
-// Any problem you faced while coding this :
+// Any problem you faced while coding this :Null pointer exception
 //this is new commeng-------------------
 
 // Your code here along with comments explaining your approach
@@ -27,8 +27,7 @@ class MyHashMap {
 
     /** Initialize your data structure here. */
     public MyHashMap() {
-        //hashmap is created
-         HashMap<Integer,Integer> map1=new HashMap<>();
+       
         
     }
     
