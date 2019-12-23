e5e298aadebeae019691ed7dcb43262603eecacf
Nikhil Agrawal <nikrasiya@gmail.com>
2019-12-23 13:56:24 -0500

    Please review

---
+++ 155_min_stack.py
@@ -0,0 +1,67 @@
+class MinStack:
+    """
+    // Time Complexity :
+                O(n)
+    // Space Complexity :
+                O(n)
+    // Did this code successfully run on Leetcode :
+        Yes
+    // Any problem you faced while coding this :
+        Took to time to dice to store the min in tuple.
+        I was initially storing the min in a local variable and recalculating each time
+
+    // Your code here along with comments explaining your approach
+        We have a list of tuples. Where the first index is the value in the stacj
+        and the second index stores the current min value until that value in the stack.
+        When ever we insert a new value we compare with the last inserted values min value in
+        the tuple and insert the lowest.
+    """
+
+    def __init__(self):
+        """
+            Time Complexity: O(1)
+            Space Complexity: O(1)
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+            n is the number of elements in the list
+            Time Complexity: O(n)
+            Space Complexity: O(1)
+        """
+        if self.stack:
+            self.stack.append((x, min(x, self.getMin())))
+        else:
+            self.stack.append((x, x))
+
+    def pop(self) -> None:
+        """
+            Time Complexity: O(1)
+            Space Complexity: O(1)
+        """
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        """
+            Time Complexity: O(1)
+            Space Complexity: O(1)
+        """
+        if self.stack:
+            return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        """
+            Time Complexity: O(1)
+            Space Complexity: O(1)
+        """
+        if self.stack:
+            return self.stack[-1][1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
