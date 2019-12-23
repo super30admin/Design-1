b23a0850f931f70d1ed7d648ffe543867717b59d
Lohith <a.lohith@yahoo.in>
2019-12-23 17:55:13 -0600

    HashMap Implementation

---
+++ NewHashNode.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Node {
+    Node next;
+    int key;
+    int value;
+
+    public Node (int key, int value) {
+        this.key = key;
+        this.value = value;
+
+    }
+}
