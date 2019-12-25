3c984d9272ccecc37d03c79aca1a6f414e8bef27
Lohith <a.lohith@yahoo.in>
2019-12-24 22:08:35 -0600

    Design-1 problem redone

--- MinStack.java
+++ MinStack.java
@@ -1,19 +1,27 @@
 class MinStack {
 Stack<Integer> stack = new Stack<Integer>();
 Stack<Integer> min = new Stack<Integer>();
+int Minimum = Integer.MAX_VALUE;
+    
+    public MinStack(){
+        min.push(Minimum);
+    }
 
-public void push(int x) {
-    if(min.empty() || x <= min.peek())
-        min.push(x);
+    public void push(int x) {
     stack.push(x);
+    if(x <= Minimum){
+        Minimum = x;
+    }
+    min.push(Minimum);
+    
 }
 
 public void pop() {
-    if(!stack.empty()){
-        if((int)min.peek() == (int)stack.peek())
-            min.pop();
+    if(stack.empty() == false){
         stack.pop();
+        min.pop();
     }
+    Minimum = min.peek();
 }
 
 public int top() {
