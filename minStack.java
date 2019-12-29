093cdea6c428f8acce2c8dce23681f7e2d3df645
Sanjay Vishwakarma <svishwak@andrew.cmu.edu>
2019-12-28 19:46:59 -0800

    MinStack Solution Updated!

--- minStack.java
+++ minStack.java
@@ -1,43 +1,42 @@
 import java.util.Stack;
 
-public class minimumElementUsingOneStack {
-		/** initialize your data structure here. */
-		Stack<Integer> st = new Stack<>();
-		int min = Integer.MAX_VALUE;
-
-		public minimumElementUsingOneStack() {
-		}
-
-		public void push(int x) {
-			if (x <= min) {
-				st.push(min);
-				min = x;
-			}
-			st.push(x);
-		}
-		public void pop() {
-			if(st.pop() == min) {
-			min = st.pop();
-			}
-		}
-
-		public int top() {
-			return st.peek();
-		}
-
-		public int getMin() {
-			return min;
-		}
-
+class MinStack {
+    Stack<Integer> st = new Stack<>();
+    int min = Integer.MAX_VALUE;
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        if (x<=min){
+            st.push(min);
+            min = x;
+        }
+        st.push(x);    
+    }
+    
+    public void pop() {
+        if(st.pop() == min){
+            min=st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
 		public static void main(String args[]) {
-			MinimumElementUsingTwoStack minimumElementUsingOneStack = new MinimumElementUsingTwoStack();
-			minimumElementUsingOneStack.push(-2);
-			minimumElementUsingOneStack.push(0);
-			minimumElementUsingOneStack.push(-3);
-			minimumElementUsingOneStack.getMin(); // --> Returns -3.
-			minimumElementUsingOneStack.pop();
-			minimumElementUsingOneStack.top(); // --> Returns 0.
-			minimumElementUsingOneStack.getMin(); // --> Returns -2.
+			MinStack minStack = new MinStack();
+			minStack.push(-2);
+			minStack.push(0);
+			minStack.push(-3);
+			minStack.getMin(); // --> Returns -3.
+			minStack.pop();
+			minStack.top(); // --> Returns 0.
+			minStack.getMin(); // --> Returns -2.
 		}
 
 	}
