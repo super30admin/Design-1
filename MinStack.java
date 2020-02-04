03dfd1322ec7ff2c7b13a621140690c3483f9993
Singh Perry <sgagandeep232@gmail.com>
2020-02-03 22:13:34 -0800

    a2, passed all test cases, corrected my mistake

--- MinStack.java
+++ MinStack.java
@@ -7,48 +7,49 @@ Time Complexity- O(1)
 import java.util.* ; 
 
 class MinStack {
-
-    //stack to be used
-    private Stack<Integer> stack ; 
-    //to keep track of minimum value
-    private ArrayList<Integer> min ; 
-    private int minimum ; 
-    private int minIndex ; 
+        
+    //to keep track of min
+    List<Integer> minA ;
+    
+    //the actual stack 
+    Stack<Integer> stack ;
+    //index for minA
+    int minIndex ; 
+    //variable that contains minimum
+    int min ; 
     
     /** initialize your data structure here. */
     public MinStack() {
-        stack = new Stack<>() ; 
-        this.minimum = Integer.MAX_VALUE ;
-        this.min = new ArrayList<>() ;
-        this.minIndex = 0 ; 
+        stack = new Stack<Integer>() ; 
+        minA = new ArrayList<>(); 
+        minIndex = 0 ;
+        //initially, when stack in empty
+        min = Integer.MAX_VALUE ; 
+        minA.add(minIndex++, min) ; 
     }
     
     public void push(int x) {
-        stack.push(x); 
-        if (x < minimum) {
-            minimum = x ;
-            min.add(minIndex++, minimum); 
-        }
+        stack.push(x) ; 
+        //updating min
+        if (x < min)
+            min = x ; 
+        //adding min to minA
+        minA.add(minIndex++, min) ; 
     }
     
     public void pop() {
-        if (top() == minimum) {
-             stack.pop() ;
-            if (!stack.contains(minimum)) {
-            min.remove(--minIndex) ; 
-            minimum = min.get(--minIndex) ; 
-        }
-    } else
-        {
-            stack.pop() ; 
-        }
+        stack.pop() ; 
+        //we remove the min from minA
+        minA.remove(--minIndex); 
+        //we update the min to the last min in minA after pop
+        min = minA.get(minIndex - 1) ; 
     }
     
     public int top() {
-        return stack.peek() ; 
+      return  stack.peek() ; 
     }
     
     public int getMin() {
-        return minimum ; 
+        return min ;
     }
 }
