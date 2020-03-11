b558b32b3b3133dfdb73cf9ac88eead2a681107d
arcsoftech <arihant_Chhajed@yahoo.in>
2020-03-10 19:15:10 -0500

    Improved Hashtable

--- HashMap.java
+++ HashMap.java
@@ -1,16 +1,26 @@
-// Time Complexity : O(1)
-// Space Complexity : O(100000)=> O(k) wher is k is some constant.
+// Time Complexity : O(n)
+// Space Complexity : O(1000)=> O(k) where is k is some constant.
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : When I used array of size 1000 , I faced issue of collision.
-class HashMap { 
-    int max = 100000; // This is working in  leetcode because i belive there is no such test case which creates conflict with this.
-    int[] data;
-    
-    public MyHashMap() {
-        data = new int[max];
-        for(int i = 0;i<max;i++)
+// Any problem you faced while coding this : When I used array of size 1000 , I faced issue of collision. So I used linked list to avoid this collision.
+class HashMap {
+    class Node {
+        int key,value;
+        public Node(int key,int value)
         {
-            data[i] = -1;
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    List<List<Node>> hashTable;
+    int max = 1000;
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        hashTable = new ArrayList<>(max);
+        for(int i = 0 ;i<max;i++)
+        {
+            hashTable.add(new LinkedList<Node>());
         }
     }
     
@@ -22,18 +32,43 @@ class HashMap {
     /** value will always be non-negative. */
     public void put(int key, int value) {
         int index = getIndex(key);
-        data[index] = value;
+        for(Node n:hashTable.get(index))
+        {
+            if(n.key==key)
+            {
+                n.value = value;
+                return;
+            }
+        }
+        hashTable.get(index).add(new Node(key,value));
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
         int index = getIndex(key);
-        return data[index];
+        for(Node n:hashTable.get(index))
+        {
+            if(n.key==key)
+            {
+                return n.value;
+            }
+        }
+        return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
         int index = getIndex(key);
-        data[index] = -1;
+        Node el = null;
+        for(Node n:hashTable.get(index))
+        {
+            if(n.key==key)
+            {
+                el = n;
+                break;
+            }
+        }
+        
+        if (el != null) hashTable.get(index).remove(el);
     }
 }
