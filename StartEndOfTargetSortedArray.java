248664a03e4b0a51cfb034601f42efb7d37891cb
varusing <62032313+varusing@users.noreply.github.com>
2020-03-23 16:46:15 -0700

    Changes varusing

--- StartEndOfTargetSortedArray.java
+++ StartEndOfTargetSortedArray.java
@@ -1,35 +1,58 @@
-public int findMinInSortedArray(int[] nums) {
-    // find low and high
-    // find mid
-    // nums[low] < nums[high] return low
-    // nums[mid] < num[mid-1] && nums[mid] < nums[mid+1] return mid
-    // nums[mid] > nums[high] RHS ; nums[low] > nums[mid] LHS;
-    
-    
-    int low = 0;
-    int high = nums.length - 1;
-    
-    while (low < high){
-        int mid = low + (high - low)/2;
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
         
-        if (mid == 0 && nums[mid] < nums[mid + 1]){
-            return nums[mid];
-        } 
+        int left = leftBS(nums, target);
+        int right = rightBS(nums,target);
         
-        if (mid == nums.length - 1  || nums[mid] > nums[mid + 1] ){
-            return nums[mid + 1];
-        } 
+        return new int[]{left, right};
         
-        if (nums[mid] < nums[mid - 1]  && nums[mid] < nums[mid + 1] ){
-            return nums[mid];
-        } else if (nums[low] < nums[high]) {
-            return nums[low];
-        }
-        else if (nums[mid] > nums[high]){
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
     }
-    return nums[low];
+    
+    private int leftBS(int[] nums, int target ){
+        
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            
+            if (nums[mid] == target){
+                if (mid == low || nums[mid - 1] != target){
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else if (nums[mid] > target){
+                high = mid -1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int rightBS(int[] nums, int target ){
+        
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            
+            if (nums[mid] == target){
+                if (mid == high || nums[mid + 1] != target){
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] > target){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
 }
