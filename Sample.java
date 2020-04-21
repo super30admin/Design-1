3ac855f0874a8be52609922cce86cc3fbba4c25c
navnitss <43095390+navnitss@users.noreply.github.com>
2020-04-20 19:58:02 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -84,4 +84,52 @@ class MyHashMap {
     this.hash_table.get(hash_key).remove(key);
   }
 }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Time Complexity :O(1)
+// Space Complexity :O(N) 
+// Did this code successfully run on Leetcode :Yes
+class MinStack {
+	//Using a datastructure node that can store 2 values 
+    class Node{
+        int value;
+        int min;
+        Node next;
+        
+        Node(int x, int min){
+            this.value=x;
+            this.min=min;
+            next = null;
+        }
+    }
+    Node head;
+	//Every node that is added becomes the new head and the min from the previous node is compared with current value and stored as the latest min
+    public void push(int x) {
+        if(null==head){
+            head = new Node(x,x);
+        }else{
+            Node n = new Node(x, Math.min(x,head.min));
+            n.next=head;
+            head=n;
+        }
+    }
+
+    public void pop() {
+        if(head!=null)
+            head =head.next;
+    }
+
+    public int top() {
+        if(head!=null)
+            return head.value;
+        return -1;
+    }
+//Accessing the min of the head we can access the min of all the values that have been inserted in the "linked list" so far
+    public int getMin() {
+        if(head!=null)
+            return head.min;
+        return -1;
+    }
+}
+
+
 
