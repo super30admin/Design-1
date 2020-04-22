edf8d1dfe0ba38c440d835c4ec3401f66c8ef009
Lovenish Garg <60829596+techielove92@users.noreply.github.com>
2020-04-22 13:16:26 -0700

    Tree-3 completed. please review.

---
+++ Problem-2.py
@@ -0,0 +1,9 @@
+"""
+## Problem 2:
+Design MinStack (https://leetcode.com/problems/min-stack/)
+
+Design a Data Structure SpecialStack that supports all the stack operations like push(), pop(), isEmpty(), isFull() and an additional operation getMin() which should return minimum element from the SpecialStack. All these operations of SpecialStack must be O(1). To implement SpecialStack, you should only use standard Stack data structure and no other data structure like arrays, list, .. etc.
+
+
+"""
+
