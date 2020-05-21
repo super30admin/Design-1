272e7190e980c25ef876b4b871c5b84ea888e76c
Lovenish Garg <60829596+techielove92@users.noreply.github.com>
2020-05-21 10:37:10 -0700

    Design-1 completed. please review.

--- Problem-1.py
+++ Problem-1.py
@@ -11,8 +11,7 @@ put(key, value) : Insert a (key, value) pair into the HashMap. If the value alre
 get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
 remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
 
-Time-
-Space-
+
 """
 class MyHashMap:
 
