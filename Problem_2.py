cff15d1cb7052ff718df0cae86ef0c0a238cf03e
Osama Soliman <osama.soliman@sjsu.edu>
2020-05-25 10:32:54 -0700

    Design1 sprint complete

---
+++ Problem_2.py
@@ -0,0 +1,44 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.myStack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if len(self.myStack) ==0:
+            #if empty stack append the intiger and minumum is the intiger
+            self.myStack.append((x,x))
+        else:
+            # if not empty append the intiger and check are this number less than the last min and put min at this stage
+            self.myStack.append((x,min(x,self.myStack[-1][1])))
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if len(self.myStack) > 0:
+            self.myStack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.myStack) > 0:
+            return self.myStack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.myStack[-1][1]
+        
