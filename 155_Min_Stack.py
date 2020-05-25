17b3e17473d3fcc1193323c1169eaffd88911540
Suyash906 <suyash90682711@gmail.com>
2020-05-25 00:48:45 -0700

    comments added

--- 155_Min_Stack.py
+++ 155_Min_Stack.py
@@ -9,6 +9,11 @@
 # Did this code successfully run on Leetcode : Yes
 # 
 # Any problem you faced while coding this : No
+#
+# Problem Approach
+# 1. The min_stack stores the values only if it is the current minimum value or was previously a minimum value  in the stack
+# 2. The top of min_stack houses the minimum value in the stack
+# 3. The stack stores all the values pushed and is used to get the top of stack
 class MinStack:
 
     def __init__(self):
@@ -19,25 +24,31 @@ class MinStack:
         self.min_stack = []
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        if len(self.min_stack) == 0:
+        self.stack.append(x) # The value is pushed on to stack
+        if len(self.min_stack) == 0: # min_stack is empty, so the value pushed is the minimum value
             self.min_stack.append(x)
         else:
-            if self.min_stack[-1] >= x:
+            if self.min_stack[-1] >= x: # push the value to min_stack if it is less than its top
                 self.min_stack.append(x)
 
     def pop(self) -> None:
         x = self.stack.pop()
-        if len(self.min_stack) > 0 and self.min_stack[-1] == x:
+        if len(self.min_stack) > 0 and self.min_stack[-1] == x: # if the popped value is same as the minumum value, then we pop from min_stack also to restore the previous minimum value
             self.min_stack.pop()
 
     def top(self) -> int:
+        """
+        Return top of stack if it exist, otherwise return 0(Stack is empty)
+        """
         if self.stack:
             return self.stack[-1]
         return 0
         
 
     def getMin(self) -> int:
+        """
+        Return top of min_stack if it exist, otherwise return 0(Stack is empty)
+        """
         if self.min_stack:
             return self.min_stack[-1]
         return 0
--- 706_Design_HashMap.py
+++ 706_Design_HashMap.py
@@ -4,7 +4,7 @@
 #       O(n) [Worst case -> All keys inserted generate the same hash code] 
 #   get:
 #       O(1) [Best case -> The first element in the hash code linked list is the required element]
-#       O(n) [Worst case -> All keys inserted generate the same hash code. The first element in the hash code linked list is the required element]
+#       O(n) [Worst case -> All keys inserted generate the same hash code. The last element in the hash code linked list is the required element]
 #   remove: O(1)
 #       O(1) [Best case -> The first element in the hash code linked list is the required element]
 #       O(n) [Worst case -> All keys inserted generate the same hash code. The first element in the hash code linked list is the required element]
@@ -13,16 +13,13 @@
 # 
 # Did this code successfully run on Leetcode : Yes
 # 
-# Any problem you faced while coding this : No
+# Any problem you faced while coding this : No, but I am not sure if this is the optimal approach
 
 
 # Your code here along with comments explaining your approach
 # 1. Initialize a <hashmap> list with 10 NULL values. Use MOD 10 value of the input key to generate hash code[0-9]. The hash code generation function used is quite simple. In a real time scenario it should be such that the chances of collision are negligible.
 # 2. Each index of the <hashmap> hold a Linked list which containes nodes of type <HashNode>
-# 3. Approach for each faeture
-#   3.1 put() -> If the list at the hashcode index is N
-#   3.2 get() ->
-#   3.3 remove() -> 
+
 class HashNode:
     def __init__(self, key, value):
         self.key = key
@@ -39,31 +36,34 @@ class MyHashMap:
         
 
     def getHashCode(self, key):
+        """
+        It returns a value between 0 to 9
+        """
         return key % 10
     
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        hashcode = self.getHashCode(key)
-        if None == self.hashmap[hashcode]:
+        hashcode = self.getHashCode(key) 
+        if None == self.hashmap[hashcode]: # No collision case, so we directly insert the value
             self.hashmap[hashcode] = HashNode(key, value)
-        else:
+        else: # Collision, 
             p = self.hashmap[hashcode]
-            if p.next is None:
-                if p.key == key:
+            if p.next is None: # If there is only one HashNode in the linked list present at the key
+                if p.key == key: # Update on existing HashNode
                     p.value = value
-                else:
+                else: # insertion of new Node
                     p.next = HashNode(key, value)
                 return
             while p.next is not None:
-                if p.key == key:
+                if p.key == key:# Update on existing HashNode
                     p.value = value
                     return
                 p = p.next
-            if p.key == key:
+            if p.key == key: # Check if the very last node in the end of linked list has the same key, if yes the update the value
                 p.value = value
-            else:
+            else: # Insert new node at the end of list
                 p.next = HashNode(key, value)
 
     def get(self, key: int) -> int:
@@ -71,14 +71,14 @@ class MyHashMap:
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
         hashcode = self.getHashCode(key)
-        if self.hashmap[hashcode] is None:
+        if self.hashmap[hashcode] is None: # There is no linked list present at the hash code index, so return -1
             return -1
         p = self.hashmap[hashcode]
-        while p is not None:
+        while p is not None: # Iterate over the hash code list and check if the key matches, if it does return the value
             if p.key == key:
                 return p.value
             p = p.next
-        return -1
+        return -1 # The value is not found so return -1
          
 
     def remove(self, key: int) -> None:
@@ -86,15 +86,15 @@ class MyHashMap:
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
         hashcode = self.getHashCode(key)
-        if self.hashmap[hashcode] is not None:
+        if self.hashmap[hashcode] is not None: # Check if there is any hash node linked list at the hashnode index
             p = self.hashmap[hashcode]
-            if p.key == key:
+            if p.key == key: # The first Node is the required element
                 if p.next is None:
                     self.hashmap[hashcode] = None
                 else:
                     self.hashmap[hashcode] = p.next
                 return
-            else:
+            else: # Iterate over the list to locate the key and then delete it
                 while p.next is not None:
                     if p.next.key == key:
                         p.next = p.next.next
