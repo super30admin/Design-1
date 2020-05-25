26dcc968a5b497f927ed6fe994dbd4609b62546f
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-05-24 22:33:55 -0400

    Completed Sprint Design-1

---
+++ MinStack.java
@@ -0,0 +1,57 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Node> st;
+    int minEle;
+
+    class Node {
+        int value;
+        int min;
+
+        public Node(int value, int min) {
+            this.value = value;
+            this.min = min;
+        }
+    }
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        st = new Stack<Node>();
+    }
+
+    public void push(int x) {
+        Node newNode;
+        if (st.isEmpty()) newNode = new Node(x, x);
+        else {
+            newNode = new Node(x, Math.min(x, st.peek().min));
+        }
+        st.push(newNode);
+    }
+
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek().value;
+    }
+
+    public int getMin() {
+        return st.peek().min;
+    }
+
+    public static void main(String[] args) {
+        MinStack obj = new MinStack();
+        obj.push(2);
+        obj.pop();
+        int param_3 = obj.top();
+        int param_4 = obj.getMin();
+    }
+
+}
---
+++ MyHashMap.java
@@ -0,0 +1,45 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.Arrays;
+
+public class MyHashMap {
+    int arr[];
+
+    MyHashMap() {
+        arr = new int[1000001];
+        Arrays.fill(arr, -1);
+    }
+
+    public void put(int key, int value) {
+       arr[key] = value;
+    }
+
+    public int get(int key) {
+        return arr[key];
+    }
+
+    public void remove(int key) {
+        arr[key] = -1;
+    }
+
+    public static void main(String[] args) {
+        MyHashMap hashMap = new MyHashMap();
+
+        hashMap.put(1, 2);
+        hashMap.put(2, 3);
+
+        System.out.println(hashMap.get(1));
+        System.out.println(hashMap.get(3));
+
+        hashMap.put(2, 4);
+
+        System.out.println(hashMap.get(2));
+
+        hashMap.remove(2);
+        System.out.println(hashMap.get(2));
+    }
+
+}
