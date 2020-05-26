fb2910416882aa391cdac7c298d95d070bb6e0b7
chirag <cr.tagadiya@gmail.com>
2020-05-26 13:33:51 -0400

    MIN_STACK USING TWO STACK

---
+++ Min_Stack.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> in;
+    Stack<Integer> out;
+    int min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        in = new Stack();
+        out =  new Stack();
+        min = Integer.MAX_VALUE;
+        out.push(min);
+    }
+    
+    public void push(int x) {
+        min = Math.min(x,min);
+        in.push(x);
+        out.push(min);
+    }
+    
+    public void pop() {
+        in.pop();
+        out.pop();
+        min  = out.peek();
+    }
+    
+    public int top() {
+        return in.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
