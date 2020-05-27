716affd1b561d5e87815b543ee889bd8b42b1ab5
Shashank Baghel <41822011+Shashank001122@users.noreply.github.com>
2020-05-26 18:56:10 -0700

    Updated please check
    I've implemented double hashing but it runs only faster than 19.7% why is too slow on LeetCode?

--- HashMap.py
+++ HashMap.py
@@ -1,60 +1,45 @@
+import math
 class MyHashMap:
-
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.arr=[]
+        self.arr = [None for x in range(1000)]
         
-
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        flag=0
-        for i in self.arr:
-            #update
-            if i[0]==key:
-                flag=1
-                i[1]=value
-        if flag==0:
-            self.arr.append([key,value])
-                
+        ##Complexity of put is O(1)
+        if(self.arr[key%1000]==None):
+            self.arr[key%1000]=[None for x in range(1000)]
+        self.arr[key%1000][key//1000]=value
         
-        
-
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        flag=0
-        for i in self.arr:
-            if i[0]==key:
-                flag=1
-                return i[1]
-        if flag==0:
+        ##Complexity of put is O(1)
+        if self.arr[key%1000]!= None:
+            if self.arr[key%1000][key//1000]!=None:
+                return self.arr[key%1000][key//1000]
+            else:
+                return -1
+        else:
             return -1
-        
         
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        indexkey=-1
-        for i in range(len(self.arr)):
-            if self.arr[i][0]==key:
-                indexkey=i
-        if indexkey!=-1:
-            del self.arr[indexkey]
-
+        ##Complexity of put is O(1)
+        if self.arr[key%1000]:
+            if self.arr[key%1000][key//1000]!=None:
+                self.arr[key%1000][key//1000]=None
         
 # Your MyHashMap object will be instantiated and called as such:
-obj = MyHashMap()
-obj.put(1,1)
-obj.put(2,2)
-print(obj.get(1))
-print(obj.get(3))
-obj.put(2,1)
-obj.remove(2)
-print(obj.arr)
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
