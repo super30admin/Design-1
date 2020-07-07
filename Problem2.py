69a116cd3b4636585a0a241e90460189cf745f6f
hackerrank <garima.adlakha2450@gmail.com>
2020-07-07 00:34:28 -0700

    Completed Problem-2 Ready for review

---
+++ Problem2.py
@@ -0,0 +1,49 @@
+class MinStack:
+
+	def __init__(self):
+		"""
+		initialize your data structure here.
+		"""
+		self.stack=[]
+		
+
+	def push(self, x: int) -> None:
+		cur_min=self.getMin()
+		if cur_min==None or x<cur_min:
+			cur_min=x
+		self.stack.append((x,cur_min))
+		
+
+	def pop(self) -> None:
+		self.stack.pop()
+		
+
+	def top(self) -> int:
+		if not self.stack:
+			return None
+		
+		return self.stack[-1][0]
+		
+		
+
+	def getMin(self) -> int:
+		if not self.stack:
+			return None
+		#stack[-1][1] means last element (-1) and (1) here is for the minimum element
+		return self.stack[-1][1]
+	#(-2,-2),(0,-2),(-3,-3)
+		
+		
+
+
+#Your MinStack object will be instantiated and called as such:
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+obj.pop()
+
+param_3 = obj.top()
+print("Top element is",param_3)
+param_4 = obj.getMin()
+print("minimum element is",param_4)
