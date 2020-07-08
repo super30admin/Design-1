12594f29581bbfddc0d9c41720e818e34f001525
Arpit Sharma <arpit317@gmail.com>
2020-07-08 02:10:26 -0700

    Update min-stack-single-stack.py

--- min-stack-single-stack.py
+++ min-stack-single-stack.py
@@ -1,3 +1,8 @@
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
+
 class MinStack(object):
     def __init__(self):
         """
