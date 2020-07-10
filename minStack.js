2346f7c368c79b15f804ce74d43da9454b45f6ed
Siyami Avci <siyamiavci@gmail.com>
2020-07-09 20:48:29 -0700

    change minStack

--- minStack.js
+++ minStack.js
@@ -1,7 +1,6 @@
 // Time Complexity : O(1)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this :
 
 class MinStack {
     stack = [];
