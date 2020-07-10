11f8912df43a54382af05a8a23229de45d104be3
prax3317 <prateeksingh.1510@gmail.com>
2020-07-09 18:02:32 -0700

    Completed Design 1class MyHashMap {

--- Sample.java
+++ Sample.java
@@ -1,7 +1,75 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class MyHashMap {
+    ListNode[] nodes = new ListNode[10000];
 
+    /** Initialize your data structure here. */
+    public MyHashMap() {
 
-// Your code here along with comments explaining your approach
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int index = getIndex(key);
+        ListNode prev = findElement(index, key);
+        if (prev.next == null) {
+            prev.next = new ListNode(key, value);
+        } else {
+            prev.next.val = value;
+        }
+
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or -1 if this map
+     * contains no mapping for the key
+     */
+    public int get(int key) {
+        int index=getIndex(key);
+        ListNode prev =findElement(index, key);
+        return prev.next==null?-1:prev.next.val;
+
+    }
+
+    /**
+     * Removes the mapping of the specified value key if this map contains a mapping
+     * for the key
+     */
+    public void remove(int key) {
+        int index = getIndex(key);
+        ListNode prev = findElement(index, key);
+        if (prev.next != null) {
+        prev.next=prev.next.next;
+        }
+
+    }
+
+    private int getIndex(int key) {
+        return Integer.hashCode(key)%10000;
+    }
+
+    private ListNode findElement(int index, int key) {
+        if (nodes[index] == null)
+            return nodes[index] = new ListNode(-1, -1);
+        ListNode prev = nodes[index];
+        while (prev.next != null && prev.next.key != key) {
+            prev = prev.next;
+        }
+        return prev;
+    }
+
+    private static class ListNode {
+        int key;
+        int val;
+        ListNode next;
+
+        ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such: MyHashMap obj
+ * = new MyHashMap(); obj.put(key,value); int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
