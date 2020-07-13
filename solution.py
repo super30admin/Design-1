d6353a594d6919421aa8b046d0bbb08cd0ce2019
jalee1 <ji.lee@silabs.com>
2020-07-13 08:24:20 -0700

    completed

---
+++ solution.py
@@ -0,0 +1,9 @@
+# // Time Complexity :
+# // Space Complexity :
+# // Did this code successfully run on Leetcode :
+# // Any problem you faced while coding this :
+#
+#
+# // Your code here along with comments explaining your approach
+# Problem 1:
+# Design Hashmap (https://leetcode.com/problems/design-hashmap/)
