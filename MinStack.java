532ee6a395b54fd3ba6fc9e2732323c59bf36a04
bhanuk97 <43115610+bhanuk97@users.noreply.github.com>
2020-08-16 17:31:29 -0400

    Completed Design1.

--- MinStack.java
+++ MinStack.java
@@ -1,7 +1,7 @@
 // Time Complexity : O(1)
 // Space Complexity : O(n)
-// Did this code successfully run on Leetcode : No
-// Any problem you faced while coding this : yes
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
