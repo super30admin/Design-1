169db8f1ce3c3e8b6c610590f8bc8372d9492d95
vatsaashwin <aashwinvats@GMAIL.com>
2020-08-18 10:10:44 -0700

    Please review minStack problem

---
+++ minStack.py
@@ -0,0 +1,35 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = float('inf')
+
+    def push(self, x: int) -> None:
+        if x <= self.min:
+            self.stack.append(self.min)
+            self.min = x
+        self.stack.append(x)
+
+    def pop(self) -> None:
+        popped = self.stack.pop()
+        if (popped == self.min):
+            self.min = self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.min
+        
+# Test code
+minStack = MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+print(minStack.getMin()); #return -3
+minStack.pop();
+print(minStack.top());    #return 0
+print(minStack.getMin()); #return -2
