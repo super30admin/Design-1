d9afada5b1d22608d996f266f89de613eb07f0b4
Mettu <reachmettu@gmail.com>
2020-08-17 22:13:35 -0400

    completed minstack

--- MinStack.java
+++ MinStack.java
@@ -1,5 +1,8 @@
 
-
+// Time Complexity :O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
 
 // Your code here along with comments explaining your approach
 class MinStack {
