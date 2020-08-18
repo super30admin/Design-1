e4d5e2bc20662c26703819d9a47689d2a117d602
Rimzim Thube <rimzimthube@Rimzims-MBP.local>
2020-08-18 12:05:52 -0700

    Updated with new approach

--- splstack.py
+++ splstack.py
@@ -1,34 +1,68 @@
 # Time Complexity : 0(1)
-# Space Complexity : 0(N) N=no. of elements in the stack
+# Space Complexity : 0(2N) N=no. of elements in the stack
 # Did this code successfully run on Leetcode : Yes. 
 # Any problem you faced while coding this : Nothing specific. 
 
 
 # Your code here along with comments explaining your approach
 
-class MinStack:
+class MinStack(object):
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.arr=[]
         
-    def push(self, x: int) -> None:
+        self.stack=[]
+        self.min=float('inf')
         
-        self.arr.append(x)
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
         
-    def pop(self) -> None:
+        if x<=self.min:
+            self.stack.append(self.min)
+            self.min=x
         
-        return self.arr.pop(-1)
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        element=self.stack.pop(-1)
+        if self.min==element:
+            
+            self.min=self.stack.pop(-1)
         
-    def top(self) -> int:
+        return element
         
-        return self.arr[-1]
+    def top(self):
+        """
+        :rtype: int
+        """
         
-    def getMin(self) -> int:
+        return self.stack[-1]
         
-        return min(self.arr)
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        
+        return self.min
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
         
 
 
