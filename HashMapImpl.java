95adf9c61cfffe5c6689272b94568b085752d6ae
Sindhu <sindhusha.vf12@gmail.com>
2020-08-19 00:51:40 -0500

    Resubmitted HashMap Design

--- HashMapImpl.java
+++ HashMapImpl.java
@@ -1,47 +1,80 @@
-// Time Complexity : O(n)
-// Space Complexity : O(1) (constant size array)
+// Time Complexity : O(1) Average case, O(n) worst case for put,get,remove
+// Space Complexity : O(n) where n is number of entries in HashMap
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : Nothing specific
 
+//Implementing LinkedList chaining method solution discussed in the class
 
-public class HashMapImpl {
-    static class MyHashMap {
+class MyHashMap {
 
     /** Initialize your data structure here. */
-    int[] map=new int[1000001];                //initialized array ds with the limit given in the problem
-    public MyHashMap() {
-        for (int i=0;i<map.length;i++){        //assigned all the values as -1 , the value to be returned if the key doesnt exist
-            map[i]= -1;
-        }  
+   
+    class ListNode{
+        int key;
+        int value;
+        ListNode next;
+        ListNode(int key, int value){
+            this.key=key;
+            this.value=value;
+        }
     }
+    private ListNode[] map;
+    public MyHashMap() {
+        map=new ListNode[100];
+        }
+   private int hashfunc(int key){
+       return key%map.length;
+   } 
+     ListNode findElem(int index,int key){
+         
+         if(map[index]==null){
+              map[index]=new ListNode(-1,-1);
+             return map[index];
+         }
+         ListNode curr=map[index];
+         while(curr.next!=null && curr.next.key!=key){
+             curr=curr.next;
+         }
+         return curr;
+         
+     }
+    
+    
     /** value will always be non-negative. */
     public void put(int key, int value) {
-        map[key]=value;                        //implementing put method by assigning the value to the key index in array
+      int index=hashfunc(key);
+    ListNode prev = findElem(index,key);
+        if(prev.next==null){
+            prev.next=new ListNode(key,value);
+        }
+        else{
+            prev.next.value=value;
+        }
+        
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return map[key];                       //implemented get method by returning the value at the index(key) of the array
+       int index=hashfunc(key);
+    ListNode prev = findElem(index,key);
+        if(prev.next==null){
+           return -1;
+        }
+        else{
+            return prev.next.value;
+        }
     }
+    
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        map[key]=-1;                          // making the value back to -1 as per requirement to remove
+        int index=hashfunc(key);
+    ListNode prev = findElem(index,key);
+        if(prev.next!=null){
+            prev.next=prev.next.next;
+        }
+        
     }
 }
-
-public static void main(String[] args) {
-    MyHashMap hashMap = new MyHashMap();
-hashMap.put(1, 2);          
-hashMap.put(2, 3);         
-System.out.println(hashMap.get(1));            // returns 2
-hashMap.get(3);            // returns -1 (not found)
-hashMap.put(2, 4);          // update the existing value
-hashMap.get(2);            // returns 4 
-hashMap.remove(2);          // remove the mapping for 2
-hashMap.get(2);            // returns -1 (not found)
-}
-}
-
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
