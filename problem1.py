cdc0b92076dce3616aad7af505d736cb1786794b
Vishal kumar M G <vishalkumarmg@Vishals-MacBook-Pro.local>
2020-08-23 15:51:37 -0500

    Updated the code with necessary changes - Optimal solutions

--- problem1.py
+++ problem1.py
@@ -1,24 +1,9 @@
-# # Design Hashmap
 # 706. Design HashMap
-# Design a HashMap without using any built-in hash table libraries.
 
-# To be specific, your design should include these functions:
-
-# put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
-# get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
-# remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
-
-# Example:
-
-# MyHashMap hashMap = new MyHashMap();
-# hashMap.put(1, 1);          
-# hashMap.put(2, 2);         
-# hashMap.get(1);            // returns 1
-# hashMap.get(3);            // returns -1 (not found)
-# hashMap.put(2, 1);          // update the existing value
-# hashMap.get(2);            // returns 1 
-# hashMap.remove(2);          // remove the mapping for 2
-# hashMap.get(2);            // returns -1 (not found) 
+class ListNode:
+    def __init__(self, key, val):
+        self.pair = (key, val)
+        self.next = None
 
 class MyHashMap:
 
@@ -26,39 +11,73 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        self.res = {}
+        self.size = 1000;
+        self.hashmap = [None]*self.size
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        self.res[key]= value
-        print("HashMap current key:value :", self.res) 
-
+        index = key % self.size
         
+        if self.hashmap[index] == None:
+            self.hashmap[index] = ListNode(key, value)
+        else:
+            curr = self.hashmap[index]
+            while True:
+                if curr.pair[0] == key:
+                    curr.pair = (key, value) #update
+                    return
+                if curr.next == None: 
+                    break
+                curr = curr.next
+            curr.next = ListNode(key, value)
+        
+
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        if key in self.res:
-            return self.res[key]
+        index = key%self.size
+        
+        curr = self.hashmap[index]
+        
+        while curr:
+            if curr.pair[0]==key:
+                return curr.pair[1]
+            else:
+                curr = curr.next
         return -1
+        
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        if key in self.res:
-            del self.res[key]
-        print("HashMap current key:value :", self.res)
+        index = key%self.size
+        curr = prev= self.hashmap[index]
+        
+        if not curr:
+            return
+        if curr.pair[0] == key:
+            self.hashmap[index] = curr.next
+        else:
+            curr= curr.next
+            while curr:
+                if curr.pair[0]==key:
+                    prev.next = curr.next
+                    break
+                else:
+                    curr, prev= curr.next, prev.next
 
-hashMap = MyHashMap()
-hashMap.put(1, 1)
-hashMap.put(2, 2)
-print(hashMap.get(1))
-print(hashMap.get(3))
-hashMap.put(2, 1)
-print(hashMap.get(2))
-hashMap.remove(2)
-print(hashMap.get(2))
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
--- problem2.py
+++ problem2.py
@@ -1,59 +1,38 @@
 # Design MinStack 
 
-# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
-# push(x) -- Push element x onto stack.
-# pop() -- Removes the element on top of the stack.
-# top() -- Get the top element.
-# getMin() -- Retrieve the minimum element in the stack.
-
-# Input
-# ["MinStack","push","push","push","getMin","pop","top","getMin"]
-# [[],[-2],[0],[-3],[],[],[],[]]
-
-# Output
-# [null,null,null,null,-3,null,0,-2]
-
-# Explanation
-# MinStack minStack = new MinStack();
-# minStack.push(-2);
-# minStack.push(0);
-# minStack.push(-3);
-# minStack.getMin(); // return -3
-# minStack.pop();
-# minStack.top();    // return 0
-# minStack.getMin(); // return -2
-
 class MinStack:
 
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.res = []
+        self.stack1 = []
+        self.min = float('inf')
         
 
     def push(self, x: int) -> None:
-        self.res.append(x)
-        print("Values in the stack : ", self.res)
+        if x<=self.min:
+            self.stack1.append(self.min)
+            self.min = x
+        self.stack1.append(x)
         
 
     def pop(self) -> None:
-        self.res.pop()
-        print("Values in the stack : ", self.res)
-
+        x = self.stack1.pop()
+        if x==self.min:
+            self.min = self.stack1.pop()
+        
 
     def top(self) -> int:
-        if len(self.res)>0:
-            return self.res[-1]
-        return -1
-        
+        return self.stack1[-1]
 
     def getMin(self) -> int:
-        if len(self.res)>0:
-            return sorted(self.res)[0]
-        return -1
+        return self.min
         
-
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
@@ -62,11 +41,3 @@ class MinStack:
 # param_3 = obj.top()
 # param_4 = obj.getMin()
 
-obj = MinStack()
-obj.push(-2)
-obj.push(0)
-obj.push(-3)
-print(obj.getMin())
-obj.pop()
-print(obj.top())
-print(obj.getMin())
