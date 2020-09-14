76e0ca7bd379e0a1a31226406fc209b213897826
akshaykajale <aksh.kajale@gmail.com>
2020-09-14 12:17:28 -0700

    Designed Hasgmap using Array

--- designHaspMap.java
+++ designHaspMap.java
@@ -1,7 +1,7 @@
 // Time Complexity : O(n)
 // Space Complexity :O(n)
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
