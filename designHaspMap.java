29caa7aac6ea465d17ae61555cb4ecd290e3bbe7
akshaykajale <aksh.kajale@gmail.com>
2020-09-14 13:39:26 -0700

    Completed Design-1

--- designHaspMap.java
+++ designHaspMap.java
@@ -3,7 +3,6 @@
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
-
 // Your code here along with comments explaining your approach
 class MyHashMap {
 
