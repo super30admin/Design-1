276c5e5d786390fd1e459004b40cc132c2fd5ed7
Sowmya246 <sowmyasonu46@gmail.com>
2020-09-14 16:48:17 -0700

    Design-1 Completed

--- hashmap.py
+++ hashmap.py
@@ -1,3 +1,9 @@
+// Time Complexity :O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+
 class MyHashMap:
 
     def __init__(self):
