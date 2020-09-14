ceb1f9b8442184fb6e9d5a77f2fd068481378b18
Work Account <workaccount@Vanis-MacBook-Pro.local>
2020-09-14 16:34:32 -0700

    commit -m "code completed"

---
+++ minstack.java
@@ -0,0 +1,33 @@
+// Time Complexity :O(1) 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+class MinStack {
+    
+    int min=Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        if(x <= min){
+           stack.push(min);
+           min=x; 
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.pop()==min) min=stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
