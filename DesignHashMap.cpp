3cbb3c6748d97318029c07e814cf0008aede18e2
sujay sudarshan gijre <sujay@sujays-MacBook-Pro.local>
2020-09-14 20:12:51 -0500

    Solutions for Design 1

--- DesignHashMap.cpp
+++ DesignHashMap.cpp
@@ -1,37 +1,37 @@
 class MinStack {
 public:
     /** initialize your data structure here. */
-    int topValue;
-    vector<int> minimumValue,stk;
+    int topValue; //top value will top to the top element 
+    vector<int> minimumValue,stk; //2 vector initialized one for normal coding and another for keeping the min value
     
     MinStack() 
     {
-        topValue=-1;
+        topValue=-1; //initiallize the value to -1
     }
     
     void push(int x) 
     {
-        topValue++;
-         stk.push_back(x); 
-        if (topValue==0) {
-            minimumValue.push_back(x);
+        topValue++; //while pushing the value increment to -1
+         stk.push_back(x);  // push the value to the normal stack
+        if (topValue==0) { // this means it is the first insertion
+            minimumValue.push_back(x); 
         } else {
-            minimumValue.push_back(min(minimumValue[topValue-1],x));
+            minimumValue.push_back(min(minimumValue[topValue-1],x)); //compare the minimum value in the top of the stack and the current value
         }
     }
     
     void pop() 
     {
-        if(topValue==-1)
+        if(topValue==-1) // empty stack condition
             return;
-        minimumValue.pop_back();
+        minimumValue.pop_back(); 
         stk.pop_back();
         topValue--;
     }
     
     int top() 
     {
-        return stk.back();
+        return stk.back(); 
     }
     
     int getMin() {
