2bdec45c160a69dc3a54d8953ec80fc9e8196f00
siddhesh25 <siddheshyadav1@gmail.com>
2020-09-16 22:09:50 -0400

    Create Problem2.py

---
+++ Problem2.py
@@ -0,0 +1,34 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        return self.stack.append(x)        
+
+    def pop(self) -> None:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
