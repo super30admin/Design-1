8e36663a3a341fe2889052e7150397303bf47db8
Pratik Dhumal <pratik.dhumal@netapp.com>
2020-10-12 22:37:51 -0700

    min stack

---
+++ MinStack.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    int min;
+    public MinStack() {
+        stack=new <Integer>Stack();
+        min=Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
