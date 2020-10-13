0b2dba6527603411f36473717c6ffa7d307350f6
Pratik Dhumal <pratik.dhumal@netapp.com>
2020-10-12 22:39:16 -0700

    Design 1,2 submission

--- MyHashMap.java
+++ MyHashMap.java
@@ -5,22 +5,6 @@
 
 
 // Your code here along with comments explaining your approach
-class Sample
-{
-        public static void main(String[] args)
-        {
-            MyHashMap hashMap = new MyHashMap();
-            hashMap.put(1, 2);
-            hashMap.put(2, 3);
-            System.out.println(hashMap.get(1));            // returns 2
-            System.out.println(hashMap.get(3));            // returns -1 (not found)
-            hashMap.put(2, 4);          // update the existing value
-            System.out.println(hashMap.get(2));            // returns 4
-            hashMap.remove(2);          // remove the mapping for 2
-            System.out.println(hashMap.get(2));            // returns -1 (not found)
-
-        }
-}
 
 class MyHashMap
 {
