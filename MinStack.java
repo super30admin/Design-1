115edde5dc323b07425fcc0eb0c4426bab54ab4c
bhumikatiwari16 <bhumikatiwari1603@gmail.com>
2020-10-13 18:05:05 -0700

    Reworked on both problems

--- MinStack.java
+++ MinStack.java
@@ -5,9 +5,9 @@ import java.util.Stack;
 class MinStack {
 
     // Time Complexity : O(1)
-    // All functions will have time complexity O(1) because they use stack DS for which push, pop and peek functions can be performed in O(1) time.
+    // All functions will have time complexity O(1) because push, peek functions can be performed in O(1) time from stack s1. Pop function can be performed in O(1) time from stack S1 and S2 both. Getmin function can be performed in O(1) time from stack S2.
     // Space Complexity : O(n)
-    // Since we are using two stacks to implement the given data structure, so space complexity will be O(n).
+    // Stack s1 and s2 will hold all n elements in worst case scenarios, when every incoming element is lesser than all the previos values.
     
     // Did this code successfully run on Leetcode : Yes
     // Any problem you faced while coding this : No
@@ -16,57 +16,53 @@ class MinStack {
     /** initialize your data structure here. */
     
     // Two stacks used for implementing min stack ds
-    // Stack st stores all elements present in min stack ds
-    // Stack minStTracker stores the least element inserted in min stack ds at a point of time
-    Stack<Integer> st;
-    Stack<Integer> minStTracker; 
+    // Stack s1 stores all elements present in min stack ds
+    // Stack s2 stores the least element inserted in min stack ds at a point of time
+    // Stacl s1 and s2 elements have a one to one mapping
+    Stack<Integer> s1;
+    Stack<Integer> s2; 
      
     // constructor to initialize min stack ds 
     // here, both stacks initialized 
+    // Max value pushed to stack s2 to enable Math.min operation for next incoming element.
     public MinStack() {
-        st = new Stack<>();
-        minStTracker = new Stack<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+        s2.push(Integer.MAX_VALUE);
     }
     
-    // Push an element to st everytime push method is called
-    // Push an element to minStTracker only when :
-    // a) minStTracker is empty
-    // b) incoming elem is lesser than the one at the top of minStTracker
+    // Push an element to s1 everytime push method is called.
+    // Compare top value in s2 to incoming elem and push lesser of those values to s2.
     public void push(int x) {
-        st.push(x);
-        if (minStTracker.isEmpty() || minStTracker.peek() >= x) {
-            minStTracker.push(x);
-        }
+        s1.push(x);
+        s2.push(Math.min(s2.peek(), x));
     }
     
-    // Remove topmost element from st everytime pop method is called
-    // Remove topmost element from minStTracker only when:
-    // Removed element from st is same as the topmost element in minStTracker
+    // Remove topmost element from s1 everytime pop method is called
+    // Remove topmost element from s2 also since one to one mapping is maintained between both the stacks
     public void pop() {
-        if (st.isEmpty()) {
+        if (s1.isEmpty()) {
             System.out.println("Min Stack is empty. Cannot pop an element.");
             return;
         }
-        int temp = st.pop();
-        if (minStTracker.peek() == temp) {
-            minStTracker.pop();
-        }
+        s1.pop();
+        s2.pop();
     }
     
-    // Topmost element of min stack ds is topmost element in st 
+    // Topmost element of min stack ds is topmost element in s1 
     public int top() {
-        if (st.isEmpty()) {
+        if (s1.isEmpty()) {
             System.out.println("Min Stack is empty. Cannot retrieve top element.");
         }
-        return st.peek();
+        return s1.peek();
     }
     
-     // Minimum element of min stack ds is topmost element in minStTracker 
+     // Minimum element of min stack ds is topmost element in s2 
     public int getMin() {
-        if (minStTracker.isEmpty()) {
+        if (s2.isEmpty()) {
             System.out.println("Min Stack is empty. Cannot retrieve minimum element.");
         }
-        return minStTracker.peek();
+        return s2.peek();
     }
 }
 
--- MyHashMap.java
+++ MyHashMap.java
@@ -2,163 +2,97 @@ package Day1;
 
 class MyHashMap {
     
-   // Time Complexity : O(n)
-   // Assuming if all the n elements have the same hashcode, they will all be part of a linked list of n size, for which time complexity for insertion, deletion and retrieval is O(n) in worst case scenarios.
-   // If there are no collisions in the map, then time complexity should be O(1) since we can directly access the array elements using their indexes.
+    // Time Complexity: O(1)
+    // All operations involved array(1-d/2-d) put, get or remove which has O(1) complexity.
+    
+    // Space Complexity: O(n) 
+    //  Worst case scenario when all 1000000 elements are initialized in the 2-d boolean storage array.
    
-   // Space Complexity : O(n)
-   // If all elements inserted using put operation have the same hashcode, they will all be placed at a common index in the fixed size array. This linked list will have the same size as the no.of insertions done, hence space complexity will be O(n).
-  
-   // Did this code successfully run on Leetcode : No
-   
-   // Any problem you faced while coding this : 
-   // 1) For very large inputs, expected output is not matching the output of the code. For normal inputs, it is working as expected. 
-   // 2) Unable to implement double hashing
-   
-   
-   // Implementing Hashmap using Array 
-    public Node[] map;
-   
-   /** Initialize your data structure here. */
-   public MyHashMap() {
-      map = new Node[10000];
-   }
-   
-   /** value will always be non-negative. */
-   
-   
-   public void put(int key, int value) {
-     
-       Node newNode = new Node(key,value);
-       
-       // get hashcode of the new object
-       // use this hashcode as index in 'map' array
-       int idx = hashIndex(key);
-       
-       // if 'map' array does not have an element at idx position
-       if(map[idx] == null){
-           map[idx] = newNode;
-       } 
-       
-       // if 'map' array has an element at idx position, then iterate through the linked list         // a) if no key in linked list matches inputted key, insert the new node at the end of the list  
-      // b)  if key in linked list matches inputted key, replace the node with new node
-       else {
-           
-           Node current = map[idx];
-           Node pre = null;
-           while(current != null){
-               if(current.equals(newNode)){
-                  
-               	if(pre == null){
-                     map[idx] = newNode;
-                     return;
-                   }
-                  pre.next = newNode;
-                   newNode.next = current.next;
-                   return;
-               }
-               pre = current;
-               current = current.next;
-           }
-           pre.next = newNode;
-           
-       }      
-   }
-   
-   /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-   // 
-   public int get(int key) {
+    // Did this code successfully run on Leetcode : Yes
+    
+    // Any problem you faced while coding this : No
+    
+     int buckets;
+     int bucketSize;
+     int[][] storage; 
 
-       // get hashcode of the object to be retrieved
-       // use this hashcode as index in 'map' array
-       int idx = hashIndex(key);
-     
-       // return -1 if the 'map' array does not have an element at idx position
-       if(map[idx] == null)
-           return -1;
-      
-       // Traverse through the linked list at idx position and compare key values of nodes in linked list with the function argument
-       // If any node's key value matches, then return that node's value
-       // else keep iterating till the end of linked list
-       Node current = map[idx];
-     
-       while(current != null){
-           if(current.key == key){
-               return current.value;
-           }
-           current = current.next;
-       }
-       
-      return -1;
-   }
-   
-   /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-   
-   public void remove(int key) {
-     
-       // get hashcode of the object to be retrieved
-       // use this hashcode as index in 'map' array
-        int idx = hashIndex(key);
-       
-       // return -1 if the 'map' array does not have an element at idx position
-        if(map[idx] == null)
-               return;
-       
-       // Traverse through the linked list at idx position and compare key values of nodes in linked list with the function argument
-       // If any node's key value matches, then delete that node by resetting next pointer using prev node
-       // else keep iterating till the end of linked list
-         Node current = map[idx];
-         Node pre = null;
-       
-         while(current != null) {
-           if(current.key == key) {
-               if(pre == null) {
-                     map[idx] = null;
-                   return;
-               } 
-               pre.next = current.next;
-               return;
-           }
-           pre = current;
-           current = current.next;
-         }      
-   }
-   
-   // function to return hashcode for a numeric input
-   public int hashIndex(int num){
-       return Integer.hashCode(num) % 10000;      
-   }
+    
+    /** Initialize your data structure here. */
+    // Initialized a 2d array of colsize = underroot of permissible Max value, i.e. 1000000.
+    // rowsize declared in a variable but yet not initialized in array
+    // rowsize will be initialized when a value has to be inserted on that specific index.
+    public MyHashMap() {
+       buckets = 1000;
+       bucketSize = 1001;
+       storage = new int[buckets][];
+    }
+    
+    /** value will always be non-negative. */
+    // get hashcodes of the new key using both the hashfunctions
+    // check if a value has been inserted at hashValue1 index in 'storage' array
+    // if yes => array rowsize has also been initialized already => just insert 'value' from function arg on array element indexed at (hashvalue1 , hashvalue2).
+    // if no => initialize rowSize array on col index = hashValue1 => then insert 'value' from function arg on array element indexed at (hashvalue1 , hashvalue2).
+    public void put(int key, int value) {
+    
+        int hashValue1 = hashFunction1(key);
+        int hashValue2 = hashFunction2(key);
 
-   // Create a custom class Node to implement a linked list in case of collision
-   class Node{
-       int key;
-       int value;
-       Node next;
-       
-       public Node(int k,int v){
-           this.key = k;
-           this.value = v;
-           this.next = null;
-       }
-       
-       // override Object class equals method
-       public boolean equals(Object obj) {               
-           if (obj == null) {
-               return false;
-           }
-           Node other = (Node) obj;
-           if (this.key != other.key) {
-               return false;
-           }
-           return true;
-       }      
-   }
+        if(storage[hashValue1] == null){
+            storage[hashValue1] = new int[bucketSize];
+            Arrays.fill(storage[hashValue1], -1);
+        } 
+        
+        storage[hashValue1][hashValue2] = value; 
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+     // get hashcodes of the new key using both the hashfunctions which will yield two different values
+   // check if a value has been inserted at hashValue1 index in 'storage' array
+    // if yes, return that value.
+    // if no, return -1 as mentioned in prob statement.
+    public int get(int key) {
+        
+        // get hashcode of the new object
+        // use this hashcode as index in 'map' array
+        int hashValue1 = hashFunction1(key);
+        int hashValue2 = hashFunction2(key);
+        
+        if (storage[hashValue1] != null )
+           return storage[hashValue1][hashValue2];
+        
+        return -1;  
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    // get hashcodes of the new key using both the hashfunctions which will yield two different values
+    // check if a value has been inserted at hashValue1 index in 'storage' array
+    // if yes, set the value back to -1.
+    public void remove(int key) {
+        
+        // get hashcode of the new object
+        // use this hashcode as index in 'map' array
+        int hashValue1 = hashFunction1(key);
+        int hashValue2 = hashFunction2(key);
+          
+        if (storage[hashValue1] != null)
+          storage[hashValue1][hashValue2] = -1;    
+    }
+    
+    // first hashcode function to return hashcode for a numeric input
+    private int hashFunction1(int num){
+        return Integer.hashCode(num) % 1000;      
+    }
+    
+    // second hashcode function to return hashcode for a numeric input
+    private int hashFunction2(int num){
+        return Integer.hashCode(num) / 1000;      
+    }   
 }
 
 /**
-* Your MyHashMap object will be instantiated and called as such:
-* MyHashMap obj = new MyHashMap();
-* obj.put(key,value);
-* int param_2 = obj.get(key);
-* obj.remove(key);
-*/
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
