b45930fb5938b7304618c3c5c100bc3bfbab9315
Jaya Pratap <pratapjaya96@gmail.com>
2020-10-14 01:03:54 -0400

    Completed MinStack

---
+++ problem2.java
@@ -0,0 +1,51 @@
+// Problem 2:
+// Design Min-Stack (https://leetcode.com/problems/design-hashmap/)
+
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Was facing implementation approach issues before.  But this was explained by jaspinder.
+
+class MinStack {
+
+    Stack<Integer> stack;
+    int min;
+	
+	/** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        min = Integer.MAX_VALUE;
+        System.out.println(min);
+    }
+    
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
