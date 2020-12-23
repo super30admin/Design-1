68c8c378301c147fa918c3b1132fd7630d776278
Soumyya Kanti Datta <61840898+skrantidatta@users.noreply.github.com>
2020-12-22 21:24:00 -0500

    Design 1 completed

--- HashMap.py
+++ HashMap.py
@@ -1,4 +1,4 @@
-#// Time Complexity :O(1) for put, get and remove
+#// Time Complexity : O(1) for put, get and remove
 #// Space Complexity :O(n)
 #// Did this code successfully run on Leetcode :yes
 #// Any problem you faced while coding this :no
