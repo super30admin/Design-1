ab5d89018fad889744f2b6afa98300064e91b39b
saiteja-ganduri <60758084+saiteja-ganduri@users.noreply.github.com>
2021-02-16 01:03:12 -0600

    minstack-_final.py

--- minstack.py
+++ minstack.py
@@ -1,30 +1,39 @@
 class minstack:
     def __init__(self):
         self.mystack=[]
-        self.min=100000
+        self.min=sys.maxsize
         
     def push(self,num):
-        self.mystack.append(num)
-        if num<self.min:
+        if num<=self.min:
+            self.mystack.append(self.min)
             self.min=num
+        self.mystack.append(num)
+            
     
     def pop(self):
-        return self.mytack.pop()
+        popped=self.mystack.pop()
+        if popped==self.min:
+            self.min=self.mystack.pop()
     
     def min_item(self):
         return self.min
         
-    def is_Empty(self):
-        if len(self.mystack)==0:
-            return True
-        else:
-            return False
+    def peek(self):
+        return self.mystack[-1]
     
-   
-
 min_stack=minstack()
 min_stack.push(10)
 min_stack.push(20)
 min_stack.push(7)
 min_stack.push(8)
 min_stack.min_item()
+min_stack.pop()
+min_stack.pop()
+min_stack.min_item()
+        
+        
+        
+        
+        
+        
+        
