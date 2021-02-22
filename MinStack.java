b14d6704ab22b9b46799540ae6482e8f03b3bb7e
Ashish Mishra <ashishk861@gmail.com>
2021-02-22 15:53:47 -0500

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -115,4 +115,48 @@ class MinStack {
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
+ 
+ */
+ //ths single stack solution
+ class MinStack {
+    Stack<Integer> st;
+    Integer min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        st=new Stack<Integer>();
+        min=Integer.MAX_VALUE;
+        st.push(min);
+    }
+    
+    public void push(int x) {
+        if(x<=min){
+            st.push(min);
+            min=x;
+        }
+        st.push(x);
+    }
+    
+    public void pop() {
+        int temp=st.pop();
+        if(temp==min){
+            min=st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
