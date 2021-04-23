f65869466d3ac243af75230e1f16f907f3537186
sp9491 <42806187+sp9491@users.noreply.github.com>
2021-04-23 17:09:53 -0400

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,3 +1,12 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach : 1 Stack approach
+
+
 class MyHashMap {
 
     /** Initialize your data structure here. */
