c7f77c35c8ede1d34a68982479e00c8c800e7986
tade10 <nadatade@gmail.com>
2021-04-23 06:03:13 -0500

    Design 1 Completed - Updated
    Added comments for the code

--- MinStack.java
+++ MinStack.java
@@ -4,38 +4,48 @@
 // Any problem you faced while coding this : during the pop operation I forgot to change the currMin, hence it was failing for
 //                                           some test cases. But I figured it out with some print statment and corrected it
 class MinStack {
+    //2-D array that store our data for stack implementation
     int[][] stack;
+    //keep tracks of current global min
     int currMin;
+    //keep track of most recently added item
     int top;
-    /** initialize your data structure here. */
+   //initializing member variables
     public MinStack() {
         stack = new int[30000][];
         currMin = Integer.MAX_VALUE;
         top = -1;
     }
-
+    //method to push item at the top of stack
     public void push(int val) {
+        // index 0 of data stores te stack value
+        // index 1 of data stores the current min value
         int[] data = new int[2];
         data[0] = val;
+        // find the current min
         currMin = Math.min(val, currMin);
+        // update current min
         data[1] = currMin;
+        // add item to top
         stack[++top] = data;
     }
-
+    // removes item from the top
     public void pop() {
+        //decreaing top
         top--;
+        // if stack is empty
         if(top==-1){
             currMin = Integer.MAX_VALUE;
-        }
+        }// if stack is not empty, update current min
         else{
             currMin = Math.min(stack[top][1], Integer.MAX_VALUE);
         }
     }
-
+    // returns most recently added elements
     public int top() {
         return stack[top][0];
     }
-
+    //returns current min element at stack
     public int getMin() {
         return stack[top][1];
     }
--- MyHashMap.java
+++ MyHashMap.java
@@ -5,62 +5,80 @@
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : the worst case for put() operation is still O(n),
 //                                           although it will be very rare. I am not able to get fully O(1) for it
+
 class MyHashMap {
 
-
+    // a 2-D array reprsenting our set
     int[][] set;
-    int bucket, bucketItem;
+    //represents the hash1 locations
+    int bucket;
+    //represents the hash2 locations
+    int bucketItem;
 
-    /** Initialize your data structure here. */
+    // Initializing
     public MyHashMap() {
+        //since there can be at max 10^6 elements and each bucket can hold 1000 elements
+        // 10^3 * 10^3 = 10^6, hence we have 1000 buckets
         this.bucket = 1000;
+        //initilizing our set
         this.set = new int[this.bucket][];
     }
 
+    // first hash function
     public int hash1(int key){
         return key%1000;
     }
-
+    //second hash function
     public int hash2(int key){
         return key/1000;
     }
 
-    /** value will always be non-negative. */
+    // puts a key-value pair in hashMap
     public void put(int key, int value) {
+        //fetching bucket index from the key
         int bucket = this.hash1(key);
+        //checking if the bucket doesn't exists
         if(this.set[bucket]==null){
+            //create a bucket with 1000 length
             this.bucketItem = 1001;
             this.set[bucket] = new int[this.bucketItem];
-            //initializing array value to -1
-            // this is causing it to run in O(n) but I need to do put() in O(1)
-            // if I don't do this, then all values of nested array is initialized to Zero and when we make a get() call
-            // even though a key might not have been put(), returns zero on get() which is not valid. it should be null or -1
+
+            //initializing initial bucket item values to -1
             for(int i=0; i<this.bucketItem; i++){
                 this.set[bucket][i] = -1;
             }
         }
+        //get item index within bucket
         int bucketItem = this.hash2(key);
+        //mark item as available
         this.set[bucket][bucketItem] = value;
     }
 
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    //gets a value based on key from HashMap
     public int get(int key) {
+        //fetching bucket index
         int bucket = this.hash1(key);
+        //checking if the bucket doesn't exists
         if(this.set[bucket]==null){
             return -1;
         }
+        //get key index within bucket
         int bucketItem = this.hash2(key);
-
+        //return value at the index
         return this.set[bucket][bucketItem];
     }
 
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+   //remove a key-value pair from HashMap
     public void remove(int key) {
+        //fetching bucket index
         int bucket = this.hash1(key);
+        //checking if the bucket doesn't exists
         if(this.set[bucket]==null){
             return;
         }
+        //get key index within bucket
         int bucketItem = this.hash2(key);
+        //mark current location as empty
         this.set[bucket][bucketItem] = -1;
     }
 }
