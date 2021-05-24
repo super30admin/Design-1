4295a33044e82f23a31ba4b33c96136f50945793
krishnachaithanya attapu <84545593+krishnaattapu@users.noreply.github.com>
2021-05-24 17:36:12 -0500

    Update MinStackMain.java
    added comments

--- MinStackMain.java
+++ MinStackMain.java
@@ -1,6 +1,9 @@
 
 /**
- * 
+ * Time Complexity :O(1) faster than 92.71% on leet code
+ * Space Complexity : O(1) less than 31.74% on leet code
+ * Did this code successfully run on Leetcode : yes
+ * Any problem you faced while coding this : No
  *
  */
 class MinStack
