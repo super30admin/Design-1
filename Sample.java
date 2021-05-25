e9a9965de76554880aa2cbcef84c0e9bee0a76ed
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-05-25 23:17:00 +0530

    TC & SC

--- Sample.java
+++ Sample.java
@@ -1,7 +1,12 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// MinStack:
+// Time Complexity : All operations O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :
 
-
-// Your code here along with comments explaining your approach
+// HashMap:
+// Time Complexity : All operations O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : This is not the optimal solution as creation of 10^6 array is not realistic. 
+//                                           I would like to reimplement this after learning the concepts in class
