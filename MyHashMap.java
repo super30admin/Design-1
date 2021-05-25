6350301b4a077aecc9c5e88a516f023bf7a094c8
Gunjan Kushwah <gunjankushwah@Gunjans-MBP.attlocal.net>
2021-05-25 16:52:33 -0500

    HashMap Implementation

--- MyHashMap.java
+++ MyHashMap.java
@@ -2,7 +2,7 @@
 Created HashMap DS using LinkedList Node --> key, value, next
 Time Complexity: O(1) average and O(n) for worst-case
 Space Complexity:O(1)
-Ran on LeetCode: Yes, accepted solution without issues
+Ran on LeetCode: Yes, accepted solution without issue
 */
 
 class MyHashMap {
