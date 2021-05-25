5c0b8d2957b26e8cf0c53104c4a638ed1ba454c7
Arjun Hegde <arjunhegde372@gmail.com>
2021-05-24 20:51:39 -0500

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,3 +1,8 @@
+// Time Complexity :  O(1)
+// Space Complexity :  O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 class MyHashMap {
 
         ListNode[] map;
