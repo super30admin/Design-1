4b27d06ba6723c042631045830a89c7c96d5bed8
Ishan Darwhekar <isdarwhekar@gmail.com>
2021-05-26 01:29:53 +0530

    Completed Design 1 - v1

---
+++ spl_stack.cpp
@@ -0,0 +1,53 @@
+class MinStack {
+private:
+    stack<int> normalStack;
+    stack<int> minStack;
+    
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        minStack.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        
+        normalStack.push(val);
+        
+        if(val <= minStack.top())
+        {
+            minStack.push(val);
+        }
+        
+        return;
+    }
+    
+    void pop() {
+        int val = normalStack.top();
+        
+        normalStack.pop();
+        
+        if(val == minStack.top())
+        {
+            minStack.pop();
+        }
+    }
+    
+    int top() {
+        
+        return normalStack.top();
+    }
+    
+    int getMin() {
+        
+        return minStack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
