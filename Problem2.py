db1b65149c89f5679c23eb6581a785ac1714fd7f
akhill-shetty <akhillshetty@gmail.com>
2021-05-25 23:33:57 -0400

    added programs - Problem 2 pending

---
+++ Problem2.py
@@ -0,0 +1,44 @@
+# Time Complexity :
+# Search operation O(1)
+# Space Complexity :
+# O(B+n) #Size of Buckets + N Keys
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+#
+#
+# Your code here along with comments explaining your approach
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+
+    def top(self):
+        """
+        :rtype: int
+        """
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
