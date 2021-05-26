a71b549ecd16585ac25d31a5f4a45520ada9cf3e
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-05-25 20:37:03 -0700

    completed hashmap problem

---
+++ HashMap/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    private final int key;
+    private final int value;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
