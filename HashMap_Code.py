a38f1993492d2b38b9ba8186e6119b04fb8822ec
BhavyaSri1001 <84418657+BhavyaSri1001@users.noreply.github.com>
2021-05-26 01:05:56 -0700

    Update HashMap_Code.py

--- HashMap_Code.py
+++ HashMap_Code.py
@@ -1,17 +1,28 @@
+
 class MyHashMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.size = 100
-        self.arr = [[] for _ in range(self.size)]
+        self.size = 1000
+        self.nestedSize = 1000
+        self.arr = [None for _ in range(self.size)]
+        # print(self.arr)
+
+    def create_nestedarr(self, hashval):
+        if hashval == 0:
+            return [None for _ in range(self.nestedSize +1)]
+        else:
+            return [None for _ in range(self.nestedSize)]
         
     def get_hashvalue(self, key):
-        hashvalue = 0
-        for char in str(key):
-            hashvalue += ord(char)
-        return hashvalue%self.size
+        return key%self.size
+    
+    def get_nested_hashvalue(self, key):
+        return key//self.nestedSize
+
+    
         
 
     def put(self, key: int, value: int) -> None:
@@ -19,14 +30,11 @@ class MyHashMap:
         value will always be non-negative.
         """
         hashval = self.get_hashvalue(key)
-        hash_bucket = self.arr[hashval]
-        key_found = False
-        for index, element in enumerate(hash_bucket):
-            if key == element[0]:
-                key_found = True
-                hash_bucket[index] = (key, value)
-        if not key_found:
-            hash_bucket.append((key, value))
+        nestedhashval = self.get_nested_hashvalue(key)
+        if self.arr[hashval] == None:
+            self.arr[hashval] = self.create_nestedarr(hashval)
+        # if self.arr[hashval][nestedhashval] == None:
+        self.arr[hashval][nestedhashval] = value
         
 
     def get(self, key: int) -> int:
@@ -34,13 +42,16 @@ class MyHashMap:
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
         hashval = self.get_hashvalue(key)
+        nestedhashval = self.get_nested_hashvalue(key)
         hash_bucket = self.arr[hashval]
-        key_found = False
-        for index, element in enumerate(hash_bucket):
-            if key == element[0]:
-                key_found = True
-                return element[1]
-        if not key_found:
+        if hash_bucket == None:
+            # print("No Elements")
+            return -1
+        elif self.arr[hashval][nestedhashval] is not None:
+            # print(self.arr[hashval][nestedhashval])
+            return self.arr[hashval][nestedhashval]
+        else:
+            # print("None")
             return -1
 
     def remove(self, key: int) -> None:
@@ -48,19 +59,23 @@ class MyHashMap:
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
         hashval = self.get_hashvalue(key)
+        nestedhashval = self.get_nested_hashvalue(key)
         hash_bucket = self.arr[hashval]
-        key_found = False
-        for index, element in enumerate(hash_bucket):
-            if key == element[0]:
-                key_found = True
-                hash_bucket.pop(index)
-        return 
-                
-        
-
-
+        if hash_bucket != None and self.arr[hashval][nestedhashval] is not None:
+            print(self.arr[hashval][nestedhashval])
+            self.arr[hashval][nestedhashval] = None
+            print(self.arr[hashval][nestedhashval])
+       
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+# obj.put(1,1)
+# obj.put(2,2)
+# obj.get(1)
+# obj.get(2)
+# obj.get(3)
+# obj.put(2,1)
+# obj.get(2)
+# obj.remove(2)
+# obj.get(2)
+
+# this code gives O(1) time complexity and it was submitted in leetcode as well.
