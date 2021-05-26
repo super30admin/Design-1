556f836d99e133d964b3cd3f0b967674a53f8c8c
unknown <nitishdh@buffalo.edu>
2021-05-26 11:15:30 -0400

    Design 1 completed. Please review

---
+++ MinStack.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    int min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+        minStack.push(min);
+    }
+    
+    public void push(int val) {
+        min = Math.min(min, val);
+        stack.push(val);
+        minStack.push(min);
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+        min = minStack.peek();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
