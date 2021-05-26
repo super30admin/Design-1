028b1a06ed1db9069fb97c66d7523d4a93d37183
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-05-26 09:36:31 +0530

    Updated after the class

--- Problem1.py
+++ Problem1.py
@@ -10,29 +10,49 @@ class MyHashMap:
         """
         Initialize your data structure here.
         """
-        self.map = {}
+        self.arr = [None]*1000
         
+    def bucket(self,key: int) -> int:
+        return key % 1000
+    
+
+    def bucketValue(self,key: int) -> int:
+        return key//1000
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        self.map[key] = value
+        a = self.bucket(key)
+        b = self.bucketValue(key)
+        if self.arr[a] == None:
+            if a == 0:
+                self.arr[a] = [None]*1001
+            else:
+                self.arr[a] = [None]*1000
+        self.arr[a][b] = value
+        
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        if key in self.map:
-            return self.map[key]
-        return -1
+        a = self.bucket(key)
+        b = self.bucketValue(key)
+        if self.arr[a] == None:
+            return;
+        return self.arr[a][b]
+        
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        if key in self.map:
-            del self.map[key]
+        a = self.bucket(key)
+        b = self.bucketValue(key)
+        if self.arr[a] == None:
+            return;
+        self.arr[a][b] = None
         
 
 
