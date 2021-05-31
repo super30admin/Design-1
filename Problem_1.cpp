064000cb1b36fbf73b96f4c334a795e047dd8687
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-30 21:59:18 -0400

    Update Problem_1.cpp
    remade hashmap

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,26 +1,43 @@
 class MyHashMap {
-vector<int> base;
-public:
-    MyHashMap() {}
+    vector<list<pair<int,int>>> m_data;
+    int m_size = 1000;
     
-    void put(int key, int value) {
-        if (key >= base.size()) {
-            base.resize(key+1, -1);
-            //this is inefficient approach, will be working on more efficient one
-            base[key%base.size()] = value;
-        }
-        else
-            base[key%base.size()] = value;
+    //I used array + list combo here, except I am not making node here and using readymade list which is is stl
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        m_data.resize(m_size);
     }
     
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        auto &list = m_data[key % m_size];
+        for (auto & val : list) {
+            if (val.first == key) {
+                val.second = value;
+                return;
+            }
+        }
+        list.emplace_back(key, value);
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-        if (key < base.size() && base[key] != -1)
-            return base[key];
+        const auto &list = m_data[key % m_size];
+        if (list.empty()) {
+            return -1;
+        }
+        for (const auto & val : list) {
+            if (val.first == key) {
+                return val.second;
+            }
+        }
         return -1;
     }
     
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
-        if (key < base.size())
-            base[key] = -1;
+        auto &list = m_data[key % m_size];
+        list.remove_if([key](auto n) { return n.first == key; });
     }
 };
