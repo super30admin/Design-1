81845a59b03678b39199256527549210f62ecfbc
Harshit <arorah1100@gmail.com>
2021-05-31 18:20:14 -0500

    Passing 34/36 on leetcode for HashMap

--- MyHashMap.java
+++ MyHashMap.java
@@ -2,25 +2,24 @@ class MyHashMap {
     private int[][] hashArray;
     private int primArrSize;
     private int secArrSize;
-    public:
+
     /** Initialize your data structure here. */
-    MyHashMap() {
+    public MyHashMap() {
         primArrSize = 1000;
         secArrSize = 1000;
         hashArray = new int[primArrSize][secArrSize];
-        // the reason we take the max length for our primary array as 1000 is because we have told value <=10^6
-        //So we use the square root
     }
+
     int getPrimaryIndex(int key){
-        return value % 1000;
+        return key % 1000;
     }
 
     int getSecondaryIndex(int key){
-        return value / 1000;
+        return key / 1000;
     }
 
     /** value will always be non-negative. */
-    void put(int key, int value) {
+    public void put(int key, int value) {
         int location1 = getPrimaryIndex(key);
         int location2 = getSecondaryIndex(key);
         if (location1 == 0 && hashArray[location1] == null){
@@ -39,40 +38,35 @@ class MyHashMap {
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    int get(int key) {
-
+    public int get(int key) {
         int location1 = getPrimaryIndex(key);
         int location2 = getSecondaryIndex(key);
 
-        if(hashArray[location1][location2] != null){
-            return hashArray[location1][location2]
+        if(hashArray[location1][location2] != 0){
+            return hashArray[location1][location2];
         }
         else{
-            return -1
+            return -1;
         }
-
 
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    void remove(int key) {
+    public void remove(int key) {
         int location1 = getPrimaryIndex(key);
         int location2 = getSecondaryIndex(key);
 
-        if(hashArray[location1][location2] != null){
-            hashArray[location1][location2] = null;
-        }
-        else{
-            System.out.println("No entry found")
+        if(hashArray[location1][location2] != 0){
+            hashArray[location1][location2] = 0;
         }
 
     }
-};
+}
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
  */
