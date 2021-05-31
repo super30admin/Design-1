e33fd96c6b8256e10710ab486c06f57d56a165a0
Harshit <arorah1100@gmail.com>
2021-05-31 18:46:56 -0500

    Double Stack method and Passes all test cases on the leetcode side

--- MinStack.java
+++ MinStack.java
@@ -1,33 +1,51 @@
 class MinStack {
-    public:
+
     /** initialize your data structure here. */
-    MinStack() {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
 
+    public MinStack() {
+
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+        stack2.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+
+        if(stack2.peek() < val){
+            stack1.push(val);
+            stack2.push(stack2.peek());
+        }
+        else{
+            stack1.push(val);
+            stack2.push(val);
+        }
+    }
+
+    public void pop() {
+        stack1.pop();
+        stack2.pop();
 
     }
 
-    void push(int val) {
+    public int top() {
+
+        return stack1.peek();
 
     }
 
-    void pop() {
+    public int getMin() {
+        return stack2.peek();
 
     }
-
-    int top() {
-
-    }
-
-    int getMin() {
-
-    }
-};
+}
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
