64c67e318a4aa4000e4d93ac59400e549b85742d
Akarsh-Chandrashekar <49974714+Akarsh-Chandrashekar@users.noreply.github.com>
2021-05-31 22:27:35 -0700

    Update Hashmap.cpp

--- Hashmap.cpp
+++ Hashmap.cpp
@@ -1,36 +1,64 @@
-#include<iostream>
-#include<stack>
-
-using namespace std;
-
-class MinStack {
+class MyHashSet {
 public:
-    /** initialize your data structure here. */
-    stack<int> st;
-    stack<int> min;
-    int minimum;
-    MinStack() {
-        minimum=INT_MAX;
-        min.push(minimum);
+    vector<vector<bool>> storage;
+    int buckets;
+    int bucketItems;
+    /** Initialize your data structure here. */
+    MyHashSet() {
+        buckets=1000;
+        bucketItems=1000;
+        storage.resize(1000);
+    }
+    int getBuckets(int key)
+    {
+        return key%buckets;
+    }
+    int getBucketItems(int key)
+    {
+        return key/bucketItems;
+    }
+    void add(int key) {
+        int bucket = getBuckets(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket].size()==0)
+        {
+            if(bucket==0)
+            {
+                storage[bucket].resize((bucketItems+1));
+            }
+            else{
+            storage[bucket].resize(bucketItems);
+            }
+        }
+        storage[bucket][bucketItem]= true;
     }
     
-    void push(int val) {
-        minimum=std::min(minimum,val);
-        st.push(val);
-        min.push(minimum);
+    void remove(int key) {
+        int bucket = getBuckets(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket].size()==0)
+        {
+            return;
+        }
+        storage[bucket][bucketItem]=false;
     }
     
-    void pop() {
-        st.pop();
-        min.pop();
-        minimum=min.top();
-    }
-    
-    int top() {
-        return st.top();
-    }
-    
-    int getMin() {
-        return minimum;
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        int bucket = getBuckets(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket].size()==0)
+        {
+            return false;
+        }
+        return storage[bucket][bucketItem];
     }
 };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
