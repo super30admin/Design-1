8a54d8d93c37bf28373b8cef8ec0e7f157b65e7a
jisakp7 <kavya.pathi@jbhunt.com>
2021-06-22 23:13:23 -0500

    - optimized solution- MinStack

--- Java/MinStack.java
+++ Java/MinStack.java
@@ -1,50 +1,49 @@
 // Time Complexity : O(1)
-// Space Complexity : (n)
-// Did this code successfully run on Leetcode : Not for all testCases
-// Any problem you faced while coding this :
-Yes what to return if the stack is empty
-
-
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 // Your code here along with comments explaining your approach
+/*
+Using only one stack and comparing the input value to the minimum and if val less than min, then push min into the stack and update min and push the original input val also into the stack.
+Else you just push the input val into the stack.
+while popping compare the value to the min and if equal, pop it twice nad update the min to the previous min or else just pops the top of the stack.
+min is getting updated timely, so we can just return min for getMin().
+* */
+
 class MinStack {
-    Queue<Integer> q = new LinkedList<>();
+    Stack<Integer> stack;
+    int min;
 
-    /** initialize your data structure here. */
+    /**
+     * initialize your data structure here.
+     */
     public MinStack() {
-
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
     }
 
     public void push(int val) {
-        q.add(val);
-
-        int size = q.size();
-        while(size-->1){
-            q.add(q.poll());
+        if (val <= min) {
+            stack.push(min);
+            min = val;
         }
+        stack.push(val);
     }
 
     public void pop() {
-        q.poll();
+        int popVal = stack.pop();
+        if (popVal == min) {
+            min = stack.pop();
+        }
+
     }
 
     public int top() {
-        if(!q.isEmpty()){
-            return q.peek();
-        } return 0;
-
+        return stack.peek();
     }
 
     public int getMin() {
-        if(!q.isEmpty()){
-            int  min = q.poll();
-
-            while(!q.isEmpty()){
-
-                min = Math.min(min, q.poll());
-            }
-            return min;
-        }
-        return -2;
+        return min;
     }
 }
 
