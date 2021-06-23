c6093fcb68f15f00674384cf364adb7f444a1dde
jisakp7 <kavya.pathi@jbhunt.com>
2021-06-23 03:29:52 -0500

    -HashMap using hashing&LinkedList

--- Java/ImplementHashMap.java
+++ Java/ImplementHashMap.java
@@ -1,38 +1,72 @@
-// Time Complexity : O(1)
-// Space Complexity : O(n)
+// Time Complexity : O(N+M) where N is number of nodes and M is unique key value pairs
+// Space Complexity : O(K/B) where n is number of keys and k is number of buckets
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :No
-
-
 // Your code here along with comments explaining your approach
 /*
- Using Large Sized Array
- Arrays initialize default value as 0, so using Arrays.fill(arr, -1) --> as the requirement is to get -1 if there is no mapping for the key
+Used hashing technique to reduce number of collisions
+and LinkedList to hold all the values assigned by hash function
 */
-
-
 class MyHashMap {
+ ListNode[] nodes;
 
- int[] arr;
  /** Initialize your data structure here. */
  public MyHashMap() {
-  arr = new int[1000001];
-  Arrays.fill(arr, -1);
+  nodes =new ListNode[10000];
+ }
+
+ int bucket(int key){
+  return key%nodes.length;
+ }
+
+ ListNode findPrev(ListNode node, int key){
+  ListNode curr = node;
+  ListNode prev=null;
+  while(curr!=null && curr.key!=key){
+   prev=curr;
+   curr=curr.next;
+  }
+  return prev;
  }
 
  /** value will always be non-negative. */
  public void put(int key, int value) {
-  arr[key] = value;
+  int i = bucket(key);
+  if (nodes[i] == null)
+   nodes[i] = new ListNode(-1, -1);
+  ListNode prev = findPrev(nodes[i], key);
+  if(prev.next==null)
+   prev.next = new ListNode(key, value);
+  else
+   prev.next.val = value;
  }
 
  /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
  public int get(int key) {
-  return arr[key];
+  int i = bucket(key);
+  if(nodes[i] == null) return -1;
+  ListNode prev = findPrev(nodes[i], key);
+  if(prev.next==null) return -1;
+  return prev.next.val;
+
  }
 
  /** Removes the mapping of the specified value key if this map contains a mapping for the key */
  public void remove(int key) {
-  arr[key] = -1;
+  int i = bucket(key);
+  if(nodes[i] == null) return;
+  ListNode prev = findPrev(nodes[i], key);
+  if(prev.next==null) return;
+  prev.next = prev.next.next;
+
+ }
+ class ListNode{
+  int key, val;
+  ListNode next;
+  ListNode(int key,int val){
+   this.key = key;
+   this.val=val;
+  }
  }
 }
 
@@ -42,4 +76,4 @@ class MyHashMap {
  * obj.put(key,value);
  * int param_2 = obj.get(key);
  * obj.remove(key);
- */
+ *//
