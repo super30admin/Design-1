bca653f76bf45e1f6b5ace41636bb9056779450f
Suramya Singh <suramyasingh@Suramyas-MBP.hsd1.ca.comcast.net>
2021-06-22 18:24:52 -0700

    Completed Design-1

--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@
 // Space Complexity :
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
-
-
 // Your code here along with comments explaining your approach
+
+
+
+//I HAVE CREATED TWO SEPERATE JAVA FILES!! PLEASE CHECK
