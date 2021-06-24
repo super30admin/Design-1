ace3eb6e8423be36de3406c34cb00a07b575b766
Mehar Shahani <mehar.shahani@gmail.com>
2021-06-23 19:26:07 -0500

    updating code with the 2 approaches discussed in class

--- design-minstack.py
+++ design-minstack.py
@@ -1,40 +1,75 @@
-class MinStack(object):
-    
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.min_stack = []
-        
+import sys
 
+class MinStack(object):
+    # 1st approach - Single stack
+    # every time minimum is encountered, 
+    # push the old minimum first and then the new minimum onto the stack
+    # time complexity - O(1) for all operations
+    # space complexity - O(1)
+    def __init__(self):
+        self.stack = []
+        self.stack.append(sys.maxsize)
+        self.min = self.stack[-1]
+
+        
     def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        self.min_stack.append(val)
+        if val <= self.min:
+            self.stack.append(self.min)
+            self.stack.append(val)
+            self.min = val
+        else:
+            self.stack.append(val)
         
 
     def pop(self):
-        """
-        :rtype: None
-        """
+        if self.stack[-1] == self.min:
+            self.stack.pop()
+            self.min = self.stack[-1]
+            self.stack.pop()
+        else:
+            self.stack.pop()
+        
+
+    def top(self):
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        return self.min
+    
+    
+    # 2nd approach - 2 stack
+    # stack - pushes and pop as required
+    # min_stack - pushes element only if it less than min else pushes min again
+    # time complexity - O(1) for all opeations
+    # space complexity - O(2n) - ignore constants so it is O(n)
+    """
+    def __init__(self):
+        self.min_stack = []
+        self.stack = []
+        self.min_stack.append(sys.maxsize)
+        
+
+    def push(self, val):
+        if val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(self.min_stack[-1])
+        self.stack.append(val)
+
+    def pop(self):
+        self.stack.pop()
         self.min_stack.pop()
         
 
     def top(self):
-        """
-        :rtype: int
-        """
-        return self.min_stack[-1]
+        if self.stack:
+            return self.stack[-1]
         
 
     def getMin(self):
-        """
-        :rtype: int
-        """
-        if self.min_stack is not None:
-            return min(self.min_stack)
+        if self.stack:
+            return self.min_stack[-1] """
 
 
 # Your MinStack object will be instantiated and called as such:
