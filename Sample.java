8e4d86aa036d4d4fbb9bcbec6820543c1ed73446
Pinak Pathak <pinakpathak@Pinaks-MacBook-Pro.local>
2021-07-20 15:54:43 -0400

    Completed Problem-2

--- Sample.java
+++ Sample.java
@@ -1,41 +1,51 @@
-// Time Complexity : O(1) FOR ALL THREE METHODS AND O(1000000) FOR ININTIALIZING HASHMAP
-// Space Complexity : O(10^6)
+// Time Complexity : O(1)
+// Space Complexity : O(N)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Want to decrease space complexity
+// Any problem you faced while coding this : Want to decrease Space Complexity
 
 
 // Your code here along with comments explaining your approach
 //CODE IS IN C++
-class MyHashMap {
+class MinStack {
 public:
-    /** Initialize your data structure here. */
-    int a[1000001];
-    MyHashMap() {
-        memset(a, -1, sizeof(a));
+    /** initialize your data structure here. */
+    stack<int>s1;
+    stack<int>s2;
+    MinStack() {
     }
     
-    /** value will always be non-negative. */
-    void put(int key, int value) {
-        a[key%1000001]=value;
+    void push(int val) {
+        s1.push(val);
+        if(s2.empty()){
+            s2.push(val);
+            return;
+        }
+        if(s2.top()>=val){
+            s2.push(val);
+            return;
+        }
+        s2.push(s2.top());
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    int get(int key) {
-        if(a[key%1000001]!=-1)
-            return a[key%1000001];
-        return -1;
+    void pop() {
+        s1.pop();
+        s2.pop();
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    void remove(int key) {
-        a[key%1000001]=-1;
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
     }
 };
 
 /**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
  */
