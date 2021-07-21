f6c4709e11c3b8440bc9dac7e72e1fe5a6d3c711
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2021-07-21 13:20:14 +0530

    Updated hashset.py with double hashing logic

--- hashSet.py
+++ hashSet.py
@@ -1,49 +1,70 @@
 
-# Time Complexity : Add:amortized O(1) Remove: O(n) Contain:O(n)
+# Time Complexity : Add: O(1) Remove: O(1) Contain:O(1)
 # Space Complexity : O(n)
 # Code ran successfully run on Leetcode 
-# Issue in finding out Space complexity. Also trying to optimize code to O(1)
-# I am using simple hashing function which stores element in the index key % 100 of the list.
-# Whole Hash table is a 2D list , with each bucket being 1D list. If element is already there is bucket, then another element 
-# is appended in the 1D list.
+# I am using double hashing : 1st hash func which stores element in the index key % 100 of the list. And second stores in key /100                                                                          
+
 class MyHashSet:
 
     def __init__(self):
-        self.MAX = 100
-        self.lst = [[] for i in range(self.MAX)]
+        self.MAX = 1000
+        #self.lst = [[][] for i in range(self.MAX)]
+        self.lst = [None] * self.MAX 
         
-    def get_Hash(self, key: int) -> int:
-        hash_code = key % self.MAX
+    def get_hash(self, key: int) -> int:
+        hash_code = int(key % self.MAX)
         return hash_code
     
+    def get_buckItem(self, key: int) -> int:
+        return int(key / self.MAX)
+    
     def add(self, key: int) -> None:
+        
+        # If key already exists, don't add
         if self.contains(key):
             return
+        bucket = self.get_hash(key)
+        buck_item = self.get_buckItem(key)
+        if self.lst[bucket] is None:
+            if (bucket == 0):
+                self.lst[bucket]  = [False] * 1001
+            else:
+                self.lst[bucket]  = [False] * 1000
+            # allocate 
+        
         
         # Add key only if it doesn't exist
-        hcode = self.get_Hash(key)
-        self.lst[hcode].append(key)
-        return
-
+       # self.get_Hash(key)
+        #if len(self.lst[hcode]) <= 0:
+            
+        self.lst[bucket][buck_item] =  True
+        
     def remove(self, key: int) -> None:
+        
+        # If the key doesn't exist, it can't be removed
         if self.contains(key) is False:
             return
-       
-        # Remove the key , as it exists 
-        hcode = self.get_Hash(key)
-        self.lst[hcode].remove(key)
-        return
+        bucket = self.get_hash(key)
+        buck_item = self.get_buckItem(key)
+        
+        # Remove the key , by changing its status to False 
+        self.lst[bucket][buck_item] =  False
     
     def contains(self, key: int) -> bool:
         
         """
         Returns true if this set contains the specified element
         """
-        hcode = self.get_Hash(key)
-        if key in self.lst[hcode] :
-            return True
-        else:
+        bucket = self.get_hash(key)
+        buck_item = self.get_buckItem(key)
+       
+        # If bucket is None, then it implies item doesn't exist
+        if  self.lst[bucket] is None :
             return False
+        
+        # Else return the status of bucket item
+        else:
+            return self.lst[bucket][buck_item]
         
 
 # Your MyHashSet object will be instantiated and called as such:
