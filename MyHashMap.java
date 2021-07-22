eb88391b7c98e9610d412f7e0e8d5bd2000690b4
msahithi29 <50932067+msahithi29@users.noreply.github.com>
2021-07-21 22:28:21 -0700

    Completed Sprint Design1 (hashmap modified)

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,80 +1,112 @@
+//Ran successfully
 class MyHashMap {
-
+    //HashMap implemenatation using separate chaining method
+    /*
+        TC:
+            put, get, remove - O(n) where n is the size of the linked list at a node which would be equal to O(max input range/array of nodes size)
+        SC: O(array of nodes size + input element size)
+    */
+    //Array of nodes as primary, linked list of nodes as secondary
     /** Initialize your data structure here. */
-    int[][] storage;
-    int primaryStorage;
-    int secondaryStorage;
-    public MyHashMap() {
-        primaryStorage = 1000;
-        secondaryStorage = 1000;
-        storage = new int[primaryStorage][];
-    }
-    private int getPrimaryIndex(int key)
+    class node //node structure creation
     {
-        return key%primaryStorage;
-    }
-    private int getSecondaryIndex(int key)
-    {
-        return key/secondaryStorage;
-    }
-    
-    /** value will always be non-negative. */
-    public void put(int key, int value) {
-        int primaryIndex = getPrimaryIndex(key);
-        int secondaryIndex = getSecondaryIndex(key);
-        if(storage[primaryIndex]==null)
+        int key,value;
+        node next;
+        public node(int key,int val)
         {
-            if(primaryIndex==0)
+            this.key = key;
+            this.value= val;
+        }
+    }
+    node[] nodes; // array of node objects
+    int size;
+    public MyHashMap() {
+        size=1000;
+        nodes = new node[size];
+    }
+    //hashing function
+    private int getHashCode(int key) 
+    {
+        return Integer.hashCode(key) % size;
+    }
+    private node find(node head,int key) // to find the previous node
+    {
+        node prev = null;
+        node cur = head;
+        while(cur!=null && cur.key != key)
+        {
+            prev = cur;
+            cur = cur.next;
+        }
+        return prev;
+    }
+    /** value will always be non-negative. */
+    //find hash value using the hashing function. check if there is a node in the index, if not, create a dummy node and add the given node by finding the previous node. if the node already exists, find the previous node and update the value of next node from previous node.
+    public void put(int key, int value) {
+        int index = getHashCode(key);
+        if(nodes[index]==null)
+        {
+            nodes[index] = new node(-1,-1);
+        }
+        node prev = find(nodes[index],key);
+        if(prev.next==null)
+        {
+            prev.next = new node(key,value);
+        }
+        else
+        {
+                prev.next.value=value;
+        }
+    }
+    /*
+    Find the hash value and check if any node is present at the index. return -1 if null. if not, find the previous node and check the next node of previous node. if it is null, return -1, else the value.
+    
+    */
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int index = getHashCode(key);
+        if(nodes[index]==null)
+        {
+            return -1;
+        }
+        else
+        {
+            node prev = find(nodes[index],key);
+            if(prev.next==null)
             {
-               storage[primaryIndex] = new int[secondaryStorage+1]; 
+                return -1;
             }
             else
             {
-                storage[primaryIndex] = new int[secondaryStorage];
+                return prev.next.value;
             }
-        }
-        if(value!=0)
-            storage[primaryIndex][secondaryIndex] = value;
-        else
-            storage[primaryIndex][secondaryIndex] = -2;
+        }   
     }
+    /*
+    Find the hash value and check if any node is present at the index. return if null. if not, find the previous node and check the next node of previous node. if it is null, return, else link the previous node with the next next node.
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        int primaryIndex = getPrimaryIndex(key);
-        int secondaryIndex = getSecondaryIndex(key);
-        if(storage[primaryIndex]!=null)
-        {
-          if(storage[primaryIndex][secondaryIndex]==-2)
-              return 0;
-          else if(storage[primaryIndex][secondaryIndex]!=0)
-              return storage[primaryIndex][secondaryIndex];
-        }
-        return -1;
-    }
-    
+    */
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        int primaryIndex = getPrimaryIndex(key);
-        int secondaryIndex = getSecondaryIndex(key);
-        if(storage[primaryIndex]!=null)
+        int index = getHashCode(key);
+        if(nodes[index]==null)
         {
-                storage[primaryIndex][secondaryIndex] = -1;
+            return;
         }
-        return;
+        else
+        {
+            node prev = find(nodes[index],key);
+            if(prev.next==null)
+            {
+                return;
+            }
+            else
+            {
+                prev.next = prev.next.next;
+            }
+        }
     }
 }
-
-/*
-    Time Complexity
-    put - O(1)
-    get - O(1)
-    remove - O(1)
-    
-    Space Complexity - O(primary array size * secondary array size)
-    
-
-*/
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
@@ -83,4 +115,3 @@ class MyHashMap {
  * int param_2 = obj.get(key);
  * obj.remove(key);
  */
-
