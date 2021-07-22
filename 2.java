16892a5f923d3cd7015ecfa1cd92fd3595d9bcad
Aditya Balasubramanyam <aditya.gnb@gmail.com>
2021-07-22 08:58:06 -0400

    MinStack

---
+++ 2.java
@@ -0,0 +1,7 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
