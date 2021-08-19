8306bc89b89ee4f9300565f6634d135c701f4d34
veeceey <varun_6april@hotmail.com>
2021-08-18 22:40:42 -0700

    Min stack implemented please review

--- minstack.py
+++ minstack.py
@@ -1,6 +1,15 @@
-###I was following a youtube video  https://www.youtube.com/watch?v=QMlDCR9xyd8 for designing this
-# and algorithm tells to insert 2*val - min if we find a value smaller than current min; need to attend today's
-##class to see how it works ###
+"""
+Time Complexity:O(1) for everything
+Space Complexity: O(n)
+Here we will be taking two arrays one with value stack and other we can call as minstack.
+we will populate the minimum value stack with infinity. For pushing the minimum value we will check
+the minimum out of the value and the current minimum value which is minimum we will it append it to minimum
+stack and we will push the value into the value stack. For popping we will remove both the top elements at
+minimum stack and value stack and reassign the min value to the top of minimum stack. For checking the top we will
+just check the top value of the value stack and for retrieving the minimum value we will just see the minimum value
+Ran on leetcoe-yes
+
+"""
 
 class MinStack:
 
@@ -8,36 +17,39 @@ class MinStack:
         """
         initialize your data structure here.
         """
-        self.stack = []
+        self.valueStack = []
+        self.minStack=[]
+        self.minvalue=float('inf')
+        self.minStack.append(self.minvalue)
 
     def push(self, val: int) -> None:
+        self.minvalue=min(val, self.minvalue)
+        self.valueStack.append(val)
+        self.minStack.append(self.minvalue)
+
         # self.stack.append(val)
-        if self.stack==[]:
-            self.stack.append(val)
-
-        elif val >= self.getMin():
-            self.stack.append(val)
-        else:
-            self.stack.append(2*val - self.getMin())
-
+        # if self.stack==[]:
+        #     self.stack.append(val)
+        #
+        # elif val >= self.getMin():
+        #     self.stack.append(val)
+        # else:
+        #     self.stack.append(2*val - self.getMin())
 
 
     def pop(self) -> None:
-        if self.stack==[]:
-            return None
+        self.valueStack.pop()
+        self.minStack.pop()
+        self.minvalue=self.minStack[-1]
 
-        ###popping will happen same way as pushing just in case if value is less than currentmin we change min to
-        ###2*min- currentvalue
 
     def top(self) -> int:
 
-        return None
+        return self.valueStack[-1]
 
     def getMin(self) -> int:
-        currentMin = self.stack[0].val ##initially
 
-            # currentMin=None
-        return currentMin
+        return self.minvalue
 
 
 
