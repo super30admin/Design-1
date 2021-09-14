81e62b91ee0bce45dc6e1369297a01049b1e87f6
hemanthatushara <50065348+hemanthatushara@users.noreply.github.com>
2021-09-13 22:00:18 -0500

    Update minstack.py

--- minstack.py
+++ minstack.py
@@ -1,4 +1,4 @@
-# Time Complexity : O(1)
+
 # Space Complexity : O(n)
 #Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
@@ -11,17 +11,19 @@ class MinStack:
         """
         self.arr = []
         
-
+# Time Complexity : O(1)
     def push(self, val: int) -> None:
         self.arr.insert(0,val) # appending an item to the array will perform the push operation 
                 
-
+# Time Complexity : O(1)
     def pop(self) -> None:
         self.arr =self.arr[1:] # The array is updated such that index 1 ---> new index zero
         
+# Time Complexity : O(1)
 
     def top(self) -> int:
         return self.arr[0]
+# Time Complexity : O(n)
 
     def getMin(self) -> int:
         
