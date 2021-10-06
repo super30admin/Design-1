fd5e476a61225685237f501edbd5733cde308b2a
Spandana Edupuganti <spandanaedupuganti31@gmail.com>
2021-10-05 21:41:00 -0400

    Design-1 completed

--- Sample.java
+++ Sample.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 // Time Complexity :
 // Space Complexity :
 // Did this code successfully run on Leetcode :
@@ -5,3 +7,55 @@
 
 
 // Your code here along with comments explaining your approach
+class MinStack {
+
+    Stack<Node> stack;
+    public MinStack() {
+        stack=new Stack<>();
+    }
+    
+    public void push(int val) {
+     Node node=new Node();
+        if(!stack.isEmpty()){
+            Node n=stack.peek();
+            int prevMin=n.min;
+            if(val<prevMin){
+                node.min=val;
+            }else{
+                node.min=prevMin;
+            }
+        }else{
+             node.min=val;
+        }
+         node.element=val;
+         stack.push(node);     
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty()){
+            stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek().element;
+    }
+    
+    public int getMin() {
+        return stack.peek().min;
+    }
+}
+
+class Node{
+    int element;
+    int min;
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
