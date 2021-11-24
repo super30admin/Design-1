d8c3be6b3ce56ad13c7b4d324e5437c797ba5dd8
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-11-24 12:51:18 -0800

    HashSet

--- main.cpp
+++ main.cpp
@@ -2,48 +2,30 @@
 #include "stack"
 
 using namespace std;
-class MinStack {
+class MyHashSet {
 public:
-    std::stack<int> s;
-    stack<int> m;
-    MinStack() {
+    bool storage[1000001] = {false};
+    MyHashSet() {
+
     }
 
-    void push(int val) {
-        s.push(val);
-        if(m.empty() || val <= m.top()){
-            m.push(val);
-        }
+    void add(int key) {
+        storage[key] = true;
     }
 
-    void pop() {
-        int temp = s.top();
-        s.pop();
-        if(temp == m.top()){
-            m.pop();
-        }
+    void remove(int key) {
+        storage[key] = false;
     }
 
-    int top() {
-        if(!s.empty()){
-            return s.top();
-        }
-        return -1;
-    }
-
-    int getMin() {
-        if(!m.empty()){
-            return m.top();
-        }
-        return -1;
+    bool contains(int key) {
+        return storage[key];
     }
 };
 
 /**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
  */
