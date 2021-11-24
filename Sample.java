408030cef15f249bc36a37663dfc73406f7081eb
saipha06 <62794146+saipha06@users.noreply.github.com>
2021-11-24 16:17:06 -0500

    executed problem

--- Sample.java
+++ Sample.java
@@ -1,7 +1,50 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+import java.util.Stack;
+// Time Complexity : O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+
+class MinStack {
+
+    Stack<Integer> main_stack;
+    int min;
+        public MinStack() {
+            main_stack = new Stack<Integer>() ;
+            min = Integer.MAX_VALUE;
+        }
+        
+        public void push(int val) {
+            if(min>=val){
+                main_stack.push(min);
+                min = val;
+            }
+            main_stack.push(val);
+        }
+        
+        public void pop() {
+            if(min ==main_stack.pop()){
+                min = main_stack.pop();
+            }
+            
+        }       
+        
+        public int top() {
+            return main_stack.peek();
+        }
+        
+        public int getMin() {
+            return min;
+        }
+    }
+    
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.push(val);
+     * obj.pop();
+     * int param_3 = obj.top();
+     * int param_4 = obj.getMin();
+     */
