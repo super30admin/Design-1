fe984aa82c121a92f6450eae639026c2d0ca020f
Kedar <kedardhere@gmail.com>
2021-11-27 22:59:44 -0800

    Completed Design 1

--- Design-1_705_HashSet.py
+++ Design-1_705_HashSet.py
@@ -1,14 +1,17 @@
 class MyHashSet:
     """
     Approach:
-    1. Key will have values between 0 - 10^6, then we will take size of primary and                secondary array as square root of maximum value(10^6) i.e. 10^3
-    2. Declare an 2D boolean array Primary array will be initailized to null and Secondary          array will initialized to false  
-    3. First hashing function will decide Primary array index. Secondary function will give        the secondary array index
+    1. Key will have values between 0 - 10^6, then we will take size of primary and secondary array as square root of maximum value(10^6) i.e. 10^3
+    2. Declare an 2D boolean array Primary array will be initailized to null and Secondary array will initialized to false  
+    3. First hashing function will decide Primary array index. Secondary function will give the secondary array index
     4. First hashing function will return Reminder of key % bucket size
     5. Secondary hashing function will return division of key / bucket_item size
-    6. Add function will first check if the index returned by first hashing function is null        in primarary array or not. If it is null then create an array of bucketitem size and        make the value of index in secondary array value at the index returned by second            hashing function as index.
-    7. Remove function will first check Primary_index[null] then return nothing otherwise          make the Secondary_array[index] = false
+    6. Add function will first check if the index returned by first hashing function is null in primarary array or not. If it is null then create an array of bucketitem size and        make the value of index in secondary array value at the index returned by second            hashing function as index.
+    7. Remove function will first check Primary_index[null] then return nothing otherwise make the Secondary_array[index] = false
     8. returm array[primary_index][secondary=_index]
+
+    Time Complexity: O(1)
+    Space Complexity: O(1)
     """
     def __init__(self):
         self.bucket = 1000
