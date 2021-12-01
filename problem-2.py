c7b42bb0ab97e43eec74d5b70491bc5334ca8449
Munazzaruddin M <mdmunazzar@hotmail.com>
2021-12-01 01:55:59 -0600

    Design 1 solved

---
+++ problem-2.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.minStack=[]
+        self.min=float('inf')
+
+    def push(self, val: int) -> None:
+        if(self.min>=val):
+            self.minStack.append(self.min)
+            self.min=val
+        self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        popped=self.minStack.pop()
+        if(self.min==popped):
+            self.min=self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.minStack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min
+        
