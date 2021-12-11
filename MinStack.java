d702748acbcebc334b66421c67294817600d7970
Taranjit Kaur <taranjit@chegg.com>
2021-12-11 18:59:33 +0530

    Added the approach to both the solns.

--- MinStack.java
+++ MinStack.java
@@ -1,9 +1,15 @@
+// Time Complexity : O(1), considering, push, pop, peek as the constant operations in the library
+// Space Complexity : O(n), where are n is the max elements that are being pushed as we are using another stack to maintain min
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes, earlier I was missing out in one of the test cases,
+// after adding code from line 47-51 it was resolved.
+
 import java.util.Stack;
 
 class MinStack {
 
-    Stack<Integer> stack;
-    Stack<Integer> major;
+    Stack<Integer> minStack; // Contains the minimum element at any given point
+    Stack<Integer> major; // Represents the current state of stack
 
     int min;
 
@@ -12,44 +18,51 @@ class MinStack {
     }
 
     public MinStack() {
-        stack = new Stack<>();
+        minStack = new Stack<>();
         major = new Stack<>();
-        min = Integer.MAX_VALUE;
+        min = Integer.MAX_VALUE; // To represent the minimum element
     }
 
     public void push(int val) {
         if(major.isEmpty()) {
             min = Integer.MAX_VALUE;
         }
+
+        // If the current element is less than or equal to the min element, push in the min stack
         if(val <= min) {
             min = val;
-            stack.push(val);
+            minStack.push(val);
         }
+
+        // Push in the main stack
         major.push(val);
     }
 
     public void pop() {
+        // Remove the element from minStack as well, when we are removing the min element from the main stack
+        // So that the new min can be obtained from the minStack when asked
         if(min == major.peek()) {
-            stack.pop();
-
+            minStack.pop();
         }
 
-        if(!stack.isEmpty()){
-            min = stack.peek();
+        if(!minStack.isEmpty()){
+            // Need to update the new min, because the in might have changed as we are popping out the elements from
+            // minStack in the above conditions for certain cases.
+            min = minStack.peek();
         }
         major.pop();
     }
 
     public int top() {
         return major.peek();
-
     }
 
     public int getMin() {
-        if(stack.isEmpty()) {
-            return stack.peek();
+        if(!minStack.isEmpty()) {
+            // Return the top element from minStack
+            return minStack.peek();
         }
-
+        // Though below will not happen as it's given in the question that there will be valid operations only
         return Integer.MAX_VALUE;
     }
 }
--- MyHashMap.java
+++ MyHashMap.java
@@ -1,11 +1,13 @@
 // Time Complexity : O(max LL length)
-// Space Complexity : O(buckets) + O(LL length)
+// Space Complexity : O(buckets) + O(LL length), max length of the linked list can be 100 if there are 1-10^6 elements present
 // Did this code successfully run on Leetcode : Y
 // Any problem you faced while coding this : no
 
 
 class MyHashMap {
 
+    // Taking implementation with the Linked list to store the elements in case of collisions,
+    // this technique is called linear chaining
     static class Node {
         int key;
         int value;
@@ -17,24 +19,32 @@ class MyHashMap {
         }
     }
 
+    // Taking buckets as 10000 because the elements range can vary from 1-10^6
+    // square root of 10^6 is 10^3, we have taken 10^4 so that the max linked list length in case of collisions is not large but 100 at max
     int buckets = 10000;
 
+    // Taking simple hash function
     private int hashFunc(int key) {
         return key%buckets;
     }
 
+    // This is the main array of type Node as it will store the address of HEAD nodes of our LinkedList
     Node[] arr = new Node[buckets];
 
     private Node find(int key) {
+        // Find hash
         int index = hashFunc(key);
 
         Node prev;
+        // Case when the key doesn't exist, we return empty Node.
         if(arr[index] == null) {
             prev = new Node(-1, -1);
             arr[index] = prev;
             return prev;
         }
+        // If the key exist, find its head
         prev = arr[index];
+        //Traverse the LinkedList to find the element
         while(prev.next!=null) {
             if(prev.next.key == key) {
                 break;
@@ -68,7 +78,10 @@ class MyHashMap {
     }
 
     public void remove(int key) {
+        // Gives the node previous to the asked key, because we want to remove the current key, hence we need the previous node to move the pointers
         Node prev = find(key);
+        // prev.next contains the key asked to be removed
+        // Moving the pointer from next to next to remove the key element
         if(prev.next!=null) {
             prev.next = prev.next.next;
         }
