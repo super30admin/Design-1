abb0c38dc28514959a49f2eeafcfde26baf1ce67
ravirammamidi <tatsuya.s.mknr@gmail.com>
2021-12-21 12:26:03 -0700

    Completed Design 1: Please Review

--- design_hashmap.py
+++ design_hashmap.py
@@ -1,42 +1,19 @@
-# class MyHashSet:
-
-#     def __init__(self):
-#         self.arr = []
-
-#     def add(self, key: int) -> None:
-#         if key not in self.arr:
-#             self.arr.append(key)
-
-#     def remove(self, key: int) -> None:
-#         if key in self.arr:
-#             self.arr.remove(key)
-
-#     def contains(self, key: int) -> bool:
-#         if key in self.arr:
-#             return True
-#         else:
-#             return False
 
 class MyHashSet:
 
     def __init__(self):
-        self.arr = []
+        self.arr = bytearray(1000001)
 
     def add(self, key: int) -> None:
-        if key not in self.arr:
-            self.arr.append(key)
+        self.arr[key] = 1
 
     def remove(self, key: int) -> None:
-        if key in self.arr:
-            self.arr.remove(key)
-
+        self.arr[key] = 0 
+        
     def contains(self, key: int) -> bool:
-        if key in self.arr:
-            return True
-        else:
-            return False
-
-    
+        return self.arr[key]
+            
+ 
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
