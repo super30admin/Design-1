9ca650a0cd59f06ae480fda0f91a0345ab8eaa6b
Piyush Manglani <piyushmanglani@Piyushs-MBP.lan>
2021-12-21 10:32:25 -0500

    completed problem-2

--- Problem_2.py
+++ Problem_2.py
@@ -1,7 +1,7 @@
 # Time Complexity : O(n)
 # Space Complexity : O(1)
-# Did this code successfully run on Leetcode : YES
-# Any problem you faced while coding this : I was confused at first but eventually figured it out.
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : I was confused at first but figured out eventually
 
 class MinStack:
     def __init__(self):
