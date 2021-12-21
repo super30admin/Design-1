878e6ce1e36ffaa0d1276ab7455358f9f40bc461
shrivatsav <kumarshrivatsav98@gmail.com>
2021-12-21 17:44:14 -0600

    Fix error in problem1 question

--- Problem1.py
+++ Problem1.py
@@ -1,55 +1,39 @@
 # Time Complexity : O(1)
-# Space Complexity : O(k^2) where k is size of hash table choosen. Here 1000. 
-# Did this code successfully run on Leetcode : 27/32 test cases passed.
-# Any problem you faced while coding this : Implemented using 2nd hashing technique as taught in class.
+# Space Complexity : O(k * k). Here K = 1000
+# Did this code successfully run on Leetcode : Tested locally.
+# Any problem you faced while coding this : No
+
 class MyHashSet:
 
     def __init__(self):
-        self.set = [None] * 1000 # Choose 1000 as key size is 10^6
+        self.hash_set = [None] * 1000
+        
 
     def add(self, key: int) -> None:
-        initial_hash_value_position = key % 1000 # position to insert the key
-        if(self.set[initial_hash_value_position] == None):
-            self.set[initial_hash_value_position] = key
-        elif type(self.set[initial_hash_value_position]) == list: # in case of collision.
-            new_second_hash_temp = key // 1000
-            self.set[initial_hash_value_position][new_second_hash_temp] = key
-        else: # in case of 1st collision.
-            temp = self.set[initial_hash_value_position]
-            self.set[initial_hash_value_position] = [None] * 1000
-            second_hash_temp = temp // 1000
-            self.set[initial_hash_value_position][second_hash_temp] = temp
-            new_second_hash_temp = key // 1000
-            self.set[initial_hash_value_position][new_second_hash_temp] = key
+        first_key = key % 1000
+        second_key = key // 1000
+        if self.hash_set[first_key] == None:
+            self.hash_set[first_key] = [None] * 1001
+        self.hash_set[first_key][second_key] = key
         
+
     def remove(self, key: int) -> None:
-        hash_position = key % 1000
-        if hash_position == None:
-            return
-        else:
-            if type(self.set[hash_position]) != list:
-                self.set[hash_position] = None
-            else:
-                second_hash_map_key = key // 1000
-                self.set[hash_position][second_hash_map_key] = None
+        first_key = key % 1000
+        second_key = key // 1000
+        if self.hash_set[first_key] != None:
+            if self.hash_set[first_key][second_key] == key:
+                self.hash_set[first_key][second_key] = None
+        
 
     def contains(self, key: int) -> bool:
-        hash_position = key % 1000
-        if hash_position == None:
-            return False
-        else:
-            if type(self.set[hash_position]) != list:
-                if self.set[hash_position] == key:
-                    return True
-                else:
-                    return False
-            else:
-                second_hash_map_key = key // 1000
-                if(self.set[hash_position][second_hash_map_key]== key):
-                    return True
-                else:
-                    return False
-                
+        first_key = key % 1000
+        second_key = key // 1000
+        if self.hash_set[first_key] != None:
+            if(self.hash_set[first_key][second_key] == key):
+                return True
+        return False
+        
+
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
