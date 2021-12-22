a70767d6eb167c205f752a72b1fb246c87f6b303
PRAYAG-PATEL-1998 <ppatel94@asu.edu>
2021-12-21 18:52:32 -0700

    Completed Sprint Min_Stack S30 FAANMG Problem #2 {Medium}

--- Sample.java
+++ Sample.java
@@ -5,3 +5,70 @@
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * @auther = PRAYAG PATEL
+ * @problem = S30 FAANMG Problem #2 {Medium} / Leetcode 155. Min Stack {easy}
+ * @asked  = Microsoft
+ */
+
+
+
+import java.util.Stack;
+
+
+class MinStack {
+    public  Stack<Integer>stack;
+    public Stack<Integer>mini;
+    public MinStack() {
+        stack = new Stack<>();
+        mini = new Stack<>();        
+    }
+    
+    public void push(int val) {
+        if(stack.empty() || val<=mini.peek())
+        {
+            mini.push(val);
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if(stack.peek().equals(mini.peek()))
+        {
+            mini.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    
+        
+    }
+    
+    public int getMin() {
+        return mini.peek();
+        
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin(); // return -3
+        minStack.pop();
+        minStack.top();    // return 0
+        minStack.getMin(); // return -2
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
