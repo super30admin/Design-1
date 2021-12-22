e0b2616af1600265e598074facd4579f31a7f935
Jayavardhan Karampudi <jay@jais-mbp.3500.dhcp.asu.edu>
2021-12-22 09:19:00 -0700

    Design1 Problems Optimized approach Complete

--- Design1_Problem1.py
+++ Design1_Problem1.py
@@ -3,35 +3,79 @@
 # Did this code successfully run on Leetcode : yes
 # Any problem you faced while coding this : None
 
-# Your code here along with comments explaining your approach : Took a byte array to increase the efficiency and stored the boolean values for the value existance
+# Your code here along with comments explaining your approach : Took a 2D array to increase the efficiency and stored the boolean values for the value existance
 
+#2D array
 class MyHashSet(object):
     
     def __init__(self):
-        self.hashset = bytearray(1000001)
+        # self.hashset = bytearray(1000001)
+        self.hashset = [[] for i in range(1000)]
+        self.bucket = 1000
+        self.bucket2 = 1000
 
     def add(self, key):
         """
         :type key: int
         :rtype: None
         """
-        if not self.hashset[key]:
-            self.hashset[key] = 1
-
+        hash1 = key%self.bucket
+        if not self.hashset[hash1]:
+            if hash1 == 0:
+                self.hashset[hash1] = [0]*1001
+            else:
+                self.hashset[hash1] = [0]*1000
+        hash2 = key//self.bucket2
+        self.hashset[hash1][hash2] = 1
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
-        if self.hashset[key]:
-            self.hashset[key] = 0
+        hash1 = key%self.bucket
+        hash2 = key//self.bucket2
+        if self.hashset[hash1]:
+            self.hashset[hash1][hash2] = 0
+        # if self.hashset[key]:
+        #     self.hashset[key] = 0
 
     def contains(self, key):
         """
         :type key: int
         :rtype: bool
         """
-        return self.hashset[key]
+        hash1 = key%self.bucket
+        hash2 = key//self.bucket2
+        return False if not self.hashset[hash1] else self.hashset[hash1][hash2]
+
+#1D Array
+# class MyHashSet(object):
+
+#     def __init__(self):
+#         self.hashset = bytearray(1000001)
+
+#     def add(self, key):
+#         """
+#         :type key: int
+#         :rtype: None
+#         """
+#         if not self.hashset[key]:
+#             self.hashset[key] = 1
+
+#     def remove(self, key):
+#         """
+#         :type key: int
+#         :rtype: None
+#         """
+#         if self.hashset[key]:
+#             self.hashset[key] = 0
+
+#     def contains(self, key):
+#         """
+#         :type key: int
+#         :rtype: bool
+#         """
+#         return self.hashset[key]
 
 
 # Your MyHashSet object will be instantiated and called as such:
--- Design1_Problem2.py
+++ Design1_Problem2.py
@@ -1,50 +1,87 @@
-# Time Complexity : O(1) for getting Min and push, O(n) for pop (because I am searching for the next min value if the popped element is the already existing minimum value)
+# Time Complexity : O(1) for getting Min and push and pop
 # Space Complexity : O(n)
 # Did this code successfully run on Leetcode : yes
 # Any problem you faced while coding this : None
 
 # Your code here along with comments explaining your approach : a min value where I compare with the already existing minimum value and update it accordingly
 
+#Optimized
 import sys
-
 class MinStack(object):
 
     def __init__(self):
-        self.min_stack = []
-        self.min = sys.maxint
+        self.stack = []
+        self.min_val = sys.maxint
 
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
-        self.min_stack.append(val)
-        if self.min > val:
-            self.min = val
+        if val <= self.min_val:
+            self.stack.append(self.min_val)
+            self.min_val = val
+        self.stack.append(val)
 
     def pop(self):
         """
         :rtype: None
         """
-        top = self.min_stack[-1]
-        self.min_stack.pop()
-        if top == self.min:
-            self.min = sys.maxint
-            for ind in range(len(self.min_stack)):
-                if self.min > self.min_stack[ind]:
-                    self.min = self.min_stack[ind]
+        if self.min_val == self.stack.pop():
+            self.min_val = self.stack[-1]
+            self.stack.pop()
 
     def top(self):
         """
         :rtype: int
         """
-        return self.min_stack[-1]
+        return self.stack[-1]
         
     def getMin(self):
         """
         :rtype: int
         """
-        return self.min
+        return self.min_val
+
+
+
+#BruteForce
+# class MinStack(object):
+
+#     def __init__(self):
+#         self.stack = []
+#         self.min_stack = [sys.maxint]
+
+#     def push(self, val):
+#         """
+#         :type val: int
+#         :rtype: None
+#         """
+#         self.stack.append(val)
+#         if self.min_stack[-1] > val:
+#             self.min_stack.append(val)
+#         else:
+#             self.min_stack.append(self.min_stack[-1])
+
+#     def pop(self):
+#         """
+#         :rtype: None
+#         """
+#         self.stack.pop()
+#         # if top == self.min_stack[-1]:
+#         self.min_stack.pop()
+
+#     def top(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.stack[-1]
+        
+#     def getMin(self):
+#         """
+#         :rtype: int
+#         """
+#         return self.min_stack[-1] if self.min_stack[-1] != sys.maxint else 0
 
 
 # Your MinStack object will be instantiated and called as such:
