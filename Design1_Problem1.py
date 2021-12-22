ef49a4079505fc3e122c38c1bd39ec3e607fdc87
Jayavardhan Karampudi <jay@jais-mbp.3500.dhcp.asu.edu>
2021-12-22 11:10:42 -0700

    Design1 Hashmap problem complete

--- Design1_Problem1.py
+++ Design1_Problem1.py
@@ -5,28 +5,42 @@
 
 # Your code here along with comments explaining your approach : Took a 2D array to increase the efficiency and stored the boolean values for the value existance
 
-#2D array
-class MyHashSet(object):
-    
+
+#2D Array
+class MyHashMap(object):
+
     def __init__(self):
-        # self.hashset = bytearray(1000001)
         self.hashset = [[] for i in range(1000)]
         self.bucket = 1000
         self.bucket2 = 1000
 
-    def add(self, key):
+
+
+    def put(self, key, value):
         """
         :type key: int
+        :type value: int
         :rtype: None
         """
         hash1 = key%self.bucket
         if not self.hashset[hash1]:
             if hash1 == 0:
-                self.hashset[hash1] = [0]*1001
+                self.hashset[hash1] = [-1]*1001
             else:
-                self.hashset[hash1] = [0]*1000
+                self.hashset[hash1] = [-1]*1000
         hash2 = key//self.bucket2
-        self.hashset[hash1][hash2] = 1
+        self.hashset[hash1][hash2] = value
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        hash1 = key%self.bucket
+        hash2 = key//self.bucket2
+        return -1 if not self.hashset[hash1] else self.hashset[hash1][hash2]
+
     def remove(self, key):
         """
         :type key: int
@@ -35,32 +49,32 @@ class MyHashSet(object):
         hash1 = key%self.bucket
         hash2 = key//self.bucket2
         if self.hashset[hash1]:
-            self.hashset[hash1][hash2] = 0
-        # if self.hashset[key]:
-        #     self.hashset[key] = 0
-
-    def contains(self, key):
-        """
-        :type key: int
-        :rtype: bool
-        """
-        hash1 = key%self.bucket
-        hash2 = key//self.bucket2
-        return False if not self.hashset[hash1] else self.hashset[hash1][hash2]
+            self.hashset[hash1][hash2] = -1
+        
 
 #1D Array
-# class MyHashSet(object):
+# class MyHashMap(object):
 
 #     def __init__(self):
-#         self.hashset = bytearray(1000001)
+#         self.hashset = [None] * (1000001)
 
-#     def add(self, key):
+
+#     def put(self, key, value):
 #         """
 #         :type key: int
+#         :type value: int
 #         :rtype: None
 #         """
 #         if not self.hashset[key]:
-#             self.hashset[key] = 1
+#             self.hashset[key] = value
+        
+
+#     def get(self, key):
+#         """
+#         :type key: int
+#         :rtype: int
+#         """
+#         return self.hashset[key]
 
 #     def remove(self, key):
 #         """
@@ -68,18 +82,43 @@ class MyHashSet(object):
 #         :rtype: None
 #         """
 #         if self.hashset[key]:
-#             self.hashset[key] = 0
+#             self.hashset[key] = -1
 
-#     def contains(self, key):
+#stupid answer
+# class MyHashMap(object):
+
+#     def __init__(self):
+#         self.hashmap = {}
+
+#     def put(self, key, value):
 #         """
 #         :type key: int
-#         :rtype: bool
+#         :type value: int
+#         :rtype: None
 #         """
-#         return self.hashset[key]
+#         self.hashmap[key] = value
+        
+
+#     def get(self, key):
+#         """
+#         :type key: int
+#         :rtype: int
+#         """
+#         return self.hashmap[key] if key in self.hashmap else -1
+        
+
+#     def remove(self, key):
+#         """
+#         :type key: int
+#         :rtype: None
+#         """
+#         if key in self.hashmap:
+#             del self.hashmap[key]
+        
 
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
 # obj.remove(key)
-# param_3 = obj.contains(key)
