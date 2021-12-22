c29c9bd69045b40a6a49b1935b8acb50112c1d05
ravirammamidi <tatsuya.s.mknr@gmail.com>
2021-12-22 12:57:43 -0700

    Update to design hashmap solution

--- design_hashmap.py
+++ design_hashmap.py
@@ -1,31 +1,45 @@
-
-
-class MyHashSet:
+class MyHashMap:
 
     def __init__(self):
-        self.arr = [ [] for i in range(1000) ]
-        self.h1 = 1000
-        self.h2 = 1000
+        self.bucket1 = 1000
+        self.bucket2 = 1000
+        self.arr = [[] for i in range(1000)]
+        
 
-    def add(self, key: int) -> None:
-        hash1 = key % self.h1
-
+    def put(self, key: int, value: int) -> None:
+        
+        hash1 = key % self.bucket1
+        hash2 = key // self.bucket2
+        
         if not self.arr[hash1]:
             if hash1 == 0:
-                self.arr[hash1] = [0]*1001
+                self.arr[hash1] = [-1 for i in range(1001)]
             else:
-                self.arr[hash1] = [0]*1000
-        hash2 = key // self.h2
-        self.arr[hash1][hash2] = 1            
+                self.arr[hash1] = [-1 for i in range(1000)]
+                
+        self.arr[hash1][hash2] = value 
+            
+
+    def get(self, key: int) -> int:
+        
+        hash1 = key % self.bucket1
+        hash2 = key // self.bucket2
+        if self.arr[hash1]:
+            return self.arr[hash1][hash2]
+        else:
+            return -1
 
     def remove(self, key: int) -> None:
-        hash1 = key % self.h1 
-        hash2 = key // self.h2 
+        hash1 = key % self.bucket1
+        hash2 = key // self.bucket2
         if self.arr[hash1]:
-            self.arr[hash1][hash2] = 0
+            self.arr[hash1][hash2] = -1
+        
 
-    def contains(self, key: int) -> bool:
-        hash1 = key % self.h1 
-        hash2 = key // self.h2 
-        return False if not self.arr[hash1] else self.arr[hash1][hash2] 
-            
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
+
