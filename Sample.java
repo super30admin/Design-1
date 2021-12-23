61e8f4f1a173190343dcfb572045ea91de555e31
gargisapkal <gargimore196@gmail.com>
2021-12-22 20:48:41 -0500

    Completed Min Stack

--- Sample.java
+++ Sample.java
@@ -1,7 +1,42 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
+import java.util.Stack;
 
 // Your code here along with comments explaining your approach
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    int min;
+    public MinStack() {
+        stack = new Stack<>(); 
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+        minStack.push(min);
+    }
+    
+    public void push(int val) {
+    min= Math.min(min,val);
+       if(val <= min){
+           stack.push(val);
+           min = val;
+           minStack.push(min);
+       } else{
+           stack.push(val);
+           minStack.push(min);
+       }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+        min = minStack.peek();
+        
+    }
+    
+    public int top() {
+       return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
