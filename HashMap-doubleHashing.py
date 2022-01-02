79ee90b212479df3e61f82a77f5edc2d40001f55
Ami Doshi <ami.doshi@outlook.com>
2022-01-02 03:45:33 -0800

    Update HashMap-doubleHashing.py

--- HashMap-doubleHashing.py
+++ HashMap-doubleHashing.py
@@ -1,70 +1,73 @@
-# -*- coding: utf-8 -*-
+# Methodology --> 
+#Primary hash table is an array. All initialized to None initially, once visited, each one is then initialized to a bucket (an array). We traverse the bucket to get, update, remove the value
 
-class MyHashMap:
+#Time Complexity - O(N) n-number of keys in a bucket
+#Space Complexity - O(K) k-number of keys 
 
+class Bucket:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.primary_array_size=1000 ## this is the length of the primary array
-        self.bucket_size=1000 ##these are the number of items in each array
+        self.bucket = []
         
-        self.primary_array=[[None] for _ in range (self.primary_array_size)]
+    def putValInBucket(self, key:int,val: int):
+        i = 0
+        #found = False
+        for i,key_val in enumerate(self.bucket):
+            if key_val[0] == key:
+                self.bucket[i]= (key,val)
+                return
+            
+        #if not found:
+        self.bucket.append((key,val))
 
-    def primary_array_f(self, key):
-        return key % self.primary_array_size
+    def getValue(self, key:int):
+        for i,key_val in enumerate(self.bucket):
+            if key_val[0] == key:
+                return key_val[1] 
+        return -1
     
-    def bucketitem(self, key: int) -> int:
-       # print(key%self.bucket_size,key,self.bucket_size)
+    def removeValue(self, key:int):
+        for i,key_val in enumerate(self.bucket):
+            if key_val[0] == key:
+                del self.bucket[i]
+    
+    
+class MyHashMap:
+    # we need to balance between hashkey and bucket size
+    
+    def __init__(self):
+        self.bucket_size = 10000
+        self.hashTable = [[None] for i in range(self.bucket_size)]
+        #print(self.hashTable)
+
+    def get_Hash_table_index(self, key:int):
         return key%self.bucket_size
     
-    
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
-       # print("put-->",value)
         
-        primary_array_index = self.primary_array_f(key)
-        #print(primary_array_index)
+        #finding bucket
+        hash_index = self.get_Hash_table_index(key)
         
-        bucket_index = self.bucketitem(key)
-        #print(bucket_index)
-            
-        if(self.primary_array[primary_array_index]==[None]):
-            if(primary_array_index == 0):
-                self.primary_array[primary_array_index] = [[None] for i in range (self.bucket_size+1)]
-            else:
-                self.primary_array[primary_array_index] = [[None] for i in range (self.bucket_size)]
-
-        self.primary_array[primary_array_index][bucket_index] = value
-     #   print(primary_array_index,bucket_index)
+        if self.hashTable[hash_index] == [None]: #Not Visited
+            self.hashTable[hash_index] = Bucket() #Make it visited
+       
+        self.hashTable[hash_index].putValInBucket(key,value)
+        
         
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        primary_array_index = self.primary_array_f(key)
-        bucket_index = self.bucketitem(key)
-      #  print("get -->",key)
-     #   print(primary_array_index,bucket_index)
-        if(self.primary_array[primary_array_index]==[None]):
-   #         print("if1")
+        #finding bucket
+        hash_index = self.get_Hash_table_index(key)
+        
+        if self.hashTable[hash_index] == [None]: #Not Visited
             return -1
-        elif self.primary_array[primary_array_index][bucket_index] == False:
-    #        print("if2")
-            return -1
-        else:
-            return self.primary_array[primary_array_index][bucket_index]
+        return self.hashTable[hash_index].getValue(key)
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        primary_array_index = self.primary_array_f(key)
-        bucket_index = self.bucketitem(key)
-        if(self.primary_array[primary_array_index]!=[None]) and (self.primary_array[primary_array_index][bucket_index] == True):
-            self.primary_array[primary_array_index][bucket_index] = False
+        hash_index = self.get_Hash_table_index(key)
+        
+        if self.hashTable[hash_index] == [None]: #Not Visited
+            return
+        return self.hashTable[hash_index].removeValue(key)
+        
 
 
 # Your MyHashMap object will be instantiated and called as such:
