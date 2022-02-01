d535c46cf998bc50a6b22b4edc127f509d5c0840
Sunidhi Kabra <kabra.sunidhi14.com>
2022-01-31 20:44:59 -0500

    Completed MinStack Implementation

--- Sample.java
+++ Sample.java
@@ -1,7 +1,54 @@
 // Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//push, pop, peek, top, getMin: O(1)
+
+// Space Complexity : O(n) for n elements in the stack
+
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+class MinStack {
+  class Node {
+      int data;
+      int min;
+      Node next;
+      
+      Node(int data, int min){
+          this.data = data;
+          this.min = min;
+          this.next = null;
+      }
+  }
+
+  Node head;
+  
+  public MinStack() {
+  }
+  
+  public void push(int val) {
+      
+      if(head == null) {
+          Node node = new Node(val, val);
+          head = node;
+      } else{
+          Node node = new Node(val, Math.min(head.min, val));
+          node.next = head;
+          head = node;
+      }
+  }
+  
+  public void pop() {
+      if(head != null) {      
+          head = head.next;    
+      }
+  }
+  
+  public int top() {
+      return head.data;
+  }
+  
+  public int getMin() {
+      return head.min;
+  }
+}
