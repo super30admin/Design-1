dee879d6ac100c54585c83f13ff94c4e12c2f494
nevilshah444 <nevilshah444@gmail.com>
2022-02-01 19:50:10 -0800

    Commit 2

--- HashMap.py
+++ HashMap.py
@@ -4,20 +4,16 @@
 class MyHashMap:
 
     def __init__(self):
-        self.dict = {} #defining a dictionary    
+        self.size = 100000
+        self.dict = [-1] * self.size#defining a list   
 
     def put(self, key: int, value: int) -> None:
         self.dict[key] = value # inserting the key-value pair in dictionary
 
     def get(self, key: int) -> int:
-        if key in self.dict:# if the key is present
-            return self.dict[key]  # return the element
-        return -1 # if the key is not present
-
+        return self.dict[key]
     def remove(self, key: int) -> None:
-        if key in self.dict:
-            self.dict.pop(key)# deleting the key-value pair inside a dictionary
-        
+        self.dict[key] = -1
         
 
 
