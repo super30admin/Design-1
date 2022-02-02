ce43498a8dd4354bfea0bef20581947385315709
Vibha Dhar <dharvibha@unknown968a4a980979.attlocal.net>
2022-02-01 19:39:10 -0600

    Added the minstack logic

--- MinStack.java
+++ MinStack.java
@@ -1,85 +1,55 @@
-public class MinStack()
-{
+public class MinStack {
+    Stack<Integer> minStack;
+    Stack<Integer> mainStack;
+    public MinStack() {
+
+        minStack = new Stack<>();
+        mainStack = new Stack<>();
+    }
+
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val)
+        {
+            minStack.push(val);
+        }
+
+
+    }
+
+    public void pop() {
+
+        if (!mainStack.isEmpty())
+        {
+            int currentVal = mainStack.peek();
+            if (currentVal == minStack.peek())
+            {
+                minStack.pop();
+            }
+            mainStack.pop();
+        }
+    }
+
+    public int top() {
+        int top = mainStack.peek();
+        return top;
+    }
+
+    public int getMin() {
+
+        int min =  minStack.peek();
+        return min;
+    }
+
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-//public class MinStack {
-//    int top ;
-//    int minValueTop;
-//    static final int MAX=1000;
-//    // List minStack = new ArrayList<Integer>();
-//    // List minValueStack = new ArrayList<Integer>();/**/
-//    int minStack[] = new int[MAX];
-//    int minValueStack[] = new int[MAX];
-//    int minValue;
-//    public MinStack() {
-//        int top=-1;
-//        int minValueTop =-1;
-//    }
-//
-//
-//    public void push(int val) {
-//        if (top >= MAX-1)
-//        {
-//            System.out.println("Stack Overflow");
-//        }
-//        else{
-//            top= top+1;
-//            minStack[top] = val;
-//
-//            if (minValueStack[minValueTop] > val)
-//            {
-//                minValueTop = minValueTop+1;
-//                minValueStack[minValueTop] = val;
-//            }
-//        }
-//    }
-//
-//    public void pop() {
-//        if (top < -1)
-//        {
-//            System.out.println("Stack underflow");
-//
-//        }
-//        else{
-//            int valuePopped = minStack[top];
-//            top = top-1;
-//            if(minValueStack[minValueTop] == valuePopped)
-//            {
-//                minValueTop = minValueTop-1;
-//            }
-//        }
-//    }
-//
-//    public int top() {
-//        return minStack[top];
-//    }
-//
-//    public int getMin() {
-//
-//        int min =  minValueStack[minValueTop];
-//        return min;
-//    }
-//
-//
-//}
-//
-///**
-// * Your MinStack object will be instantiated and called as such:
-// * MinStack obj = new MinStack();
-// * obj.push(val);
-// * obj.pop();
-// * int param_3 = obj.top();
-// * int param_4 = obj.getMin();
-// */
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
