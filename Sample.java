726f00df6ed906d3b889382eaa95f52a2522b096
Hitesh Purshotam Dayaramani <hdayaram@tibco.com>
2022-03-01 17:30:03 +0530

    Min Stack

--- Sample.java
+++ Sample.java
@@ -1,7 +1,53 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :Few Silly mistakes
 
 
 // Your code here along with comments explaining your approach
+
+class MinStack {
+    int min=Integer.MAX_VALUE;
+    int top=-1;
+    ArrayList<Integer> stack;
+    static Stack<Integer> minStack;
+    public MinStack() {
+        stack = new ArrayList<Integer>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        top++;
+        if(min>=val){
+            min=val;
+            minStack.push(val);
+        }
+        stack.add(val);
+    }
+
+    public void pop() {
+        //System.out.println(stack.get(top)+" result "+minStack.peek());
+        if(Objects.equals(stack.get(top), minStack.peek())){
+            //System.out.println("poped");
+            minStack.pop();
+            if(minStack.isEmpty()){
+                min=Integer.MAX_VALUE;
+            }else
+                min=minStack.peek();
+
+        }
+
+        stack.remove(top);
+        top--;
+    }
+
+    public int top() {
+        return stack.get(top);
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
+
