3ef6d78410fe1ec80acc473c0c3ea70b8ed6c1f5
Krina Patel <kp42942n@pace.edu>
2022-03-02 21:39:32 -0500

    Problem 2 Design HashMap completed

--- minStack.js
+++ minStack.js
@@ -1,50 +1,50 @@
-// Time Complexity : Time complexity is provided after each functions
-
+// Time Complexity : O(1)
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes code ran successfully on Leetcode
+// Any problem you faced while coding this : Not an issue exactly but I couldn't figure out the way of having O(1)
 
-// Any problem you faced while coding this : Not an issue exactly but I couldn't figure out the way of having O(1) 
 // time complexity for getMin function
 
-var MinStack = function() {
-    this.stack = []
-    this.currentIndex = 0    
+var MinStack = function () {
+  this.stack = [];
+  this.currentIndex = 0;
 }; //O(1)
 
-/** 
+/**
  * @param {number} val
  * @return {void}
  */
-MinStack.prototype.push = function(val) {
-    this.stack[this.currentIndex] = val;
-    this.currentIndex++;
+MinStack.prototype.push = function (val) {
+  this.stack[this.currentIndex] = val;
+  this.currentIndex++;
 }; //O(1)
 
 /**
  * @return {void}
  */
-MinStack.prototype.pop = function() {
-    this.currentIndex -= 1;
+MinStack.prototype.pop = function () {
+  this.currentIndex -= 1;
 }; //O(1)
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function() {
-    return this.stack[this.currentIndex-1]
+MinStack.prototype.top = function () {
+  return this.stack[this.currentIndex - 1];
 }; //O(1)
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function() {
-    let min = 99999999999999999999;
-    for (let i = this.currentIndex - 1; i >= 0; i--) {
-        min = this.stack[i] <= min ? this.stack[i] : min
-    }
-    return min;
+MinStack.prototype.getMin = function () {
+  let min = 99999999999999999999;
+  for (let i = this.currentIndex - 1; i >= 0; i--) {
+    min = this.stack[i] <= min ? this.stack[i] : min;
+  }
+  return min;
 }; //O(n)
 
-/** 
+/**
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
  * obj.push(val)
