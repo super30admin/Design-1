0288c9c7c2c9f49c3173b4a22a031eb1ada3516e
Shrutika Mokashi <mokashi.shrutika12345@gmail.com>
2022-03-04 00:34:54 -0500

    Completed Day 2 Homework

---
+++ Problem2.java
@@ -0,0 +1,41 @@
+class MinStack {
+    private Stack<Integer> st;
+    private int min;
+
+    public MinStack() {
+        this.st = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if (min >= val) {
+            st.push(min);
+            min = val;
+        }
+        st.push(val);
+    }
+
+    public void pop() {
+        if (min == st.pop()) {
+            min = st.pop();
+        }
+
+    }
+
+    public int top() {
+        return st.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
