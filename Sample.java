cdb1ebf01f4b556c3e938df3343a6d91aed4ddf1
priyank <ppatel631@gmail.com>
2022-03-29 17:53:41 -0400

    Completed Design-1

--- Sample.java
+++ Sample.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 // Time Complexity :
 // Space Complexity :
 // Did this code successfully run on Leetcode :
@@ -5,3 +9,59 @@
 
 
 // Your code here along with comments explaining your approach
+
+class myBucket{
+    int MAX_VALUE = 100000;
+    int array_size =100;
+    List<List<Integer>> parentList;
+
+    public myBucket(){
+        parentList = new ArrayList<>(array_size);
+        for(int i=0;i<array_size;i++){
+            parentList.add(null);
+        }
+    }
+
+    public void add(int key){
+        int index = key % array_size;
+        List<Integer> childList = parentList.get(index);
+        if(childList == null){
+            List<Integer> list = new LinkedList<>();
+            list.add(key);
+            parentList.set(index, list);
+        }
+        else{
+            if(!childList.contains(key)){
+                childList.add(key);
+            }
+        }
+    }
+
+    public void remove(int key){
+        int index = key % array_size;
+        List<Integer> childList = parentList.get(index);
+        if(childList != null){
+            childList.remove(Integer.valueOf(index));
+        }
+    }
+
+    public boolean contains(int key){
+        boolean res = false;
+        int index = key % array_size;
+        List<Integer> childList = parentList.get(index);
+        if(childList != null && childList.contains(key)){
+            res = true;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        myBucket mb = new myBucket();
+        mb.add(1);
+        mb.add(2);
+        System.out.println(mb.contains(1));
+        System.out.println(mb.contains(3));
+        mb.add(2);
+        System.out.println(mb.contains(2));
+    }
+}
