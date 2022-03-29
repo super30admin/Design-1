a45fd29b5f31078d0c116f86b7cd3981f06287fa
adityamulik <aditya.mulik@gmail.com>
2022-03-29 18:25:01 +0530

    Boilerplate code

---
+++ MinStack.java
@@ -0,0 +1,32 @@
+public class MinStack {
+    /*
+     * Time Complexity:
+     * Space Complexity: 
+     * Author: Aditya Mulik
+     */
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+	
+    }
+}
