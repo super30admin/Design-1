46f304d0f679c895185bac5ca41e127ac38c1ca9
versha-khare <vershakhare19@gmail.com>
2022-04-01 11:31:19 -0500

    Design1 - HashMap

--- Sample.java
+++ Sample.java
@@ -1,3 +1,71 @@
+
+//Design HasMap(https://leetcode.com/problems/design-hashmap/)
+// Time Complexity :O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
+
+
+//Solved using double hash function
+class MyHashMap {
+
+    int keybuckets = 1000;
+    int bucketvalues = 1001;
+    int[][] storage;
+    public MyHashMap() {
+        storage = new int[keybuckets][bucketvalues];
+        for(int i=0;i<=keybuckets-1;i++){
+            for(int j=0;j<=keybuckets-1;j++){
+                storage[i][j]=-1;
+            }
+        }
+    }
+    private int keybucket(int key){
+        return key%keybuckets;
+    }
+    private int bucketvalue(int key){
+        return key/bucketvalues;
+    }
+    public void put(int key, int value) {
+        int keybucket = keybucket(key);
+        int bucketvalue = bucketvalue(key);
+       
+            storage [keybucket][bucketvalue] = value;
+        
+    }
+    
+    public int get(int key) {
+        int keybucket = keybucket(key);
+        int bucketvalue = bucketvalue(key);
+       
+        return storage [keybucket][bucketvalue];
+        
+    }
+    
+    public void remove(int key) {
+        int keybucket = keybucket(key);
+        int bucketvalue = bucketvalue(key);
+        storage [keybucket][bucketvalue] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
+
+
+
+
+
+
+
+
+
+
 //Design MinStack (https://leetcode.com/problems/min-stack/)
 // Time Complexity :O(1)
 // Space Complexity :O(n)
