c1b005924c9ddb710510e04322a75309e2ced41c
antonysebastian197 <antonysebastian197@gmail.com>
2022-04-02 05:10:31 -0700

    Completed

--- DesignHashMap.py
+++ DesignHashMap.py
@@ -3,7 +3,7 @@
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
-
+# Can be further optimised by sorting the linkedlist possibly using a previous pointer
 class Node(object):
     def __init__(self,key,value):
         self.key = key
