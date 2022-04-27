5fa8904a1bbc216144003fab4d81b2a47bebb78a
suresh-mula <89316778+suresh-mula@users.noreply.github.com>
2022-04-26 17:20:45 -0700

    Solved Design-1 Assignment

--- MinStack.java
+++ MinStack.java
@@ -1,47 +1,99 @@
-// Time Complexity : O(n)
+// Time Complexity : O(1)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : YES
 // Any problem you faced while coding this : NO
 
-import java.util.PriorityQueue;
-import java.util.Stack;
-
+import java.util.Arrays;
 class MinStack {
-    //Creating Two Datastructure
+    int[] arr;
+    int[] orderedArr;
+    int MAX_SIZE = 1000;
+    //To keep track of number of elements in the arr
+    int counter = 0;
 
-    //PriorityQueue is created, to store elements in ascending order
-    PriorityQueue<Integer> priorityQueue;
-    //Stack is created, to store elements in sequencail order that user inserted
-    Stack<Integer> stack;
-
-    //Initializing PriorityQueue and Stack
     public MinStack() {
-        this.priorityQueue = new PriorityQueue<>();
-        this.stack = new Stack<>();
+        arr = new int[MAX_SIZE];
+        orderedArr = new int[MAX_SIZE];
+        Arrays.fill(arr, -1);
+        Arrays.fill(orderedArr, -1);
     }
     
 
     //Adding Elements
     public void push(int val) {
-        priorityQueue.add(val);
-        stack.push(val);
+        if(isFull())
+            return;
+        arr[counter] = val;
+        orderedArr[counter] = val;
+        sort(val);
+        counter++;
     }
     
-    //Deleting elemnets
+    //Moving the newly added element to the correct position
+    private void sort(int val) {
+        for(int i = counter-1; i>=0; i--){
+            if(orderedArr[i] > orderedArr[i+1]){
+                int temp = orderedArr[i];
+                orderedArr[i] = orderedArr[i+1];
+                orderedArr[i+1] = temp; 
+            }
+            else{
+                return;
+            }
+        }
+    }
+
+
+    //Decrementing counter 
     public void pop() {
-        //First Deleting the top element in the queue and storing the value
-        int value = stack.pop();
-        //Deleting the same element in the PriorityQueue by passing value as parameter
-        priorityQueue.remove(value);
+        if(isEmpty())
+            return;
+        delete(arr[counter-1]);
+        counter--;
     }
     
-    //Retriving top element from stack
+    //Deleting top element in the orderedArr
+    public void delete(int val){
+        for(int i = 0; i<=counter; i++){
+            if(val == orderedArr[i]){
+                swap(i);
+            }
+        }
+    }
+    
+    //After deleting top element, need move all elements to left in orderededArr
+    public void swap(int index){
+        for(int i = index; i < counter;i++){
+            orderedArr[i] = orderedArr[i+1];
+        }
+        orderedArr[counter] = -1;
+    }
+    
+    //Retriving element at (counter-1)th index of arr
     public int top() {
-        return stack.peek();
+        if(isEmpty())
+            return -1;
+        int val = arr[counter - 1];
+        return val;
     }
     
-    //Retriving top element from PriorityQueue results in getting minimum value
+    //Retriving element at 0th index of orderedArr
     public int getMin() {
-        return priorityQueue.peek();
+        if(isEmpty())
+            return -1;
+        int val = orderedArr[0];
+        return val;
+    }
+
+    public boolean isFull(){
+        if(counter >= MAX_SIZE)
+            return true;
+        return false;
+    }
+
+    public boolean isEmpty(){
+        if(counter == 0)
+            return true;
+        return false;
     }
 }
