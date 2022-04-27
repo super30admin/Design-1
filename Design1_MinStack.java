0e28ea495060993270f691e8a9e3a7d3a33a4bb2
Rutuja Mandlik <mandlik.r@northeastern.edu>
2022-04-27 01:29:57 -0400

    Min Stack - Updated

--- Design1_MinStack.java
+++ Design1_MinStack.java
@@ -1,43 +1,43 @@
-/* Time Complexity: O(1) */
-/* Space Complexity: O(n) */
-/* Did this code successfully run on Leetcode : Yes */
-/* Any problem you faced while coding this : I faced some syntactical and compilation errors. Also i had to take help from the google solution to proceed with the problem*/
+//TC: O(1) -- each opr
+//SC: O(n)
 
 
 class MinStack {
     
-    /*initialized 2 stacks. stack1 is the main stack where all values will be pushed. stack2 is the auxilary stack to store minimum values */
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
+    Stack<Integer> st;
+    int min;
 
-    /*contructor*/
     public MinStack() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+        this.st =  new Stack<>();
+        this.min = Integer.MAX_VALUE;
     }
     
     public void push(int val) {
-        stack1.push(val);       /* Pushing the values in stack1 */
-        if(stack2.isEmpty()){   /*  Checking is stack2 is empty  if yes then it will simply push the value in the stack else  value pushed will be the min value */
-            stack2.push(val);
+        if(min >= val){
+            //prev min push to st (only if changing or equal)
+            st.push(min);
+            
+            //reset min val
+            min = val;
         }
-        else{
-            stack2.push(Math.min(stack2.peek(),val));
-        }
-        
+        //always pushing elemnt on top 
+        st.push(val);
     }
     
-    public void pop() { /* Pops values from both stack */
-        stack1.pop();
-        stack2.pop();
+    public void pop() {
+        if(min == st.pop() ){
+            //reset min to prev min and pop it
+            min = st.pop();
+        }          
+            
     }
     
-    public int top() { /* Peeps values from stack1 */
-        return stack1.peek();
+    public int top() {
+      return  st.peek();
     }
     
-    public int getMin() { /* Returns min value from stack2 */
-        return stack2.peek();
+    public int getMin() {
+        return min; //global min reset to curr min always
     }
 }
 
