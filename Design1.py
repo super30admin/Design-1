7c2f15c235ce64878ee2118fec413eefbebf082a
Ruthwik12 <76807404+Ruthwik12@users.noreply.github.com>
2022-05-25 09:55:57 -0700

    Update Design1.py

--- Design1.py
+++ Design1.py
@@ -7,9 +7,9 @@ class MinStack:
 
   def push(self, val: int) -> None:
     if len(self.l) <= self.size:
-        self.l.append([val, min(val, self.m)])
-    else:    
-        print("Stack is at its capacity")
+      self.l.append([val, min(val, self.m)])
+    else:
+      print("Stack is at its capacity")
 
 
   def pop(self) -> None:
@@ -30,3 +30,18 @@ class MinStack:
       return self.l[-1][1]
     else:
       return float("inf")
+    
+  def isFull(self) -> int:
+    if(len(self.l) == size):
+      print("Stack is Full")
+    else:
+      print("Stack is not Full")
+   
+      
+  def isEmpty(self) -> int:
+    if( not len(self.l)):
+      print("Stack is Empty")
+    else:
+      print("stack is not Empty")
+  
+  
