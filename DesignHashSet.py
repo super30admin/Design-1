ad4efb91301b600ccd38548191b70e7ce6b73353
Mukta Parab <78190285+mukta2695@users.noreply.github.com>
2022-05-25 10:46:44 -0400

    Update DesignHashSet.py

--- DesignHashSet.py
+++ DesignHashSet.py
@@ -1,13 +1,28 @@
+'''
+Time Complexity : O(1)
+Space Complexity : O(n)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach'''
 class MyHashSet(object):
 
     def __init__(self):
+        #Primary list len
         self.buckets=1000
+        
+        #Secondary List len
         self.bucketItems=1000
+        
+        #Primary list
         self.table=[None]*self.buckets
         
+    #Hash function to check the index into the primary list
     def hash_function1(self,key):
         return key % self.buckets
     
+    #Hash function to check the index into the secondary list
     def hash_fucntion2(self,key):
         return key/self.bucketItems
 
@@ -16,12 +31,19 @@ class MyHashSet(object):
         :type key: int
         :rtype: None
         """
+        #Get primary index
         bucket_val=self.hash_function1(key)
+        
+        #Get secondary index
         buckectItems_val=self.hash_fucntion2(key)
+        
+        #If secondary list exists and if the element is not already present then set the secondary index to True
         if self.table[bucket_val] is not None:
             if self.contains(key)==False:
                 self.table[bucket_val][buckectItems_val]=True
+        #Else create a secondary list and then set the element as True
         else:
+            #Handling the edge case
             if(bucket_val==0):
                 self.table[bucket_val]=[False]*(self.bucketItems+1)
                 self.table[bucket_val][buckectItems_val]=True
@@ -38,6 +60,7 @@ class MyHashSet(object):
         """
         bucket_val=self.hash_function1(key)
         buckectItems_val=self.hash_fucntion2(key)
+        #Check if the element is present(location if true) and set it to false
         if self.table[bucket_val] is not None:
             if self.table[bucket_val][buckectItems_val]==True:
                 self.table[bucket_val][buckectItems_val]=False
