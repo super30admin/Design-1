2c7ceab95d557a6d6e9531c1f1a3b65454efa6da
akash0218 <92997084+akash0218@users.noreply.github.com>
2022-05-25 15:09:10 -0500

    finished

--- minstack.py
+++ minstack.py
@@ -8,25 +8,50 @@
 # added a tuple in the list which is for minimum value.
 # when ever push function is called it checks the 2nd element and adds the minimum element b/w two elements
 # for getting minimum value return the last element in the last tuple in the list
+import sys
+
 
 class MinStack:
     def __init__(self):
         self.stack = []
+        self.min = sys.maxsize
+    # def push(self, val: int) -> None:
+    #     if not self.stack:
+    #         self.stack.append((val, val))
+    #     else:
+    #         self.stack.append((val, min(self.stack[-1][-1], val)))
+    #
+    # def pop(self) -> None:
+    #     self.stack.pop()
+    #
+    # def top(self) -> int:
+    #     return self.stack[-1][0]
+    #
+    # def getMin(self) -> int:
+    #     return self.stack[-1][-1]
 
-    def push(self, val: int) -> None:
+    def push(self, val):
         if not self.stack:
-            self.stack.append((val, val))
+            self.stack.append(val)
+            self.min = val
         else:
-            self.stack.append((val, min(self.stack[-1][-1], val)))
+            if val <= self.min:
+                self.stack.append(self.min)
+                self.stack.append(val)
+                self.min = val
+            else:
+                self.stack.append(val)
 
-    def pop(self) -> None:
-        self.stack.pop()
+    def pop(self):
+        x = self.stack.pop()
+        if x == self.min:
+            self.min = self.stack.pop()
 
-    def top(self) -> int:
-        return self.stack[-1][0]
+    def top(self):
+        return self.stack[-1]
 
-    def getMin(self) -> int:
-        return self.stack[-1][-1]
+    def getMin(self):
+        return self.min
 
 
 # Your MinStack object will be instantiated and called as such:
