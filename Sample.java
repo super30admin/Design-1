39d7d5a82f83b35347c4b6a59a434b9aab292c8d
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-25 14:24:58 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -67,3 +67,44 @@ class MyHashSet {
 }
 
 //****Design A MINSTACK****
+//Time Complexity:o(1);
+//Space Complexity:o(n);
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : NO
+class MinStack {
+    Stack<Integer> stack;
+    int min;
+
+    public MinStack() {
+        min=Integer.MAX_VALUE;
+        stack =new Stack<>();
+        
+    }
+    
+    public void push(int val) {
+        if(val<=min)
+        {
+            stack.push(min);
+            min=val;
+        }
+        stack.push(val);
+        
+    }
+    
+    public void pop() {
+        if(min==stack.pop())
+        {
+            min=stack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+        
+    }
+}
