836fa08ff51e396f4ca2bc7273125a1d8c34adb2
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-26 13:20:51 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 // Your code here along with comments explaining your approach
 
-//****dESIGN HASHMAP USING DOUBLE HASHING****
+//****DESIGN HASHSET USING DOUBLE HASHING****
 //Time complexity:o(1);
 //space complexity:o(1);
 // Did this code successfully run on Leetcode : yes
@@ -68,94 +68,6 @@ class MyHashMap {
 }
 
 
-//****HASHMAP USING LINEAR CHAINING****
-//Time complexity:o(1);
-//space complexity:o(1);
-// Did this code successfully run on Leetcode : yes
-// Any problem you faced while coding this : NO
-class MyHashMap {
-    class Node{
-        int key;
-        int val;
-        Node next;
-        
-        public Node(int key, int val)
-        {
-            this.key=key;
-            this.val=val;
-        }
-    }
-    Node [] nodes;
-
-    public MyHashMap() {
-        nodes=new Node[10000];
-        
-    }
-    
-    public int idx(int key)
-    {
-        return key%10000;
-    }
-    
-    private Node find(Node head, int key)
-    {
-        Node prev=null;
-        Node curr=head;
-        
-        while(curr!=null && curr.key!=key)
-        {
-            prev=curr;
-            curr=curr.next;
-        }
-        return prev;
-    }
-    
-    public void put(int key, int value) {
-        int idx=idx(key);
-        if(nodes[idx]==null)
-        {
-            nodes[idx]= new Node(-1,-1);
-        }
-        //If already exists in out list;
-        Node prev=find(nodes[idx], key);
-        if(prev.next==null)
-        {
-            prev.next = new Node(key, value);
-        }
-        else
-        {
-            prev.next.val=value;
-        }
-        
-    }
-    
-    public int get(int key) {
-        
-        int idx=idx(key);
-        if(nodes[idx]==null) return -1;
-        
-        Node prev= find(nodes[idx], key);
-        if(prev.next==null)
-        {
-            return -1;
-        }
-            return prev.next.val;
-        
-        
-        
-    }
-    
-    public void remove(int key) {
-         int idx=idx(key);
-        if(nodes[idx]==null) return;
-         Node prev= find(nodes[idx], key);
-        if(prev.next==null) return;
-        prev.next=prev.next.next;
-        
-    }
-        
-        
-    }
 
 //****DESIGN A MINSTACK****
 //Time Complexity:o(1);
