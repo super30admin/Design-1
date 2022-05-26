a66b34456201ac4cb755d2e18d9c2950cefcc2f6
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-26 12:07:15 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -66,7 +66,93 @@ class MyHashMap {
     }
 }
 
-//****Design A MINSTACK****
+
+//****HASHMAP USING LINEAR CHAINING****
+class MyHashMap {
+    class Node{
+        int key;
+        int val;
+        Node next;
+        
+        public Node(int key, int val)
+        {
+            this.key=key;
+            this.val=val;
+        }
+    }
+    Node [] nodes;
+
+    public MyHashMap() {
+        nodes=new Node[10000];
+        
+    }
+    
+    public int idx(int key)
+    {
+        return key%10000;
+    }
+    
+    private Node find(Node head, int key)
+    {
+        Node prev=null;
+        Node curr=head;
+        
+        while(curr!=null && curr.key!=key)
+        {
+            prev=curr;
+            curr=curr.next;
+        }
+        return prev;
+    }
+    
+    public void put(int key, int value) {
+        int idx=idx(key);
+        if(nodes[idx]==null)
+        {
+            nodes[idx]= new Node(-1,-1);
+        }
+        //If already exists in out list;
+        Node prev=find(nodes[idx], key);
+        if(prev.next==null)
+        {
+            prev.next = new Node(key, value);
+        }
+        else
+        {
+            prev.next.val=value;
+        }
+        
+    }
+    
+    public int get(int key) {
+        
+        int idx=idx(key);
+        if(nodes[idx]==null) return -1;
+        
+        Node prev= find(nodes[idx], key);
+        if(prev.next==null)
+        {
+            return -1;
+        }
+            return prev.next.val;
+        
+        
+        
+    }
+    
+    public void remove(int key) {
+         int idx=idx(key);
+        if(nodes[idx]==null) return;
+         Node prev= find(nodes[idx], key);
+        if(prev.next==null) return;
+        prev.next=prev.next.next;
+        
+    }
+        
+        
+    }
+
+//****DESIGN A MINSTACK****
 //Time Complexity:o(1);
 //Space Complexity:o(n);
 // Did this code successfully run on Leetcode : yes
