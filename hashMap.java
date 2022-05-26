9b8c376d18e9f8b18f3e7493435270082b676624
praneeha <34100736+praneeha@users.noreply.github.com>
2022-05-26 11:44:49 -0500

    Update hashMap.java

--- hashMap.java
+++ hashMap.java
@@ -1,21 +1,65 @@
 class MyHashMap {
-    //Time complexity:O(1)
-    //Space complexity: O(n)
-    int[] arr=new int[1000001];
+    class Node{
+        int key,val;
+        Node next;
+        public Node(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+    Node[] nodes=new Node[10000];
+    private Node find(Node head,int key){
+        Node prev=null;
+        Node curr=head;
+        while(curr!=null && curr.key!=key){
+            prev=curr;
+            curr=curr.next;
+        }
+        return prev;
+    }
+    public int idx(int key){
+        return key%10000;
+    }
     public MyHashMap() {
-        Arrays.fill(arr,-1);
+        
     }
     
     public void put(int key, int value) {
-        arr[key]=value;
+        int idx=idx(key);
+        if(nodes[idx]==null){
+            nodes[idx]=new Node(-1,-1);
+        }
+        Node prev=find(nodes[idx],key);
+        if(prev.next==null){
+            prev.next=new Node(key,value);
+        }
+        else{
+            prev.next.val=value;
+        }
     }
     
     public int get(int key) {
-        return arr[key];
+        int idx=idx(key);
+        if(nodes[idx]==null){
+            return -1;
+        }
+        Node prev=find(nodes[idx],key);
+        if(prev.next==null){
+            return -1;
+        }
+        return prev.next.val;
     }
     
     public void remove(int key) {
-        arr[key]=-1;
+        int idx=idx(key);
+        if(nodes[idx]==null){
+            return;
+        }
+        Node prev=find(nodes[idx],key);
+        if(prev.next==null){
+            return;
+        }
+        prev.next=prev.next.next;
     }
 }
 
