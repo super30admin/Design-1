df4cea997f6cf71b3cdb32cb13ba3d3f9f07e832
plokaiah <plokaiah@asu.edu>
2022-05-28 12:26:54 -0700

    Hashmap using Linear Chining

---
+++ HashMap.py
@@ -0,0 +1,43 @@
+class MyHashMap(object):
+    def __init__(self):
+        self.arr=[None]*10000
+    class MyNode(object):
+        def __init__(self,key,val):
+            self.key=key
+            self.val=val
+            self.next=None
+    def find(self,head,key):
+        prev=head
+        curr=head.next
+        while(curr!=None and curr.key!=key):
+            prev=curr
+            curr=curr.next
+        return prev
+    def put(self, key, value):
+        n=self.MyNode(key,value)
+        hash=key%10000
+        if self.arr[hash]==None:
+            self.arr[hash]=self.MyNode(-1,-1)
+        prev=self.find(self.arr[hash],key)
+        if prev.next==None:
+            prev.next=n
+        else:
+            prev.next.val = value
+    def get(self, key):
+        hash=key%10000
+        if self.arr[hash]==None:
+            return -1
+        prev=self.find(self.arr[hash],key)
+        if prev.next==None:
+            return -1
+        else:
+            return prev.next.val
+    def remove(self, key):
+        hash=key%10000
+        if self.arr[hash]==None:
+            return
+        prev=self.find(self.arr[hash],key)
+        if prev.next==None:
+            return
+        else:
+            prev.next=prev.next.next
