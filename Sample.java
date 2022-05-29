356e2f79146c92f3a62e67093a5a6b6178a534d4
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-05-28 21:25:01 -0700

    sample.java done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,15 @@
+// Q1
+// Time Complexity : sqrt(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : I got error -  I was creading list of list at nested index
+
+// Q2
 // Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
-
-// Your code here along with comments explaining your approach
+// push: O(1)
+// pop: O(n)
+// peek: O(n)
+// empty:O(1)
+// Space Complexity :(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : na
