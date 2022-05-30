f59f121213213f27c3183bdc26e0f75683d92ab4
akash0218 <92997084+akash0218@users.noreply.github.com>
2022-05-29 19:17:14 -0500

    finished

--- hashmap.py
+++ hashmap.py
@@ -35,3 +35,109 @@ print(obj.get(2))
 obj.remove(2)
 print(obj.get(2))
 
+
+# Using double hashing
+# class MyHashMap:
+#     def __init__(self):
+#         self.hashmaplength = 1000
+#         self.hashmap = [[]] * self.hashmaplength
+#
+#     def hi(self, key):
+#         return key % 1000
+#
+#     def hv(self, key):
+#         return key // 1000
+#
+#     def put(self, key, value):
+#         hi = self.hi(key)
+#         hv = self.hv(key)
+#         if not self.hashmap[hi] and hi == 0:
+#             self.hashmap[hi] = [[]] * (self.hashmaplength+1)
+#         elif not self.hashmap[hi]:
+#             self.hashmap[hi] = [[]] * self.hashmaplength
+#         self.hashmap[hi][hv] = value
+#
+#     def get(self, key):
+#         hi = self.hi(key)
+#         hv = self.hv(key)
+#         if not self.hashmap[hi] or self.hashmap[hi][hv] == []:
+#             return -1
+#         else:
+#             return self.hashmap[hi][hv]
+#
+#     def remove(self, key):
+#         hi = self.hi(key)
+#         hv = self.hv(key)
+#         if not self.hashmap[hi] or not self.hashmap[hi][hv]:
+#             return
+#         else:
+#             self.hashmap[hi][hv] = []
+#
+#
+# check = MyHashMap()
+# check.put(11, 0)
+# print(check.hashmap[11][0])
+# print(check.get(11))
+# print(check.hashmap)
+
+
+# Using linear chaining
+# class Node:
+#     def __init__(self, key, value):
+#         self.key = key
+#         self.value = value
+#         self.next = None
+#
+#
+# class Hashmap:
+#     def __init__(self):
+#         self.hashmap = [None] * 10000
+#
+#     def hash(self, key):
+#         return key % 10000
+#
+#     def put(self, key, value):
+#         hi = self.hash(key)
+#         if not self.hashmap[hi]:
+#             self.hashmap[hi] = Node(key, value)
+#         else:
+#             temp = self.hashmap[hi]
+#             while temp:
+#                 if temp.key == key:
+#                     temp.value = value
+#                     return
+#                 prev = temp
+#                 temp = temp.next
+#             prev.next = Node(key, value)
+#
+#     def get(self, key):
+#         hi = self.hash(key)
+#         temp = self.hashmap[hi]
+#         while temp:
+#             if temp.key == key:
+#                 return temp.value
+#             temp = temp.next
+#         return -1
+#
+#     def remove(self, key):
+#         hi = self.hash(key)
+#         temp = self.hashmap[hi]
+#         if temp and temp.key == key:
+#             self.hashmap[hi] = temp.next
+#             return
+#         while temp:
+#             if temp.key == key:
+#                 prev.next = temp.next
+#                 return
+#             prev = temp
+#             temp = temp.next
+#
+#
+# llist = Hashmap()
+# llist.put(1, 1)
+# llist.put(10001, 10)
+# llist.remove(200)
+# print(llist.get(10001))
+
+
+
