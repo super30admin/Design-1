06d8f471ab2082bda3e210b922a057c575d5ca87
Pranjali Seth <pranjaliseth@Pranjalis-MacBook-Air.local>
2022-06-21 23:17:50 -0700

    Updated

---
+++ minStack.py
@@ -0,0 +1,45 @@
+#Time Complexity = O(1) 
+#Space Complexity = O(n)
+
+
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.mainStack = deque()
+        self.minStack = deque()
+        
+
+    def push(self, val: int) -> None:
+        self.mainStack.append(val)
+        
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            if self.minStack[-1] >= val:
+                self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        topElement = self.mainStack.pop()
+        if topElement == self.minStack[-1]:
+            self.minStack.pop()
+        return topElement
+        
+
+    def top(self) -> int:
+        return self.mainStack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
