a8bcbd12bcec2851d85bf1ee14985cb5caeb4ddd
Shreya Chaudhari <shreyamchaudhari@gmail.com>
2022-06-21 22:15:09 -0400

    Completed Design1 MinStack

---
+++ MinStack.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> s;
+    Stack<Integer> minStack;
+    int min;
+    
+    public MinStack() {
+        s = new Stack<>();
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+        minStack.push(min);
+    }
+    
+    public void push(int val) {
+        min = Math.min(min , val);
+        s.push(val);
+        minStack.push(min);
+    }
+    
+    public void pop() {
+        s.pop();
+        minStack.pop();
+        min = minStack.peek();
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
