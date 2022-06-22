f2713dc0f4f6d9bf5f76d3e4e6bf095b55fcaed5
sahithi6445 <bsahithi6445@gmail.com>
2022-06-21 23:33:34 -0700

    Completed Design-1

---
+++ MyHashMap.java
@@ -0,0 +1,69 @@
+// Your code here along with comments explaining your approach
+
+//****DESIGN HASHSET USING DOUBLE HASHING****
+//Time complexity:o(1)
+//space complexity:o(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : NO
+
+
+class MyHashSet {
+    
+    boolean[][] storage;
+    int buckets;
+    int bucketItems;
+
+    public MyHashSet() {
+        buckets = 1000;
+        bucketItems = 1000;
+        storage = new boolean[buckets][];   
+    }
+    
+    private int getBucket(int key) {
+        return key % buckets;
+    }
+    
+    private int getBucketItem(int key){
+        return key / bucketItems;
+    }
+    
+    public void add(int key) {
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);  
+        if(storage[bucket] == null) {
+            if(bucket == 0) {
+                storage[bucket] = new boolean[bucketItems +1];
+        }
+            else {
+                storage[bucket] = new boolean[bucketItems];
+           }
+        }
+        storage[bucket][bucketItem] = true;
+    }
+    
+    public void remove(int key) {
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);
+        if(storage[bucket] == null){
+            return;
+        }
+        storage[bucket][bucketItem] = false;
+    }
+    
+    public boolean contains(int key) {
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItem(key);
+        if(storage[bucket] == null) {
+            return false;
+        }
+        return storage[bucket][bucketItem];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
