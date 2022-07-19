ad79cc858571cef0b304e66d78c4305101697fea
yash-bhargava18 <ybhargava@scu.edu>
2022-07-19 00:25:44 -0700

    Create stack.py
    stack implementation

---
+++ stack.py
@@ -0,0 +1,25 @@
+#time complexity = O(1)
+#space complexity = O(n)
+#Code ran successfully on leetcode
+#As I am brushing up on DSA, I took help of online resources. I plan to get on track as class progresses.
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
