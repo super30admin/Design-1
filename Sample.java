80be89f62b4c217f372500b12f77eec72c202e51
Vishnuvardhan Reddy yeruva <vishnuvardhan.reddy98@gmail.com>
2022-07-19 20:41:48 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,37 @@
 
 
 // Your code here along with comments explaining your approach
+class MyHashSet {
+
+    static ArrayList a;
+    
+    public MyHashSet() {
+        
+        this.a = new ArrayList<>();
+        
+    }
+    
+    public void add(int key) {
+        if(!a.contains(key))
+            a.add(key);
+    }
+    
+    public void remove(int key) {
+        if(a.contains(key))
+            a.remove(a.indexOf(key));
+        
+    }
+    
+    public boolean contains(int key) {
+        return a.contains(key);
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
