b013aa124403e37203916c2b0cbfb1a969592e09
admin <admin@uscs-mac260.lan>
2022-07-20 01:27:51 -0700

    Problem1 HashmapSet  Day 1 Assignment

---
+++ MyHashSet.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class MyHashSet {
+    private final int MAX_LEN = 100000; 
+    private List<Integer>[] set;     
+
+
+    private int getIndex(int key) {
+        return key % MAX_LEN;
+    }
+
+    private int getPos(int key, int index) {
+        // Each bucket contains a list.
+        List<Integer> temp = set[index];
+        if (temp == null) {
+            return -1;
+        }
+  
+        for (int i = 0; i < temp.size(); ++i) {
+            if (temp.get(i) == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public MyHashSet() {
+        set = (List<Integer>[])new ArrayList[MAX_LEN];
+    }
+
+    public void add(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos < 0) {
+
+            if (set[index] == null) {
+                set[index] = new ArrayList<Integer>();
+            }
+            set[index].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        if (pos >= 0) {
+
+            set[index].remove(pos);
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = getIndex(key);
+        int pos = getPos(key, index);
+        return pos >= 0;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
