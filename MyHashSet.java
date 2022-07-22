cc91e3187ad01af48fb2543302ba52730c8ea375
admin <admin@uscs-mac260.lan>
2022-07-22 10:48:17 -0700

    Modified  the  design-hashset in Design-1

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,60 +1,53 @@
 import java.util.*;
-
 class MyHashSet {
-    private final int MAX_LEN = 100000; 
-    private List<Integer>[] set;     
-
-
-    private int getIndex(int key) {
-        return key % MAX_LEN;
-    }
-
-    private int getPos(int key, int index) {
-        // Each bucket contains a list.
-        List<Integer> temp = set[index];
-        if (temp == null) {
-            return -1;
-        }
-  
-        for (int i = 0; i < temp.size(); ++i) {
-            if (temp.get(i) == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
+    private boolean [][] storage;
+    int buckets;
+    int bucketItems;
 
     public MyHashSet() {
-        set = (List<Integer>[])new ArrayList[MAX_LEN];
+        this.buckets =1000;
+        this.bucketItems = 1000;
+        this.storage  = new boolean[buckets][];
+    }
+    private int hash1(int key){
+        return key%buckets;
+    }
+    private int hash2(int key){
+        return key/bucketItems;
     }
 
-    public void add(int key) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        if (pos < 0) {
+    public void add(){
+        int hash1 =  hash1(key);
+        int hash2 = hash2(key);
 
-            if (set[index] == null) {
-                set[index] = new ArrayList<Integer>();
+        if(storage[hash1]== null){
+            if(hash1  == 0){
+                storage[hash1] = new  boolean[bucketItems+1];
             }
-            set[index].add(key);
+            else{
+                storage[hash1] = new  boolean[bucketItems];
+            }
         }
+        storage[hash1][hash2] =  true;
     }
-
     public void remove(int key) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        if (pos >= 0) {
-
-            set[index].remove(pos);
-        }
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1] == null) return;
+       
+        storage[hash1][hash2] = false;
+        
     }
 
     public boolean contains(int key) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        return pos >= 0;
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        
+        if(storage[hash1] == null) return false;
+        return storage[hash1][hash2];
     }
+
+
 }
 
 /**
