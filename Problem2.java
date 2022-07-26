1ab9a4b98f7f2500f5e8ced2652294c8d62937ad
Prateek Dagariya <91617758+pd2303@users.noreply.github.com>
2022-07-26 10:39:53 -0400

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,40 @@
+class MinStack {
+    
+     private Stack<Integer> stack = new Stack<>();
+     private Stack<Integer> minStack = new Stack<>();
+    
+    public MinStack() {
+       
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
