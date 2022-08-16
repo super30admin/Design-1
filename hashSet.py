79978888e6a0c81d3831c151cb9ef70a3978ec12
suraj7696 <satmakur@asu.edu>
2022-08-16 17:54:14 -0400

    added comments

--- hashSet.py
+++ hashSet.py
@@ -1,11 +1,16 @@
+#Time Complexity = add: O(1), remove: O(1), contains: O(n)
+#Space Complexity = add: O(1), remove: O(1), contains: O(1)
 class MyHashSet:
+    #Declare a bucket of a large size to handle larger capacity of data
     BUCKET = 1000
 
     def __init__(self):
+        #declare a map with the global BUCKEt
         self.map = [[] for _ in range(MyHashSet.BUCKET)]
         
 
     def add(self, key: int) -> None:
+        #Add the key towards the index which is modulo of key and bucket and return none if there is a collission or else add it into the array
         i = key % MyHashSet.BUCKET
         if key in self.map[i]:
             return None
@@ -14,6 +19,7 @@ class MyHashSet:
         
 
     def remove(self, key: int) -> None:
+        #if the key is found remove the element key at ith index
         i = key% MyHashSet.BUCKET
         if key in self.map[i]:
             del self.map[i][self.map[i].index(key)]
@@ -22,6 +28,7 @@ class MyHashSet:
         
 
     def contains(self, key: int) -> bool:
+        #return true if the array holds value at key
         i = key% MyHashSet.BUCKET
         if key in self.map[i]:
             return True
