3c11ce2751215bb92bb97fc8a4aca4079a806d08
Nishanth.Manjunatha_Group <nishanth.di.m@gmail.com>
2022-08-16 18:08:39 -0400

    update hash set

--- HashSet.py
+++ HashSet.py
@@ -1,27 +1,34 @@
 class MyHashSet:
     def __init__(self):
+        # used 10000 here because of at most constrant
         self.size = 10000
-        self.data = [[None] for _ in self.size]
+        # list of lists for given size
+        self.data = [None] * self.size
 
     def calculate_hash_value(self, key: int) -> int:
         return key % self.size
-   
+    
+    def hash_value_in_data(self, hash_value: int) -> bool:
+        return self.data[hash_value] is not None
 
     def add(self, key: int) -> None:
-        if not self.contains(key):
-            hash_value = self.calculate_hash_value(key)
+        hash_value = self.calculate_hash_value(key)
+        if self.hash_value_in_data(hash_value):
             self.data[hash_value].append(key)
-        
+        else:
+            self.data[hash_value] = [key]
 
     def remove(self, key: int) -> None:
-        if self.contains(key):
-            hash_value = self.calculate_hash_value(key)
-            self.data[hash_value].remove(key)
-
+        hash_value = self.calculate_hash_value(key)
+        if self.hash_value_in_data(hash_value):
+            while key in self.data[hash_value]:
+                self.data[hash_value].remove(key)
 
     def contains(self, key: int) -> bool:
         hash_value = self.calculate_hash_value(key)
-        return hash_value in self.data[hash_value]
+        if self.hash_value_in_data(hash_value):
+            return key in self.data[hash_value]
+        return False
         
 
 
