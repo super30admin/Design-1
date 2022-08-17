029cf58d554dbd031cb2ed098665eb2c0a8cdb98
Onkar Kumbhar <onkarkumbhar@Onkars-MacBook-Air.local>
2022-08-17 10:17:14 +0530

    Pushed HashSet Code

--- Sample.java
+++ Sample.java
@@ -1,7 +1,57 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Implement HashSet 
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+    
+    boolean[][] storage;
+    int buckets;
+    int bucketItems;
+
+    public MyHashSet() {
+        this.buckets = 1000;
+        this.bucketItems = 1000;
+        this.storage = new boolean[buckets][];
+    }
+    
+    private int hash1(int key) {
+        return key%buckets;
+    }
+    
+    private int hash2(int key) {
+        return key/bucketItems;
+    }
+    
+    public void add(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1] == null)
+        {
+            if(hash1 == 0)
+                storage[hash1] = new boolean[bucketItems+1];
+            else
+                storage[hash1] = new boolean[bucketItems];    
+        }
+        storage[hash1][hash2] = true;
+        
+    }
+    
+    public void remove(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1]==null) return;
+            storage[hash1][hash2] = false;
+    }
+    
+    public boolean contains(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1]==null) return false;
+            return storage[hash1][hash2];
+    }
+}
