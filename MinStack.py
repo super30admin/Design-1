29394bd086d140a2de04ec4c1abddc812cf77208
Nishanth.Manjunatha_Group <nishanth.di.m@gmail.com>
2022-08-17 18:07:01 -0400

    min stack

---
+++ MinStack.py
@@ -0,0 +1,36 @@
+class MinStack:
+    def __init__(self):
+        self.list = []
+        self.min_value = float('inf')
+        self.min_list = [self.min_value]
+        
+
+    def push(self, val: int) -> None:
+        self.list.append(val)
+        if val < self.getMin():
+            self.min_list.append(val)
+            self.min_value = val
+        else:
+            self.min_list.append(self.min_value)
+
+
+    def pop(self) -> None:
+        self.list.pop()
+        self.min_list.pop()
+        self.min_value = self.getMin()
+        
+
+    def top(self) -> int:
+        return self.list[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_list[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
