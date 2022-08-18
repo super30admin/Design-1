4af61336d50fad1996e221e8a8083873fe57534f
tanayapanditrao15 <89812452+tanayapanditrao15@users.noreply.github.com>
2022-08-18 15:37:56 -0500

    Update Design1 Hashset and MinStack

--- Sample.java
+++ Sample.java
@@ -6,8 +6,7 @@
 
 // Your code here along with comments explaining your approach
 
-
-
+Problem 1 - Hashset
 class MyHashSet {
     
     boolean [][] storage;
@@ -86,4 +85,58 @@ class MyHashSet {
  * obj.add(key);
  * obj.remove(key);
  * boolean param_3 = obj.contains(key);
+ */
+
+//Problem 2 - MinStack
+// Time Complexity : O(1)
+// Space Complexity : O(n) as we are using two stacks
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class MinStack {
+    private Stack<Integer> st;
+    private Stack<Integer> minStack;
+    private int min;
+
+    public MinStack() {
+        this.st = new Stack<>();
+        this.minStack = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+        minStack.push(min);
+    }
+   
+   
+    public void push(int val) {
+        min = Math.min(min,val);
+        st.push(val);
+        minStack.push(min);
+       
+    }
+   
+    public void pop() {
+        st.pop();
+        minStack.pop();
+        min = minStack.peek();
+    }
+   
+    public int top() {
+        return st.peek();
+       
+    }
+   
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
