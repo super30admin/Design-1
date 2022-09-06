6331773e4dc56460f421133758275fc2e299e146
SagarKhandagale <khandagale.s@northeastern.edu>
2022-09-06 19:00:29 -0400

    Completed Design-1

--- Sample.java
+++ Sample.java
@@ -1,7 +1,7 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n) Because TC of ArrayList contains method is O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
