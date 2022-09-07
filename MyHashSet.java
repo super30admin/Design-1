db815318e07e850c7d1decee6ea8056599199e32
Tejasvib <tejasvibhadrinath26@gmail.com>
2022-09-07 16:30:55 -0400

    optimised code

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,44 +1,73 @@
 // Time Complexity : O(1)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Solved collision by increasing size of hashset, unsure of implementation of double hashing.
+// Any problem you faced while coding this : No
 
-//We are creating an array and the mod of the value to the size of the created array gives us index to add/insert the value(hashing function).
-//To get the index to add/remove/check if the index contains the value, we call the hashing function 
+//We are using double hasing for solving collisions and are using boolean 2D array for simplification
+
 
 class MyHashSet {
-    int[] setarray;
-        public MyHashSet() {
-       setarray = new int[1000000];
-            for(int i=0; i<setarray.length; i++){
-                setarray[i]=-1;
+    boolean[][] storage; //2D boolean array
+    int buckets;
+    int bucketItems;
+    public MyHashSet() {
+        //since the range is 10^6, we take square root of the range for primary array (10^3)
+        buckets=1000; //length of primary array
+        bucketItems=1000; //length of secondary array
+        storage= new boolean[buckets][];
+        }
+    
+    
+    public void add(int key) {
+int bucket=getBucket(key); // to find the index in primary array
+        int bucketItem= getBucketItem(key);// to find the index in secondary array
+            
+            if(storage[bucket]==null){
+                if(bucket==0){
+                   storage[bucket]= new boolean[bucketItems +1]; //To handle edge case for 10^6 
+                }
+                else{
+                    storage[bucket]= new boolean[bucketItems]; //initialize secondary array
+                }
             }
-        }
-        
-        public void add(int key) {
-            int index= hashfunction(key);
-            setarray[index]=key;
-        }
-        
-        public void remove(int key) {
-            int index= hashfunction(key);
-            setarray[index]=-1;
-        }
-        
-        public boolean contains(int key) {
-            int index= hashfunction(key);
-            if(setarray[index]== key){
-                return true;
-            }
-            else return false;
-        }
-        
-        public int hashfunction(int key){
-       int index= key% setarray.length;
-            return index;
-        }
+        storage[bucket][bucketItem]=true;
     }
     
+    public void remove(int key) {
+int bucket=getBucket(key); // to find the index in primary array
+        int bucketItem= getBucketItem(key);// to find the index in secondary array
+             if(storage[bucket]==null){
+                 return;
+             }
+        storage[bucket][bucketItem]=false;
+    }
+    
+    public boolean contains(int key) {
+int bucket=getBucket(key); // to find the index in primary array
+        int bucketItem= getBucketItem(key);// to find the index in secondary array
+        if(storage[bucket]== null){
+            return false;
+        }
+        else return storage[bucket][bucketItem]; 
+        //will give true or false since it is a boolean array
+    }
+    //Double Hashing
+    public int getBucket(int key){
+       return key% buckets; //for primary array
+    }
+    public int getBucketItem(int key){
+       return key/ bucketItems; //for secondary array
+    }
+
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
     /**
      * Your MyHashSet object will be instantiated and called as such:
      * MyHashSet obj = new MyHashSet();
