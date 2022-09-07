61b866c68ad9bf199a276e7a43e35bb278a9bcf0
lakshaynanda <lakshay.nanda08@gmail.com>
2022-09-06 17:27:13 -0700

    Design 1 Complete - Please Review

--- Design_1_MinStack.java
+++ Design_1_MinStack.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(1)
-// Space Complexity : O(1)
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this: No
 
@@ -8,23 +8,33 @@ import java.util.Stack;
 class MinStack {
     Stack<Integer> min;
     Stack<Integer> stack;
+    int size;
+    int topStack;
+    int topMin;
     public MinStack() {
         min = new Stack<Integer>();
         stack = new Stack<Integer>();
+        size = 100;
+        topStack = -1;
+        topMin = -1;
     }
     
     public void push(int val) {
         int minval = val;
         stack.push(val);
+        topStack++;
         if (!min.isEmpty() && min.peek() < minval) {
             minval = min.peek();
         }
         min.push(minval);
+        topMin++;
     }
     
     public void pop() {
         stack.pop();
         min.pop();
+        topStack--;
+        topMin--;
     }
     
     public int top() {
@@ -33,5 +43,13 @@ class MinStack {
     
     public int getMin() {
         return min.peek();
+    }
+
+    public boolean isEmpty() {
+        return (topStack == -1);
+    }
+
+    public boolean isFull() {
+        return (topStack == size - 1);
     }
 }
