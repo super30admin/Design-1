11ae05bd425fa1612b6e3321172e6c6b3a9a7179
Tanaya-Mahakalkar <tanaya.mahakalkar95@gmail.com>
2022-10-05 10:55:55 -0500

    Completed - Design 1 - Exercise 2

--- Exercise 2_Min_Stack.py
+++ Exercise 2_Min_Stack.py
@@ -1,7 +1,7 @@
 # Time Complexity : O(1)
 # Space Complexity : O(2n) --> O(n)
 # Did this code successfully run on Leetcode : Yes
-# Any problem you faced while coding this : No
+# Any problem you faced while coding this : Nope
 
 
 class MinStack:
