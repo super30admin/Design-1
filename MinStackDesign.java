8a6b4dc6dbfd6b0dc7c1a9d7468320e902fdc591
katta332 <praveenkumar.kpc@gmail.com>
2022-10-28 11:02:46 -0600

    added approached learnt from class

--- MinStackDesign.java
+++ MinStackDesign.java
@@ -31,11 +31,34 @@ class MinStack {
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+//Approach learnt from class
+class MinStack {
+    Stack<Integer> st = new Stack<Integer>();
+    int min = Integer.MAX_VALUE;
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if(val<=min){
+            st.push(min);
+        }
+        min = Math.min(val, min);
+        st.push(val);
+    }
+    
+    public void pop() {
+        int num =st.pop();
+        if(num==min){
+            min = st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
