f909e7caced194e34682ae5e37b206486c3097c4
zeel <zeeltrivedi@csu.fullerton.edu>
2022-10-29 16:49:34 -0700

    Done with problem 1 and problem 2

--- Sample.java
+++ Sample.java
@@ -36,4 +36,74 @@ class MinStack {
     public int getMin() {
         return minStack.peek();
     }
-}
+/// Problem 2 solution:
+
+class MyHashSet {
+
+    int buckets = 1000;
+ 
+    int bucketItems = 1000;
+ 
+    boolean [][] storage = new boolean[buckets][];
+ 
+    public int bucket(int value){ 
+ 
+        return value%buckets;
+ 
+    }
+ 
+    public int bucketItem(int value){ 
+ 
+        return value/buckets;
+ 
+    }
+ 
+    // Initialize your data structure here. 
+ 
+    public MyHashSet() {
+ 
+    }
+ 
+    public void add(int value ) {
+ 
+        int bucket = bucket(value);      
+ 
+        int bucketItem = bucketItem(value);
+ 
+        if(storage[bucket] == null){
+ 
+            storage[bucket] = new boolean[bucketItems]; 
+ 
+        }
+ 
+        storage[bucket][bucketItem] = true;
+ 
+    }
+ 
+    public void remove(int value) {
+ 
+         int bucket = bucket(value);
+ 
+         int bucketItem = bucketItem(value);
+ 
+        if(storage[bucket] != null){
+ 
+            storage[bucket][bucketItem] = false;
+ 
+        }
+ 
+    }
+ 
+    // Returns true if this set contains the specified element 
+ 
+    public boolean contains(int value) {
+ 
+        int bucket = bucket(value);
+ 
+        int bucketItem = bucketItem(value);
+ 
+        return storage[bucket] != null && storage[bucket][bucketItem];
+ 
+    }
+ 
+ }
