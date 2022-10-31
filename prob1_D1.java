45b832a39225fcd9f318f6cddf32fdc6dcf8977f
Vanshika <vanshikaaa1009@gmail.com>
2022-10-30 22:22:02 -0500

    Create prob1_D1.java

---
+++ prob1_D1.java
@@ -0,0 +1,53 @@
+/*Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
+
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+*/
+
+class MyHashSet {
+    int size = (int)Math.pow(10,6)+1;
+	boolean[] flag;
+    public MyHashSet() {
+		flag = new boolean[size];
+	}
+    public void add(int key) {
+        flag[key]=true;
+    }
+    public void remove(int key) {
+        flag[key]=false;
+    }
+    public boolean contains(int key) {
+        return flag[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
