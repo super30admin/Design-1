d333594d082a70f1f9eb87684a3213ce112b7fac
Akash Bangera <abanger@amazon.com>
2022-11-22 10:17:16 -0600

    Adding comments for each action

--- Sample.java
+++ Sample.java
@@ -7,19 +7,22 @@
 // Your code here along with comments explaining your approach
 
 class MyHashSet {
+    //creating an array list as base data structure to work as hashset
     ArrayList<Integer> hashset;
     public MyHashSet() {
+        //intialize in constructor
         hashset = new ArrayList<>();
-
     }
 
     public void add(int key) {
+        //if key is not availabe in the arraylist, adding the key to it (key is the actual value here)
         if(!hashset.contains(key)){
             hashset.add(key);
         }
     }
 
     public void remove(int key) {
+        //if key is available in the arraylist, removing it by adding it to new arraylist (it will copy all elements to new array list, so removed tge old one)
         if(hashset.contains(key)) {
             ArrayList<Integer> newHashSet = new ArrayList<>();
             newHashSet.add(key);
@@ -28,6 +31,7 @@ class MyHashSet {
     }
 
     public boolean contains(int key) {
+        //return true if arraylist has the key
         if(hashset.contains(key)) {
             return true;
         }
