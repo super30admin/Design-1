4030a5ee94c494c3d8fb02565f031365e5559f2a
Akash Bangera <abanger@amazon.com>
2022-11-22 10:10:06 -0600

    Completed Design-1 - Please Review

--- Sample.java
+++ Sample.java
@@ -1,7 +1,36 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : Add = O(n), Remove = O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Didn't know the concept behind it. Learnt in leetcode before coding this
 
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+    ArrayList<Integer> hashset;
+    public MyHashSet() {
+        hashset = new ArrayList<>();
+
+    }
+
+    public void add(int key) {
+        if(!hashset.contains(key)){
+            hashset.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        if(hashset.contains(key)) {
+            ArrayList<Integer> newHashSet = new ArrayList<>();
+            newHashSet.add(key);
+            hashset.removeAll(newHashSet);
+        }
+    }
+
+    public boolean contains(int key) {
+        if(hashset.contains(key)) {
+            return true;
+        }
+        return false;
+    }
+}
