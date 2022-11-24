f27645683f4b0e36eb318fc841b8a0b1870e21cd
Seelam Vinay Kumar Reddy <vinayseelam.98@gmail.com>
2022-11-23 17:14:11 -0800

    HashSetDesign & Min Stack

---
+++ MinStack.java
@@ -0,0 +1,53 @@
+class MinStack {
+    
+    private Stack<Integer> st;
+    private Stack<Integer> minSt;
+    private int min;
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+        min = Integer.MAX_VALUE;
+        minSt.push(min);
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        min = Math.min(min,val);
+        minSt.push(min);
+    }
+    
+    public void pop() {
+        st.pop();
+        minSt.pop();
+        min = minSt.peek();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+// Creating 2 stacks. one is for value and another is for minimum value.
