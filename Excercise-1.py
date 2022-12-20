b5c8f28dd4d438105cd2a808d571314fab6705cf
Rohan-Balakrishna <119444369+Rohan-Balakrishna@users.noreply.github.com>
2022-12-20 17:39:19 +0530

    Add files via upload

---
+++ Excercise-1.py
@@ -0,0 +1,53 @@
+# Time Complexity : O(1) for lookup,insertion,remove
+# Space Complexity :O(n) #For HashSet
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this :No
+
+
+
+class MyHashSet(object):
+
+    def __init__(self):
+        self.HashSet=[[] for i in range (1000)]
+
+    def Hash_Function(self,key):
+        return(key%1000)  
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        self.HashSet[self.Hash_Function(key)].append(key)
+        
+        
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        hv=self.Hash_Function(key)
+        if(self.HashSet[hv] !=[]):
+            while(key in self.HashSet[hv]):
+                self.HashSet[hv].remove(key)
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        hv=self.Hash_Function(key)
+        if(self.HashSet[hv] !=[]):
+            if(key in self.HashSet[hv]):
+                return(True)
+        return(False)
+        
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
