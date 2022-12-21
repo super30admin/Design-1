2452f0d17732ae776b7acba9f7045bc7f9d90165
rajesh <rajeshdm9@gmail.com>
2022-12-20 18:28:40 -0800

    hashstack v1

--- Hashset.py
+++ Hashset.py
@@ -1,5 +1,5 @@
 # Time Complexity :
-# Push, pop, top and getMin - O(1)
+#Add,remove, contains O(1), 
 
 # Space Complexity :
 # O(N)
@@ -8,5 +8,22 @@
 #Yes
 
 # Any problem you faced while coding this :
-# I missed the point that if the original minimum is removed, the self.min has to be updated
+#This solution seems very trivial - not sure about a better way
 
+class MyHashSet:
+
+    def __init__(self):
+        self.data = [False] * ((10**6) + 1 )
+        self.max_value = 10 ** 6
+
+    def hash(self,key):
+        return key % self.max_value
+
+    def add(self, key: int) -> None:
+        self.data[self.hash(key)] = True
+
+    def remove(self, key: int) -> None:
+        self.data[self.hash(key)] = False
+
+    def contains(self, key: int) -> bool:
+        return self.data[self.hash(key)]
