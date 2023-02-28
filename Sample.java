2edb2cb0611aa34abf541198dd7ddc0b1fc3d0a6
Abhiruchi Shinde <92135919+ruh123@users.noreply.github.com>
2023-02-28 15:34:36 -0800

    Completed HashSet Implementation

--- Sample.java
+++ Sample.java
@@ -1,7 +1,54 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes 
+// Any problem you faced while coding this : no
 
+class MyHashSet {
 
-// Your code here along with comments explaining your approach
+    private final int Array_size= 100;
+    private List<List<Integer>> ParentList;
+    public MyHashSet() {
+
+        ParentList = new ArrayList<>(Array_size);
+        for(int i=0; i< Array_size; i++)
+        {
+            ParentList.add(null);
+        }
+    }
+    
+    public void add(int key) {
+        int index = key%Array_size;
+
+        List<Integer> childlist=ParentList.get(index);
+
+        if(childlist==null)
+        {
+           List<Integer> list = new LinkedList<>();
+           list.add(key);
+           ParentList.set(index, list);
+        }
+        else {
+                if(!childlist.contains(key))
+                 childlist.add(key);
+        }
+
+        
+    }
+    
+    public void remove(int key) {
+       int index = key%Array_size;
+       List<Integer> childlist = ParentList.get(index);
+
+       if(childlist!= null)
+        childlist.remove(Integer.valueOf(key));
+    }
+
+    
+    public boolean contains(int key) {
+      int index = key%Array_size;
+      List<Integer> childlist = ParentList.get(index);
+
+      return childlist != null && childlist.contains(key);
+
+    }
+}
