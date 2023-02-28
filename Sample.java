ee25134a93026e1f00294e921d0176d0ae7dd9cf
Supreeta Anand Byatnal <supreeta.byatnal@gmail.com>
2023-02-27 20:03:15 -0500

    Design-1 completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,28 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : 
 
+//Not while coding but I have difficulty figuring out the time complexity.
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+    boolean[] a ;
+        public MyHashSet() {
+             a = new boolean[1000001];
+        }
+        
+        public void add(int key) {
+             a[key] = true;                     
+        }
+        
+        public void remove(int key) {
+            a[key] = false;
+        }
+        
+        public boolean contains(int key) {
+            return a[key];
+        }
+    }
+    
