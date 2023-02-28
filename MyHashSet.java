46d750fa5137268d1084f186a1a41d92aa47fade
rashmeetkhanuja <rashmeetkaur237@gmail.com>
2023-02-28 10:14:20 -0800

    HashSet implementation using double hashing

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,76 +1,67 @@
-class MyHashSet{
-    int size = 3;
-    int[] arr;
-    double capacityFactor = 0.8;
-    int count = 0;
+class MyHashSet {
+    private int buckets; //size of the primary array
+    private int bucketItems; //size of nested array
+    boolean[][] storage;
 
     public MyHashSet() {
-        arr = new int[size];
+        buckets = 100;
+        bucketItems = 100;
+        storage = new boolean[buckets][];
     }
 
-    //a hash function place the elements
-    public int  hashFunction(int key){
-        return key%size;
+    //hash function for primary array
+    public int hashFunction1(int key) {
+        return key % buckets;
     }
 
-    //if the hashset reaches 80% capacity, allocate a new array double the size and copy contents of the old one into the new
-    public void chainingFunction(int key){
-        if(count == (int)(capacityFactor*size) || hashFunction(key)>=size){
-            count = 0;
-            size*=2;
-            int[] oldArr = arr;
-            arr = new int[size];
-            for(int i = 0; i<oldArr.length; i++){
-                arr[i]  = oldArr[i];
-            }
-        }
-    }
-
-    public void collisionResolution(){
-        //ToDo: implement a collision resolution function if hashFunction returns the same index for 2 or more keys
+    //hash function for nested array to handle collisions
+    public int hashFunction2(int key) {
+        return key / buckets;
     }
 
     public void add(int key) {
-        chainingFunction(key);
-        if(contains(key)) return;
-
-        int index = hashFunction(key);
-        if(arr[index] == 0){
-            arr[index] = key;
+        int pIdx = hashFunction1(key);
+        int nIdx = hashFunction2(key);
+        if (storage[pIdx] == null) {
+            if (pIdx == 0) {
+                storage[pIdx] = new boolean[bucketItems + 1];
+            } else {
+                storage[pIdx] = new boolean[bucketItems];
+            }
         }
-        else {
-            collisionResolution();
-        }
-        count++;
+        storage[pIdx][nIdx] = true;
     }
 
     public void remove(int key) {
-        int[] newArr = new int[size-1];
-        for(int i =0, j=0; i<size; i++){
-            if(arr[i]!= key){
-                newArr[j] = arr[i];
-            }
-        }
-        arr = newArr;
-        count --;
+        int pIdx = hashFunction1(key);
+        int nIdx = hashFunction2(key);
+        if (storage[pIdx] == null) return;
+        storage[pIdx][nIdx] = false;
     }
 
     public boolean contains(int key) {
-        int index  = hashFunction(key);
-        return arr[index] == key;
+        int pIdx = hashFunction1(key);
+        int nIdx = hashFunction2(key);
+        if (storage[pIdx] == null) return false;
+        return storage[pIdx][nIdx];
     }
 
 
     public static void main(String[] args) {
         MyHashSet myHashSet = new MyHashSet();
-        myHashSet.add(21);
-        myHashSet.add(32);
-        myHashSet.add(10);
-        myHashSet.add(5);
+        myHashSet.add(9);
+        myHashSet.remove(19);
         myHashSet.add(14);
-        myHashSet.contains(21);
-        myHashSet.contains(33);
-        myHashSet.remove(14);
-        //10, 21, 32, 0, 14, 5
+        myHashSet.remove(19);
+        myHashSet.remove(9);
+//        myHashSet.contains(1);
+//        myHashSet.contains(3);
+        myHashSet.add(0);
+        myHashSet.add(3);
+        myHashSet.add(4);
+        myHashSet.add(0);
+        myHashSet.remove(9);
+
+
     }
 }
