9bf6032367f35f9f20d3641a03e8b1220639bf96
sivakami-projects <sivakami.sde@gmail.com>
2023-02-27 21:26:32 -0800

    Added time complexity for hash set implementation.

--- com/sivakami/ds/implementation/hashSet/MyHashSet.java
+++ com/sivakami/ds/implementation/hashSet/MyHashSet.java
@@ -1,3 +1,16 @@
+/*		Time complexity - O(log N/K)
+		Space Complexity - O(K + N)	
+		Approach - Implemented hash set using array of bst. 
+				   add - Data is added to the bst tree in the offset represented by hashfxn. 
+				   		 time complexity of adding in a BST is O(log D) 
+				   		 	Where D is the depth of the tree. 
+				   		 	If the tree is skewed then Depth D becomes N no of nodes in tree.
+				   		    Here D is N / k (assuming that the values are all equally distributed), Where 
+				   		    					N is the number of values in hashmap. 
+				   		       					k is the key size of the hashmap. 
+				   remove - Data is removed from BST tree in the offset represented by hashfx.
+				   			O(N/k) time.
+*/
 package com.sivakami.ds.implementation.hashSet;
 
 public class MyHashSet {
