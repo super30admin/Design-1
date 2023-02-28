9e97095a444e19e69fbdc6c663b66e43612a5675
virat02 <virat.goradia96@gmail.com>
2023-02-27 17:14:11 -0800

    Add time and space complexity info to hashset problem

--- design_hashset.py
+++ design_hashset.py
@@ -1,17 +1,27 @@
+# Space Complexity: O(n)
 class MyHashSet:
     def __init__(self):
+        # Prime number for a good hash modular value
         self.capacity = 769
         self.arr = [LLBucket() for i in range(self.capacity)]
 
+    # Time Complexity: O(1)
     def _hash(self, key: int) -> int:
         return key % self.capacity
 
+    # Time Complexity: O(1)
     def add(self, key: int) -> None:
         self.arr[self._hash(key)].add(key)
 
+    # Time Complexity: O(n)
+    # Average case would be O(1) if no to extremely few collisions.
+    # Worst case O(n) due to searching the "key" to be removed from the linked list bucket.
     def remove(self, key: int) -> None:
         self.arr[self._hash(key)].remove(key)
 
+    # Time Complexity: O(n)
+    # Average case would be O(1) if no to extremely few collisions.
+    # Worst case O(n) due to searching the "key" from the linked list bucket.
     def contains(self, key: int) -> bool:
         return self.arr[self._hash(key)].contains(key)
 
@@ -26,12 +36,16 @@ class LLBucket():
     def __init__(self):
         self.head = Node(float('-inf'))
     
+    # Time Complexity: O(1)
     def add(self, data: int):
         if not self.contains(data):
             newNode = Node(data)
             newNode.next = self.head.next
             self.head.next = newNode
 
+    # Time Complexity: O(n)
+    # Average case would be O(1) if no to extremely few collisions.
+    # Worst case O(n) due to searching the "key" to be removed from the linked list bucket.
     def remove(self, data: int):
         prev = self.head
         curr = self.head.next
@@ -44,6 +58,9 @@ class LLBucket():
             prev = curr
             curr = curr.next
     
+    # Time Complexity: O(n)
+    # Average case would be O(1) if no to extremely few collisions.
+    # Worst case O(n) due to searching the "key" from the linked list bucket.
     def contains(self, data: int) -> bool:
         curr = self.head.next
 
