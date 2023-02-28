02159b9f1ad7b368e6ff69199ac7ea23d6291047
sivakami-projects <sivakami.sde@gmail.com>
2023-02-27 21:06:43 -0800

    Time complexity for minStack.

--- com/sivakami/ds/implementation/Stack/MinStack.java
+++ com/sivakami/ds/implementation/Stack/MinStack.java
@@ -1,8 +1,16 @@
 package com.sivakami.ds.implementation.Stack;
 
 /*		Time complexity - O(1)
-		Space Complexity - O(N) where N is the size of the stack.			*/
-
+		Space Complexity - O(2 * N) ~ O(N)  where N is the size of the stack.	
+		Approach - Implemented stack using linkedlist. 
+				   Push - Data is added to the head of linked list. O(1) time 
+				   Pop - Data is removed from head of linked list.  O(1) time
+				   top - Value of head is returned.                 O(1) time
+				   min - To fetch min value another stack is used.  O(1) time
+				   		 At push we look for min value. Every time a min value is encountered it is added to the min stack.
+				   		 At pop, if the popped value is same as min value, we pop min stack.
+				   		 Head of min stack would give min value of the stack.
+*/
 public class MinStack {
 	
     StackNode top; 
