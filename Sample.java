214ca91d23cb8c1dd4c988021b3b42520162a3a6
Supreeta Anand Byatnal <supreeta.byatnal@gmail.com>
2023-02-27 20:39:11 -0500

    Design-1

--- Sample.java
+++ Sample.java
@@ -3,9 +3,13 @@
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : 
 
-//Not while coding but I have difficulty figuring out the time complexity.
+//Not while coding but I have difficulty figuring out the time complexity in general. 
 
-// Your code here along with comments explaining your approach
+// Your code here along with comments explaining your approach : 
+
+//This code below sets the value of that key as true. Hence when we want to find if it is contained ,
+//we return true as well. Similarly we set it to false in the remove function. So when we call contains on a removed 
+//element, it returns false as expected. 
 
 class MyHashSet {
     boolean[] a ;
@@ -14,7 +18,7 @@ class MyHashSet {
         }
         
         public void add(int key) {
-             a[key] = true;                     
+             a[key] = true;     
         }
         
         public void remove(int key) {
