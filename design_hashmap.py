1362c829569c8b8014be285cbb6b9e5c9378dc25
swapnil wagh <70155167+swapnilwagh2204@users.noreply.github.com>
2023-02-28 19:31:10 -0800

    Update design_hashmap.py

--- design_hashmap.py
+++ design_hashmap.py
@@ -1,1 +1,28 @@
 
+
+
+class MyHashSet:
+
+    def __init__(self):
+        self.numBuckets=15000
+        self.buckets=[[] for i in range(self.numBuckets)]
+        
+    def hash_function(self,key):
+        return key%self.numBuckets
+
+    def add(self, key: int) -> None:
+        i=self.hash_function(key)
+        if not key in self.buckets[i]:
+            self.buckets[i].append(key)
+
+    def remove(self, key: int) -> None:
+        i=self.hash_function(key)
+        if key in self.buckets[i]:
+            self.buckets[i].remove(key)
+        
+
+    def contains(self, key: int) -> bool:
+        i=self.hash_function(key)
+        if key in self.buckets[i]:
+            return True
+        return False
