1cc017a03168acecbc1dbb709e3c40e5444ffc6b
Dimple Mehra <60533553+mehra-dimple@users.noreply.github.com>
2023-03-14 16:41:55 -0700

    super30admin/master

---
+++ Day1_ImplementHashSet.py
@@ -0,0 +1,48 @@
+# TimeComplexity: O(1)
+# SpaceComplexity: O(n)
+# Accepted on Leetcode: https://leetcode.com/problems/design-hashset/submissions/915315294/
+
+class MyHashSet(object):
+
+    def __init__(self):
+        self.size = 10000
+        self.table = [None] * self.size
+
+
+    def hash_function(self, key):
+        return key % self.size
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        index = self.hash_function(key)
+        if self.table[index] == None:
+            self.table[index] = [key]
+        else:
+            self.table[index].append(key)
+
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        index = self.hash_function(key)
+        if self.table[index] != None:
+            while key in self.table[index]:
+                self.table[index].remove(key)
+
+    def contains(self, key):
+        """
+        :type key: int
+        :rtype: bool
+        """
+        index = self.hash_function(key)
+        if self.table[index] != None:
+            return key in self.table[index]
+        else:
+            return False
+        
+        
