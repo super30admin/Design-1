3ab5d66a991d8c5b68056ec79c9c047db50a7430
malfusion2 <134125857+malfusion2@users.noreply.github.com>
2023-05-21 15:20:45 -0700

    Create Exercise_1.py

---
+++ Exercise_1.py
@@ -0,0 +1,29 @@
+# Time Complexity : O(n/k) for all operations, where n is the number of keys and k is the number of buckets.
+# Space Complexity : O(n) where n is the number of keys present in the hashset
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Uses a simple hashing function to obtain a bucket from the given key, and then an array within the bucket stores the elements.
+
+
+class MyHashSet:
+
+    def __init__(self):
+        self.numBuckets = 1000
+        self.buckets = [[] for _ in range(self.numBuckets)]
+
+    def add(self, key: int) -> None:
+        b = self._bucket(key)
+        if key not in b:
+            b.append(key)
+
+    def remove(self, key: int) -> None:
+        b = self._bucket(key)
+        if key in b:
+            b.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self._bucket(key)
+
+    def _bucket(self, key: int):
+        return self.buckets[key % self.numBuckets]
