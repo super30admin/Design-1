628f5c7fd35364ab88814f41420176725912efbb
SuvidAnand41292 <suvid41292@gmail.com>
2023-05-23 17:28:13 -0700

    Design-1 Completed - Please review

---
+++ minstack.py
@@ -0,0 +1,24 @@
+# Time Complexity : O(1)
+# Space Complexity :O(n)
+# Did this code successfully run on Leetcode : Yes
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
