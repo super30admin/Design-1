9826394c67c9eb00bffe1c7270c722b696a6fc7f
Satish Solanki <satishsolanki1990@gmail.com>
2023-05-24 23:29:57 -0700

    complete hashmap with linkedlist as 2ndry

--- LC705-designHashset.java
+++ LC705-designHashset.java
@@ -16,91 +16,78 @@
          collision handelling : linkedlist
 */
 
+
 class MyHashSet {
     
-    // set imperical size of the base data structure 
-    private int size = 1000;
-    Node[] arr = new Node[size];
-
+    
     public class Node {
         int key;
         Node next;
-            
+
         public Node (int data) {
             key = data;
         }
     }
+
+    // we iterate thru linkedlist multiple time, so lets make a function
+    private Node findNode(Node head, int key) {
+        // will return 1 node prev
+        Node prev = null;
+        Node cur = head;
+        while (cur!=null && cur.key!= key){
+            prev = cur;
+            cur = cur.next;
+        }
+        return prev;
+    }
+
+    // get hash 
+    private int hash(int key) {
+        return key % 1000;
+    }
     
-    public MyHashSet() {}
+
+    
+    private int size;
+    Node[] arr;
+        
+    public MyHashSet() {
+        size = 1000;// set imperical size of the base data structure 
+        arr = new Node[size];
+    }
     
     public void add(int key) {
-        if (!contains(key)) 
-        {
-            int _hash = hash(key);
+        int _hash = hash(key);
 
-            Node temp = arr[_hash];
+        if (arr[_hash] == null) {
+            arr[_hash] = new Node(-1); // dummy node
+        } 
 
-            if (temp != null) {
-
-                while(temp.next != null) temp = temp.next;
-                temp.next = new Node(key);
-
-            } else {
-                arr[_hash] = new Node(key);
-            }
-        }
+        //find the node
+        Node prev = findNode(arr[_hash], key);
+        if (prev.next == null) {
+            prev.next = new Node(key);
+        } 
     }
     
     public void remove(int key) {
-        if (contains(key)) {
-            int _hash = hash(key);
+        int _hash = hash(key);
+        if (arr[_hash] == null) return;
 
-            Node temp = arr[_hash];
+        Node prev = findNode(arr[_hash],key);
 
-            if (temp != null) {
-
-
-                while (temp.next != null && temp.next.key != key) temp = temp.next;
-
-                 // single value
-                if (temp.next == null && temp.key == key) {
-                    arr[_hash] = null;
-                } else if (temp.next != null && temp.next.key == key) {
-                    temp.next = temp.next.next;
-                }
-            }
+        if (prev.next != null) {
+            // found the node to remove
+            prev.next = prev.next.next;
         }
     }
     
     public boolean contains(int key) {
         int _hash = hash(key);
-        
-        if (arr[_hash] == null) {
-            return false;
-        } else {
-            Node temp = arr[_hash];
-            while(temp != null) {
-                if (temp.key == key) return true;
-                temp = temp.next;
-            }
-        }
-        
-        return false;
-        
-    }
-    
-    // get hash 
-    private int hash(int key) {
-        return key % 1000;
-    }
+        if (arr[_hash] == null) return false;
+
+        Node prev = findNode(arr[_hash], key);
+
+        return prev.next != null;
+    }       
 }
-
-
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
