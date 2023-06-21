066f50aa6fa2b369689285cf036cb6e221860c9c
Shrushitha <36665943+Shrushitha@users.noreply.github.com>
2023-06-21 17:42:20 -0400

    minstack.java

--- Sample.java
+++ Sample.java
@@ -1,58 +1,54 @@
 // Time Complexity :O(1)
-// Space Complexity :O(n)
+// Space Complexity :O(1 but 29/31 testcases passed)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :
 
-class MyHashSet {
-    private int buckets;
-    private int bucketItems;
-    private boolean[][] storage;
-    public MyHashSet() {
-        this.buckets=10000;
-        this.bucketItems=10000;
-        this.storage= new boolean[buckets][];
-
-    }
-    private int bucket(int key){
-        return key%buckets;
-    }
-    private int bucketItem(int key){
-        return key%bucketItems;
+class MinStack {
+    private int top = -1;
+    private int max = 100000;
+    private int stack[] = new int[max];
+    private int min;
+    private int x;
+    private int prevmin;
+    
+    public MinStack() {
+        // Constructor - nothing specific to initialize
     }
     
-    public void add(int key) {
-         int bucket=bucket(key);
-         int bucketItem=bucketItem(key);
-        if(storage[bucket]==null){
-           if(bucket==0){
-             storage[bucket] = new boolean[bucketItems+1];
-           }
-           else{
-    storage[bucket] = new boolean[bucketItems];
-        }
-        }
-         storage[bucket][bucketItem]=true;
-    
-
-    }
-    
-    public void remove(int key) {
-        int bucket=bucket(key);
-         int bucketItem=bucketItem(key);
-        if(storage[bucket]==null){
-             return;
-         }
-         storage[bucket][bucketItem]=false;
+    public void push(int val) {
+        // Pushes the given value onto the stack
+        stack[++top] = val;
         
+        if (top == 0) {
+            min = stack[top]; // If the stack is empty, set the minimum as the first element
+        }
+        
+        prevmin = min; // Store the previous minimum
+        if (min >= val) {
+            min = val; // Update the minimum if the new value is smaller
+        }
     }
     
-    public boolean contains(int key) {
-        int bucket=bucket(key);
-         int bucketItem=bucketItem(key);
-         if(storage[bucket]==null){
-             return false;
-         }
-        return  storage[bucket][bucketItem];
+    public void pop() {
+        // Removes the top element from the stack
+        x = stack[top--];
+        
+        if (x == min) {
+            min = prevmin; // If the popped element was the minimum, restore the previous minimum
+        }
+    }
+    
+    public int top() {
+        // Returns the top element of the stack
+        return stack[top];
+    }
+    
+    public int getMin() {
+        // Returns the minimum element in the stack
+        if (top == 0) {
+            return stack[top]; // If the stack has only one element, it is the minimum
+        }
+        return min;
     }
 }
 
