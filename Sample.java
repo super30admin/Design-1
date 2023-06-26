d9c9695040a25c1e921747d5cf8ac5d95e262d82
akshay <er.akshay@gmail.com>
2023-06-26 11:53:56 -0700

    Hashset designed using boolean array

--- Sample.java
+++ Sample.java
@@ -1,7 +1,25 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+class MyHashSet {
+    boolean[] arr;
+    public MyHashSet() {
+        arr  = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        arr[key] = true;
+    }
+    
+    public void remove(int key) {
+        arr[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return arr[key];
+    }
+}
