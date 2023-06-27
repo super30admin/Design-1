c9efd04e1fa110cd28a6158418ac500df3bdd69d
Adduri <purna.adduri@sap.com>
2023-06-27 08:13:52 -0700

    Completed Design HashSet

--- Sample.java
+++ Sample.java
@@ -1,7 +1,27 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+
+boolean[] arr;
+    public MyHashSet() {
+        arr = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        arr[key] = true;
+    }
+    
+    public void remove(int key) {
+        arr[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return arr[key];
+    }
+}
