20e91a7e501564a13d2a6a3ed4dc62b25f1ddbb1
somonnoy <sb7238@g.rit.edu>
2023-06-27 21:32:43 -0400

    MyHashSet.java completed

---
+++ MyHashSet.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class MyHashSet {
+
+    ArrayList<Integer> list;
+
+    public MyHashSet() {
+        list = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        if(!list.contains(key))
+        {
+            list.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        list.remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        return list.contains(key);
+    }
+
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
