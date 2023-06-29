acb7bc331145d77f8a8b2f5cf5c1939f341792fa
lavanyareddy21 <lavanyareddy2095@gmail.com>
2023-06-28 20:08:45 -0700

    Updated hashset file of Design1

--- hashset.py
+++ hashset.py
@@ -1,36 +1,32 @@
-# time complexity:O(1) on average 
-# space complexity: O(1)
+# Time Complexity: O(1)
+# Space Complexity: O(n)
 class MyHashSet:
 
     def __init__(self):
-        self.bucket_size = 1000 # declare the size of array 
-        self.bucket = [[] for _ in range(self.bucket_size)]  # create a nested list
+        self.bucket_size = 1000
+        self.bucket = [[] for _ in range(self.bucket_size)]
 
     def add(self, key: int) -> None:
-        index = key % self.bucket_size      # calculate index and secodary index using modulo and integer division operations
+        index = key % self.bucket_size
         secondary_index = key // self.bucket_size
-        if not self.contains(key):  # check if key present in contains
-            self.bucket[index].append(secondary_index) # if not: append key into index 
+        if secondary_index >= len(self.bucket[index]):
+            self.bucket[index].extend([None] * (secondary_index - len(self.bucket[index]) + 1))
+        self.bucket[index][secondary_index] = True
 
     def remove(self, key: int) -> None:
-        index = key % self.bucket_size      # delete the key 
+        index = key % self.bucket_size
         secondary_index = key // self.bucket_size
-        if (
-                index < len(self.bucket)
-                and secondary_index in self.bucket[index]
-        ):
-            self.bucket[index].remove(secondary_index)
+        if secondary_index < len(self.bucket[index]):
+            self.bucket[index][secondary_index] = False
 
     def contains(self, key: int) -> bool:
         index = key % self.bucket_size
         secondary_index = key // self.bucket_size
-        if (
-                index < len(self.bucket)
-                and secondary_index in self.bucket[index]
-        ):
+        if secondary_index < len(self.bucket[index]) and self.bucket[index][secondary_index] is True:
             return True
         else:
             return False
+
 
 obj = MyHashSet()
 obj.add(1)
