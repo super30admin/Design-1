cf7e78ab03f8031f2cadb5b854e385706dcdba4f
Vartika Joshi <96254851+VartikaaJoshi@users.noreply.github.com>
2023-07-26 16:59:59 -0700

    Adding comment in Minstack.py

--- Minstack.py
+++ Minstack.py
@@ -1,47 +1,27 @@
-''' Creating a class called MinStack'''
+'''
+This MinStack class implements a stack data structure that, in addition to usual push and pop operations, has a function to retrieve the minimum element in O(1) time. The stack is implemented using a Python list, and a variable 'min' keeps track of the current minimum. Upon every push operation, if the new element is smaller or equal to the current minimum, it first pushes the current minimum onto the stack and then the new element.
+'''
+
 class MinStack:
 
-    ''' Defining the constructor method that gets called when a new object 
-of this class is instantiated'''
     def __init__(self):
-      ''' Initializing an empty list to use as the main stack'''
-        self.stack=[]
-        ''' Setting initial minimum value as maximum int value'''
-        self.min = int(sys.maxsize)
+        self.stack = []
+        self.min = float('inf')
 
-    ''' Defining the method to push a value onto the stack'''
     def push(self, val: int) -> None:
-        ''' Checking if the value being pushed is less than or equal to 
-the current minimum value in the stack'''
         if val <= self.min:
-        ''' If so, pushing the current minimum value onto the stack 
-before pushing the new value'''
             self.stack.append(self.min)
-            # Then, updating the current minimum value
             self.min = val
-        # Regardless, always pushing the new value onto the stack
         self.stack.append(val)
 
-    # Defining the method to pop a value from the stack
     def pop(self) -> None:
-        # Popping the top value from the stack
-        popped = self.stack.pop()
-        # Checking if the popped value is the current minimum
-        if popped == self.min:
-            # If so, popping the next value from the stack, which is the 
-previous minimum value, and setting it as the new minimum
-            self.min= self.stack.pop()
+        if self.stack:
+            popped = self.stack.pop()
+            if popped == self.min:
+                self.min = self.stack.pop() if self.stack else float('inf')
 
-    # Defining the method to get the top value of the stack
     def top(self) -> int:
-       ''' Returning the last item in the list, which represents the top 
-of the stack'''
+        return self.stack[-1] if self.stack else None
 
-        return self.stack[-1]
-
-    # Defining the method to get the current minimum value in the stack
     def getMin(self) -> int:
-        # Returning the current minimum value
-        return self.min
-
-
+        return self.min if self.min != float('inf') else None
