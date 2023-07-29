03a8240070112fcb2db4f88083d5b2640a290884
dixitamendpara <47654180+DixitaMendpara@users.noreply.github.com>
2023-07-29 18:04:07 -0400

    Add files via upload

--- HashMap.py
+++ HashMap.py
@@ -1,46 +1,53 @@
-#for bucket and bucket items we have 1000 predefined size and hash function for getting value for bucket and bucket items
-#in put method if bucket is none initialize new item 
-#in get method return true if bucket and bucket item found otherwise false
-#for remove set bucket and bucket item to none
-class MyHashMap(object):
+#use find method to fing the prev node use it in put method if bucket has no node then define dummy node(-1,-1)
+#to get value also use previous node and if prev node has next node return that value
+#to remove value use prev node and make next node value none
+class MyHashMap:
+
+    class Node:
+        def __init__(self,key:int,value:int):
+            self.key=key
+            self.value=value
+            self.next=None
+    def find(self,key:int,head:Node)->Node:
+        prev=head
+        curr=head.next
+        while curr!=None and curr.key!=key:
+            prev=curr
+            curr=curr.next
+        return prev
 
     def __init__(self):
-        self.buckets = 1000
-        self.bucketItems = 1000
-        self.storage = [None] * self.buckets
+        self.buckets=10000
+        self.storage=[None]* self.buckets
+    
+    def getBucket(self, key:int)->int:
+        return hash(key)%self.buckets
 
-    def getBucket(self, key):
-        return key % self.buckets
-
-    def getBucketItems(self, key):
-        return key // self.bucketItems
-
-    def put(self, key, value):
-        bucket = self.getBucket(key)
-        bucketItem = self.getBucketItems(key) 
-
-        if self.storage[bucket] == None:
-            if bucket == 0:
-                self.storage[bucket] = [None] * (self.bucketItems + 1)
-            else:
-                self.storage[bucket] = [None] * self.bucketItems
-
-        self.storage[bucket][bucketItem] = True
+    def put(self, key: int, value: int) -> None:
+        bucket=self.getBucket(key)
+        if self.storage[bucket]==None:
+            self.storage[bucket]=self.Node(-1,-1)
         
+        prev=self.find(key,self.storage[bucket])
+        if prev.next==None:
+            prev.next=self.Node(key,value)
+        else:
+            prev.next.value=value
 
-    def get(self, key):
-        bucket = self.getBucket(key)
-        bucketItem = self.getBucketItems(key) 
+    def get(self, key: int) -> int:
+        bucket=self.getBucket(key)
+        if self.storage[bucket]==None:
+            return -1
+        prev=self.find(key,self.storage[bucket])
+        if prev.next==None:
+            return -1
+        return prev.next.value
 
-        if self.storage[bucket] is None:
-            return False
-
-        return self.storage[bucket][bucketItem] is True
-        
-
-    def remove(self, key):
-        bucket = self.getBucket(key)
-        bucketItem = self.getBucketItems(key) 
-
-        if self.storage[bucket] is not None and self.storage[bucket][bucketItem] is not None:
-            self.storage[bucket][bucketItem] = None
+    def remove(self, key: int) -> None:
+        bucket=self.getBucket(key)
+        if self.storage[bucket]==None:
+            return
+        prev=self.find(key,self.storage[bucket])
+        if prev.next==None:
+            return
+        prev.next=prev.next.next
