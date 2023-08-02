44275cf17c762f80ab35623b382edf3d3ef4c9e9
chsrimanaswi <53294091+chsrimanaswi@users.noreply.github.com>
2023-08-01 23:04:24 -0400

    Design-1 Completed - Please review

--- MinStack.java
+++ MinStack.java
@@ -1,3 +1,11 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
 class MinStack {
 
     Stack<Integer> st;
