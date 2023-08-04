5ac4402ade11903dd2c497989a6ebe374c25de4b
Sandesh Shalavadi <sandeshshalavadi1995@gmail.com>
2023-08-04 15:47:01 -0700

    Create MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,31 @@
+class MinStack {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
