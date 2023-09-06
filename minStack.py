951b8f0c099a1bc81462f83f6b76cbcedf1f43b4
anaghayn <anagha2605@gmail.com>
2023-09-06 15:01:09 -0700

    Create minStack.py

---
+++ minStack.py
@@ -0,0 +1,51 @@
+# Time Complexity = O(1)
+# Space Complexity = O(n)
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min = float('inf')
+
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if self.min >= val:
+            self.stack.append(self.min)
+            self.min = val
+        self.stack.append(val)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.stack.pop() == self.min:
+            self.min = self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min
+
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
