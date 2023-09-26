cba88467e902886034d911442ff2e6ec03e576a7
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-09-26 00:01:30 -0700

    Create Min_Stack.cpp

---
+++ Min_Stack.cpp
@@ -0,0 +1,38 @@
+// Time O(1)
+// Space O(n)
+
+#include <vector>
+class MinStack {
+public:
+vector<pair<int,int>> stack;
+    MinStack() {
+        vector<pair<int,int>> stack;
+
+    }
+    
+    void push(int val) {
+    if(stack.empty())stack.push_back({val,val});
+    else stack.push_back({val,min(stack.back().second,val)});
+    }
+    
+    void pop() {
+        stack.pop_back();
+    }
+    
+    int top() {
+        return stack.back().first;
+    }
+    
+    int getMin() {
+        return stack.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
