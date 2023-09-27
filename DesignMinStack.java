e6e97ada8fc5278289f5eca62e20bae46d05d276
Harsh Gupta <harshhgupta16@gmail.com>
2023-09-27 10:29:56 -0400

    Design 1 done

--- DesignMinStack.java
+++ DesignMinStack.java
@@ -1,51 +1,46 @@
 // T.C -> O(1)
 // S.C -> O(N)
 
-// Does it pass LC: No
-// I have passed 28/31 test case and I am having problem when integer goes out of range
+// Does it pass LC: Yes
+// I earlier passed 28/31 test case and I am having problem when integer goes out of range. 
+// Then I used two Stacks to solve it. 
+
+import java.util.Stack;
 
 public class DesignMinStack {
-    int cnt;
-    int[] arr;
-    int[] minUntilNow;
+    private Stack<Integer> stk;
+    private Stack<Integer> minStk;
 
     public DesignMinStack() {
-        cnt = -1;
-        arr = new int[1000000];
-        minUntilNow = new int[1000000];
+        stk = new Stack<Integer>();
+        minStk = new Stack<Integer>();
     }
 
     public void push(int val) {
-        cnt++;
-        arr[cnt] = val;
+        stk.push(val);
 
-        if (cnt == 0)
-            minUntilNow[cnt] = val;
-        else {
-            if (val < minUntilNow[cnt - 1])
-                minUntilNow[cnt] = val;
-            else
-                minUntilNow[cnt] = minUntilNow[cnt - 1];
-
+        if (minStk.isEmpty()) {
+            minStk.push(val);
+        } else {
+            if (val < minStk.peek()) {
+                minStk.push(val);
+            } else {
+                minStk.push(minStk.peek());
+            }
         }
 
     }
 
     public void pop() {
-        if (cnt > 0)
-            cnt--;
+        stk.pop();
+        minStk.pop();
     }
 
     public int top() {
-        if (cnt > -1) {
-            return arr[cnt];
-        } else {
-            return -1;
-        }
-
+        return stk.peek();
     }
 
     public int getMin() {
-        return minUntilNow[cnt] == Integer.MIN_VALUE ? arr[cnt] : minUntilNow[cnt];
+        return minStk.peek();
     }
 }
