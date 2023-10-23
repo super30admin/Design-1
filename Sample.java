9b5a422ae3243ca38c853e1dbd9b17e168c7f0bf
sure402 <sure402@gmail.com>
2023-10-23 16:18:47 -0700

    Completeed Design-1

--- Sample.java
+++ Sample.java
@@ -63,3 +63,46 @@ class MyHashSet {
  * obj.remove(key);
  * boolean param_3 = obj.contains(key);
  */
+
+class MinStack {
+
+  private Stack<Integer> st;
+   private Stack<Integer> minStack;
+   private int min;
+
+    public MinStack() {
+    this.min = Integer.MAX_VALUE;
+       this.st = new Stack<>();
+       this.minStack = new Stack<>();
+       this.minStack.push(min);
+    }
+    
+    public void push(int val) {
+       min = Math.min(min, val);
+       st.push(val);
+       minStack.push(min);   
+    }
+    
+    public void pop() {
+      st.pop();
+      minStack.pop();
+      this.min = minStack.peek();   
+    }
+    
+    public int top() {
+        return st.peek();    
+    }
+    
+    public int getMin() {
+         return minStack.peek();   
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
