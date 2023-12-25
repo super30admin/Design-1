b1f1774dfcd08dd212bc1219a9ccd4715f3dd2e1
Nidhi Dewan <nidhidewan@Nidhis-MacBook-Pro.local>
2023-12-25 01:10:18 -0800

    done

--- MinStack.java
+++ MinStack.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class MinStack {
+import java.util.Stack;
+
+class MinStack {
+    Stack<Pair> s1;
+
+
+    public MinStack() {
+        s1=new Stack();
+
+    }
+    public class Pair{
+        int key;
+        int val;
+        Pair(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+
+    public void push(int x) {
+        int min;
+        if (s1.isEmpty()) {
+            min = x;
+        } else {
+            min = Math.min(s1.peek().val, x);
+        }
+        s1.push(new Pair(x, min));
+    }
+
+    public void pop() {
+        s1.pop();
+    }
+
+    public int top() {
+        return s1.peek().key;
+    }
+    public int getMin() {
+        return  s1.peek().val;
+    }
 }
