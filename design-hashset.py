9baca7e6747b69bb79e69344509d95d8c398a667
Siddesh Iyer <iyersiddesh10@gmail.com>
2024-01-01 08:21:45 +0400

    practiced hashset

--- design-hashset.py
+++ design-hashset.py
@@ -1,21 +1,41 @@
 # Time complexity: O(n/k) where n is the number of elements and k is size of the list which is 1000
 # Space Complexity: O(N)
 # Did this code successfully run on Leetcode : yes
+# class MyHashSet:
+
+#     def __init__(self):
+#         self.storage = [[] for index in range(1000)]
+
+#     def add(self, key: int) -> None:
+#         hash = key % 1000
+#         if not self.contains(key):
+#             self.storage[hash].append(key)
+
+#     def remove(self, key: int) -> None:
+#         hash = key % 1000
+#         if self.contains(key):
+#             self.storage[hash].remove(key)
+
+#     def contains(self, key: int) -> bool:
+#         hash = key % 1000
+#         return key in self.storage[hash]
+
 class MyHashSet:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.storage = [[] for index in range(1000)]
 
-    def add(self, key: int) -> None:
+    def add(self, key:int):
         hash = key % 1000
         if not self.contains(key):
             self.storage[hash].append(key)
-
-    def remove(self, key: int) -> None:
-        hash = key % 1000
+    
+    def remove(self, key:int):
+        hash = key%1000
         if self.contains(key):
             self.storage[hash].remove(key)
 
-    def contains(self, key: int) -> bool:
-        hash = key % 1000
+    def contains(self,key:int) -> bool:
+        hash = key%1000
         return key in self.storage[hash]
+            
