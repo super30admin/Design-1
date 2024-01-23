e285fc3f209b96e0c5318f6b31e3ab54e31f7971
mythreye1998 <35624726+mythreye1998@users.noreply.github.com>
2024-01-23 16:10:37 -0500

    Update DesignHashset.java

--- DesignHashset.java
+++ DesignHashset.java
@@ -1,4 +1,4 @@
-//Several elements in a hash set may hash to the same index. I've used a linked list to hold all the elements that hash to the same index in order to prevent collisions. A key-value pair is represented by each element in the linked list. In this manner, items having the same hash code can be kept in the same block together. 
+//Several elements in a hash set may hash to the same index. A is used to linked list to hold all the elements that hash to the same index in order to prevent collisions. A key-value pair is represented by each element in the linked list. In this manner, items having the same hash code can be kept in the same block together. 
 //In the worst scenario, the time complexity is O(n) because linked lists use linear searches.
 // Space complexity - o(n+m)
 class MyHashSet {
