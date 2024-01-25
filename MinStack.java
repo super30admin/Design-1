92a07c12b5dfbe473c63201d82d28170b1103f01
nirvanvjain <nirvanvjain22@gmail.com>
2024-01-25 08:31:39 -0600

    Minor edge case validations

--- MinStack.java
+++ MinStack.java
@@ -1,5 +1,12 @@
-class MinStack {
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes (I do not have premium account though.)
+// Any problem you faced while coding this : Syntax issues. Struggling at the moment
 
+// It is two stack approach. Maintainly two stacks - one for push and another one for min
+// min stack only holds min values. It new value is less than then push. If it is duplicate then also push.
+
+public class MinStack {
     private Stack<Integer> minstack;
     private Stack<Integer> stack;
     private int min;
@@ -7,7 +14,7 @@ class MinStack {
     public MinStack() {
         this.min = Integer.MAX_VALUE;
         this.stack = new Stack();
-        this.minstack = new MinStack()
+        this.minstack = new Stack();
     }
 
     public void push(int val) {
@@ -21,22 +28,23 @@ class MinStack {
     public void pop() {
         if(stack.empty()) return;
 
-        int val = stack.pop();
+        int val = stack.pop(); // return integer value in this case - AutoBoxing/Unboxing
         if(val == min) {
-            minstack.pop(val);
-            min = minstack.peek()
+            minstack.pop();
+            min = !minstack.empty()?minstack.peek():Integer.MAX_VALUE; //though this is not required as we are performing operation on non-empty stack
         }
-
     }
 
     public int top() {
-        stack.peek();
+        if(stack.empty()) return -1;
+        return stack.peek();
     }
 
     public int getMin() {
-        retun min;
+        return this.min;
     }
 }
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
