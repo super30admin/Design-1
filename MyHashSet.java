a88630bcb274414ee732d6275b147ff60114f1dc
Arun Rao <arunrao@Aruns-MacBook-Air.local>
2024-01-25 12:18:22 -0800

    Done Design-1

---
+++ MyHashSet.java
@@ -0,0 +1,22 @@
+package s30;
+
+class MyHashSet {
+	private boolean [][]storage;
+	int buckets;
+	int bucketitems;
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+    
+    public boolean contains(int key) {
+        
+    }
+}
