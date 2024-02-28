511694d9959927b28f1f548a1427932675d785ec
ParvathiRPai <parvathirpai@outlook.com>
2024-02-28 13:02:28 -0800

    hset and minstack

---
+++ minstack.py
@@ -0,0 +1,20 @@
+class MinStack:
+    def __init__(self):
+        self.stack =[]
+        self.minstack = []
+        
+    def push(self, val):
+        self.stack.append(val)
+        val = min(val, self.minstack[-1] if self.minstack else val)
+        self.minstack.append(val)
+        
+    def pop(self):
+        self.stack.pop()
+        self.minstack.pop()
+        
+    def top(self):
+        return self.stack[-1]
+    
+    def getMin(self):
+        return self.minstack[-1]
+    
